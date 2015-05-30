35a88954ed411ac94ae56f99d8502b351c25440d
akileez <git@keeda.net>
2015-05-30 17:41:07 -0400

    update toArray

--- src/lang/toArray.js
+++ src/lang/toArray.js
@@ -1,7 +1,22 @@
+var kindOf = require('./kindOf')
+var isFiniteNumber = require('./isFiniteNumber')
+
 function toArray (value) {
-  if (value === null || value === undefined) return []
-  else if (typeof value.length === 'number' &&  isArguments(value)) return Array.prototype.slice.call(value)
-  else return Array.isArray(value) ? value : [ value ]
+  var ret = []
+  var kind = kindOf(value)
+  var n
+
+  if (value !== null && value !== undefined) {
+    if (value.length === null || kind === 'string' || kind === 'function' || kind === 'regExp' || isFiniteNumber(value)) {
+      ret[ret.length] = value
+    } else {
+      n = value.length
+      while (n--) {
+        ret[n] = value[n]
+      }
+    }
+  }
+  return ret
 }
 
 module.exports = toArray
