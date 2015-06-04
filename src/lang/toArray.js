ea19b1957b4cd342414346ee079d450f0ff8da0d
akileez <git@keeda.net>
2015-06-04 08:34:10 -0400

    update toArray
    use mout.js original version. achieves the effect I was trying to
    obtain. use of GLOBAL fixed the issue. I did not use that originally.

--- src/lang/toArray.js
+++ src/lang/toArray.js
@@ -1,15 +1,22 @@
 var kindOf = require('./kindOf')
-var isFiniteNumber = require('./isFinite')
+var GLOBAL = require('./GLOBAL')
+
+// convert array-like object into array
 
 function toArray (value) {
   var ret = []
   var kind = kindOf(value)
   var n
 
-  if (value !== null && value !== undefined) {
-    if (value.length === null || kind === 'string' || kind === 'function' || kind === 'regExp' || isFinite(value)) {
+  if (value != null){
+    if (value.length == null || kind === 'string' || kind === 'function' || kind === 'regexp' || value === GLOBAL) {
+      // string, regexp, function have .length but user probably just want
+      // to wrap value into array..
       ret[ret.length] = value
     } else {
+      // window returns true on isObject in IE7 and may have length
+      // property. `typeof NodeList` returns function on Safari so
+      // we can't use it (#58)
       n = value.length
       while (n--) {
         ret[n] = value[n]
