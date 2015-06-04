6794a6d2691c4b67f99b089ad2d7af9978836227
akileez <git@keeda.net>
2015-06-04 15:27:29 -0400

    source requires

---
+++ src/query.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'contains' : require('./queryString/contains'),
+  'decode'   : require('./queryString/decode'),
+  'encode'   : require('./queryString/encode'),
+  'getParam' : require('./queryString/getParam'),
+  'getQuery' : require('./queryString/getQuery'),
+  'parse'    : require('./queryString/parse'),
+  'setParam' : require('./queryString/setParam')
+}
---
+++ src/time.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'convert'      : require('./time/convert'),
+  'now'          : require('./time/now'),
+  'parseMs'      : require('./time/parseMs'),
+  'toTimeString' : require('./time/toTimeString')
+}
