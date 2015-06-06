cd191944448f054ad6414c4abe0c3133f5e9073d
akileez <git@keeda.net>
2015-06-06 09:28:01 -0400

    typo

--- src/array/filter.js
+++ src/array/filter.js
@@ -2,7 +2,7 @@ var makeIterator = require('../function/makeIterator_')
 
 function filter (arr, fn, thisObj) {
   fn = makeIterator(fn, thisObj)
-  var resuts = []
+  var results = []
   if (arr == null) return results
   var iter = -1
   var len = arr.length
