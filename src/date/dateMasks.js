1321c8c6c1d1b64b343f1e6c818b85592e07b3f8
akileez <git@keeda.net>
2015-06-07 04:53:49 -0400

    source file
    date masks for dateFormat

---
+++ src/date/dateMasks.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'default'             : 'ddd mmm dd yyyy HH:MM:SS',
+  'now'                 : 'dddd, mmmm o, yyyy HH:MM:SS \'GMT\'Z',
+  'shortDate'           : 'm/d/yy',
+  'mediumDate'          : 'mmm d, yyyy',
+  'longDate'            : 'mmmm d, yyyy',
+  'fullDate'            : 'dddd, mmmm d, yyyy',
+  'shortTime'           : 'h:MM TT',
+  'mediumTime'          : 'h:MM:SS TT',
+  'longTime'            : 'h:MM:SS TT Z',
+  'isoDate'             : 'yyyy-mm-dd',
+  'isoTime'             : 'HH:MM:SS',
+  'iso'                 : 'yyyy-mm-dd\'T\'HH:MM:SSZ',
+  'isoDateTime'         : 'yyyy-mm-dd\'T\'HH:MM:SS:ss',
+  'isoUtcDateTime'      : 'yyyy-mm-dd\'T\'HH:MM:SS\'Z\'',
+  'expiresHeaderFormat' : 'ddd, dd mmm yyyy HH:MM:SS Z',
+  'timestamp'           : 'yyyymmddHHMMSSss'
+}
