7620f8adee5952c72974d2e194600aff2751929b
akileez <git@keeda.net>
2015-06-16 18:04:53 -0400

    source files
    new uploads

---
+++ src/function/once.js
@@ -0,0 +1,10 @@
+function once (fn, context) {
+  var result
+  return function () {
+    if (fn) {
+      result = fn.apply(context || this, arguments)
+      fn = null
+    }
+    return result
+  }
+}
