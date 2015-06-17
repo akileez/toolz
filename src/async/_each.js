42dd9c280fe1577e87ca9dd1ccbe9c8705d6b085
akileez <git@keeda.net>
2015-06-17 11:57:17 -0400

    async refactor
    combine each and eachSeries into one file

--- src/async/_each.js
+++ src/async/_each.js
@@ -1,8 +1,14 @@
+var eachOf = require('./eachOf')
 var eachOfSeries = require('./eachOfSeries')
 var _withoutIndex = require('../utils/asyncWithoutIndex')
+
+function each (arr, iterator, callback) {
+  return eachOf(arr, _withoutIndex(iterator), callback)
+}
 
 function eachSeries (arr, iterator, callback) {
   return eachOfSeries(arr, _withoutIndex(iterator), callback)
 }
 
-module.exports = eachSeries
+exports.each = each
+exports.eachSeries = eachSeries
--- src/async/each.js
+++
@@ -1,8 +0,0 @@
-var eachOf = require('./eachOf')
-var _withoutIndex = require('../utils/asyncWithoutIndex')
-
-function each (arr, iterator, callback) {
-  return eachOf(arr, _withoutIndex(iterator), callback)
-}
-
-module.exports = each
