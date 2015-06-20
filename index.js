57cd05be765d2748f902862989dbc913415c531a
akileez <git@keeda.net>
2015-06-20 14:53:04 -0400

    move require indices to lib directory

--- index.js
+++ index.js
@@ -6,19 +6,19 @@
  */
 
 module.exports = {
-  'array'       : require('./src/array'),
-  'async'       : require('./src/async'),
-  'base'        : require('./src/base'),
-  'collection'  : require('./src/collection'),
-  'date'        : require('./src/date'),
-  'function'    : require('./src/function'),
-  'lang'        : require('./src/lang'),
-  'math'        : require('./src/math'),
-  'number'      : require('./src/number'),
-  'object'      : require('./src/object'),
-  'query'       : require('./src/query'),
-  'random'      : require('./src/random'),
-  'string'      : require('./src/string'),
-  'time'        : require('./src/time'),
-  'fn'          : require('./src/function')
+  'array'       : require('./lib/array'),
+  'async'       : require('./lib/async'),
+  'base'        : require('./lib/base'),
+  'collection'  : require('./lib/collection'),
+  'date'        : require('./lib/date'),
+  'function'    : require('./lib/function'),
+  'lang'        : require('./lib/lang'),
+  'math'        : require('./lib/math'),
+  'number'      : require('./lib/number'),
+  'object'      : require('./lib/object'),
+  'query'       : require('./lib/query'),
+  'random'      : require('./lib/random'),
+  'string'      : require('./lib/string'),
+  'time'        : require('./lib/time'),
+  'fn'          : require('./lib/function')
 };
