c2c0ac5cb350fc11670c13f38cb5f1f68105bfc5
akileez <git@keeda.net>
2015-06-20 09:10:20 -0400

    source file - deepExtend

---
+++ src/object/deepExtend.js
@@ -0,0 +1,23 @@
+var isPlainObject = require('../lang/isPlainObject')
+var forOwn = require('./forOwn')
+
+function deepExtend (target) {
+  var arg
+  var key
+  var len = arguments.length
+  var i = 0
+
+  while (++i < len) {
+    arg = arguments[i]
+    if (!arg) continue
+
+    forOwn(arg, function (val, key) {
+      var currVal = target[key]
+      if (isPlainObject(val) && isPlainObject(currVal)) deepExtend(currVal, val)
+      else target[key] = val
+    })
+  }
+  return target
+}
+
+module.exports = deepExtend
