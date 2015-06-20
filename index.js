24720d9ca400eb572dd0f8ce68a0ed07ced59def
akileez <git@keeda.net>
2015-06-20 12:00:15 -0400

    update require indices
    edit to base.js removing ‘.js’ from files and adding two new functions
    createElement and createTag. edit index.js to format 2 spaces instead
    of 4.

--- index.js
+++ index.js
@@ -6,19 +6,19 @@
  */
 
 module.exports = {
-    'array'       : require('./src/array'),
-    'async'       : require('./src/async'),
-    'base'        : require('./src/base'),
-    'collection'  : require('./src/collection'),
-    'date'        : require('./src/date'),
-    'function'    : require('./src/function'),
-    'lang'        : require('./src/lang'),
-    'math'        : require('./src/math'),
-    'number'      : require('./src/number'),
-    'object'      : require('./src/object'),
-    'query'       : require('./src/query'),
-    'random'      : require('./src/random'),
-    'string'      : require('./src/string'),
-    'time'        : require('./src/time'),
-    'fn'          : require('./src/function')
+  'array'       : require('./src/array'),
+  'async'       : require('./src/async'),
+  'base'        : require('./src/base'),
+  'collection'  : require('./src/collection'),
+  'date'        : require('./src/date'),
+  'function'    : require('./src/function'),
+  'lang'        : require('./src/lang'),
+  'math'        : require('./src/math'),
+  'number'      : require('./src/number'),
+  'object'      : require('./src/object'),
+  'query'       : require('./src/query'),
+  'random'      : require('./src/random'),
+  'string'      : require('./src/string'),
+  'time'        : require('./src/time'),
+  'fn'          : require('./src/function')
 };
