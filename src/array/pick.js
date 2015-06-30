f572b68da2b59cc83a67e83588780b95169beadc
akileez <git@keeda.net>
2015-06-30 13:31:57 -0400

    rename files
    pick —> pickRandRemove
    cherryPick —> pick
    more inline with functionality

--- src/array/pick.js
+++ src/array/pick.js
@@ -1,22 +1,27 @@
-var randInt = require('../random/randInt')
+var toArray = require('../lang/toArray')
 
-function pick (arr, nItems) {
-  if (nItems != null) {
-    var result = []
-    if (nItems > 0 && arr && arr.length) {
-      nItems = nItems > arr.length ? arr.length : nItems
-      while (nItems--) {
-        result.push(pickOne(arr))
-      }
-    }
-    return result
-  }
-  return (arr && arr.length) ? pickOne(arr) : void(0)
-}
+// was cherryPick. changed to array/pick and moved previous file
+// to array/pickRandRemove
 
-function pickOne (arr) {
-  var idx = randInt(0, arr.length -1)
-  return arr.splice(idx, 1)[0]
+function pick () {
+  var args = toArray(arguments)
+  var arrayOfObjects = args.shift()
+  var properties = args
+  if (!Array.isArray(arrayOfObjects)) throw new Error("pick() input must be an array")
+
+  return arrayOfObjects
+    .filter(function(obj) {
+      return properties.some(function(prop) {
+        return obj[prop] !== undefined
+      })
+    })
+    .map(function(obj) {
+      var output = {}
+      properties.forEach(function(prop) {
+        if (obj[prop] !== undefined) output[prop] = obj[prop]
+      })
+      return output
+    })
 }
 
 module.exports = pick
