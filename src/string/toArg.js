893f896dd67a030667f6e0e0f0104c7bdfd28bbc
akileez <git@keeda.net>
2015-07-02 11:15:13 -0400

    reformat string/toArg

--- src/string/toArg.js
+++ src/string/toArg.js
@@ -2,18 +2,17 @@ var dashify = require('./dashCase')
 var isPrimitive = require('../lang/isPrimitive')
 
 function toArgs (key, val, opts) {
-  if (!key || typeof key !== 'string') throw new TypeError('toArgs expects a string')
+  if (!key || typeof key !== 'string')
+    throw new TypeError('toArgs expects a string')
 
   if (Array.isArray(val)) val = val.join(',')
-
-  if (!isPrimitive(val)) throw new TypeError('toArgs expects an array or primitive for the second argument')
+  if (!isPrimitive(val))
+    throw new TypeError('toArgs expects an array or primitive for the second argument')
 
   if (val === true) val = null
-
   if (val === false && (!opts || opts && opts.invert !== false)) key = 'no-' + key
 
   key = '--' + (key.length === 1 ? key.toLowerCase() : dashify(key))
-
   return key + (val ? '=' + val : '')
 }
 
