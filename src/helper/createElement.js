8c32f9777168b48a98ca261b3952d028768006d1
akileez <git@keeda.net>
2015-07-04 11:08:50 -0400

    source file -- helper
    version 0.2.1 of createElement in new helper collection.
    version 0.1.0 will remain in string collection for now

---
+++ src/helper/createElement.js
@@ -0,0 +1,40 @@
+var emptyTags = [
+  'link',
+  'track',
+  'param',
+  'area',
+  'command',
+  'col',
+  'base',
+  'meta',
+  'hr',
+  'source',
+  'img',
+  'keygen',
+  'br',
+  'wbr',
+  'input'
+]
+
+function openTag (type, closing, attr) {
+  var html = ['<' + type]
+
+  for (var prop in attr) {
+    // A falsy value is used to remove the attribute.
+    // EG: attr[false] to remove, attr['false'] to add
+    if (attr[prop]) html.push(prop + '="' + attr[prop] + '"')
+  }
+  return html.join(' ') + (!closing ? ' /' : '') + '>'
+}
+
+function closeTag (type) {
+  return '</' + type + '>'
+}
+
+function createElement(type, attr, contents) {
+  var closing = emptyTags.indexOf(type) === -1 ? true : false
+
+  return openTag(type, closing, attr) + (closing ? (contents || '') + closeTag(type) : '')
+}
+
+module.exports = createElement
