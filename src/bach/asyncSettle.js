a23afd618f4fe22dc165b44bdcc45a8c2aa23e03
akileez <git@keeda.net>
2015-07-08 04:00:26 -0400

    source file -- bach

---
+++ src/bach/asyncSettle.js
@@ -0,0 +1,19 @@
+var asyncDone = require('./asyncDone')
+
+function settle (fn, done) {
+  asyncDone(fn, function (error, result) {
+    var settled = {}
+
+    if (error != null) {
+      settled.state = 'error'
+      settled.value = error
+    } else {
+      settled.state = 'success'
+      settled.value = result
+    }
+
+    done(null, settled)
+  })
+}
+
+module.exports = settle
