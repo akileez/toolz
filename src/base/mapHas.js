3162de1127f5c46e4d0e6586aee541db9c275b02
akileez <git@keeda.net>
2015-07-22 13:37:20 -0400

    bug fixes
    missing call in Map caches.

--- src/base/mapHas.js
+++ src/base/mapHas.js
@@ -3,7 +3,7 @@ var hasOwn = require('../object/hasOwn')
 // checks if a cached value for 'key exists'
 
 function mapHas (key) {
-  return key != '__proto__' && hasOwn(this.__data__, key)
+  return key != '__proto__' && hasOwn.call(this.__data__, key)
 }
 
 module.exports = mapHas
