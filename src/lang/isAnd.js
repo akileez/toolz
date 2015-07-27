f372d24a0f9e75cb866b91ad2b3548cf4d59ec06
akileez <git@keeda.net>
2015-07-27 01:25:40 -0400

    update
    exclusive equal

--- src/lang/isAnd.js
+++ src/lang/isAnd.js
@@ -5,7 +5,7 @@ function isAnd (value) {
   var args = slice(arguments, 1, arguments.length)
 
   return every(args, function (val) {
-    return (value == val)
+    return (value === val)
   })
 }
 
--- src/lang/isOr.js
+++ src/lang/isOr.js
@@ -5,7 +5,7 @@ function isOr (value) {
   var args = slice(arguments, 1, arguments.length)
 
   return some(args, function (val) {
-    return (value == val)
+    return (value === val)
   })
 }
 
