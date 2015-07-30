7669acda52644e05c9f8bb743f71b911b9f668b9
akileez <git@keeda.net>
2015-07-30 17:39:15 -0400

    update async/iterate
    — add sequence and compose functions from caolan/async
    — minor edits

--- src/async/iterate.js
+++ src/async/iterate.js
@@ -1,13 +1,11 @@
 // async series collection
 // lightweight serial async iterators:
-// [each, reduce, map, filter, reject, detect, every, some, concat, times, series]
+// [each, reduce, map, filter, reject, detect, every, some, concat, times, series
+// sequence(composite) and compose]
 // adpoted from: https://github.com/aliaksandr-pasynkau/async-iterate
 
 function asyncEachArray (arr, iterator, done) {
-  if (!arr || !arr.length) {
-    done()
-    return
-  }
+  if (!arr || !arr.length) return done()
 
   var lastIdx = arr.length
   var iterate = function (idx) {
@@ -129,14 +127,58 @@ function asyncSeries (obj, done) {
   }, done)
 }
 
-exports.each   = asyncEach
-exports.reduce = asyncReduce
-exports.map    = asyncMap
-exports.filter = asyncFilter
-exports.reject = asyncReject
-exports.detect = asyncDetect
-exports.every  = asyncEvery
-exports.some   = asyncSome
-exports.concat = asyncConcat
-exports.times  = asyncTimes
-exports.series = asyncSeries
+function asyncSequence (/* functions ... */) {
+  var fns = arguments
+  return function () {
+    var that = this
+    var args = _baseSlice(arguments)
+    var cb = args.slice(-1)[0]
+
+    if (typeof cb === 'function') args.pop()
+    else cb = function () {}
+
+    asyncReduce(fns, args, function (resultObject, v, k, done) {
+      v.apply(that, resultObject.concat([function () {
+        var err = arguments[0]
+        var nextargs = _baseSlice(arguments, 1)
+        done(err, nextargs)
+      }]))
+    }, function (err, res) {
+      cb.apply(that, [err].concat(res))
+    })
+  }
+}
+
+function asyncCompose (/* functions ... */) {
+  return asyncSequence.apply(null, [].reverse.call(arguments))
+}
+
+function _baseSlice (arr, start) {
+  start = start || 0
+  var idx = -1
+  var len = arr.length
+
+  if (start) {
+    len -= start
+    len = len < 0 ? 0 : len
+  }
+  var result = Array(len)
+  while (++idx < len) {
+    result[idx] = arr[idx + start]
+  }
+  return result
+}
+
+exports.each      = asyncEach
+exports.reduce    = asyncReduce
+exports.map       = asyncMap
+exports.filter    = asyncFilter
+exports.reject    = asyncReject
+exports.detect    = asyncDetect
+exports.every     = asyncEvery
+exports.some      = asyncSome
+exports.concat    = asyncConcat
+exports.times     = asyncTimes
+exports.series    = asyncSeries
+exports.composite = asyncSequence
+exports.compose   = asyncCompose
