39c44fb896c18c820656915c88781988f34eb83a
akileez <git@keeda.net>
2015-07-31 16:34:22 -0400

    update async/limit
    add remainder functions map, filter, reject, detect, every, some,
    times, parallel.

--- src/async/limits.js
+++ src/async/limits.js
@@ -1,4 +1,5 @@
-// placeholder for async limit functions
+// lightweight limit async iterators:
+// [each, reduce, map, filter, reject, detect, every, some, times, parallel]
 
 function asyncEachArray (arr, limit, iterator, done) {
   if (!arr || !arr.length) return done()
@@ -8,17 +9,16 @@ function asyncEachArray (arr, limit, iterator, done) {
   lastIdx = len = arr.length
 
   var running = 0
-  var started = -1
+  var i = -1
 
   function next (err) {
-    if (err) return once(done(err))
-    if (--len === 0) return done(null)
+    if (--len === -1) return once(done(null))
 
-    while (running < limit && ++started < lastIdx) {
+    while (running < limit && ++i < lastIdx) {
       running += 1
-      iterator(arr[started], started, function (err) {
+      iterator(arr[i], i, function (err) {
         running -= 1
-        if (err) done(err)
+        if (err) return once(done(err))
         else next()
       })
     }
@@ -38,4 +38,109 @@ function asyncEach (obj, limit, iterator, done) {
   }, done)
 }
 
-exports.each = asyncEach
+function asyncReduce (obj, limit, result, iterator, done) {
+  asyncEach(obj, limit, function (v, k, done) {
+    iterator(result, v, k, function (err, value) {
+      result = value
+      done(err)
+    })
+  }, function (err) {
+      done(err, result)
+  })
+}
+
+function asyncMap (obj, limit, iterator, done) {
+  asyncReduce(obj, limit, [], function (resultObject, v, k, done) {
+    iterator(v, k, function (err, result) {
+      resultObject.push(result)
+      done(err, resultObject)
+    })
+  }, done)
+}
+
+function asyncFilter (obj, limit, iterator, done) {
+  asyncReduce(obj, limit, [], function (resultObject, v, k, done) {
+    iterator(v, k, function (err, result) {
+      if (result) resultObject.push(obj[k])
+      done(err, resultObject)
+    })
+  }, done)
+}
+
+function asyncReject (obj, limit, iterator, done) {
+  asyncReduce(obj, limit, [], function (resultObject, v, k, done) {
+    iterator(v, k, function (err, result) {
+      if (!result) resultObject.push(obj[k])
+      done(err, resultObject)
+    })
+  }, done)
+}
+
+function asyncDetect (obj, limit, iterator, cb) {
+  asyncReduce(obj, limit, [], function (resultObject, v, k, done) {
+    iterator(v, k, function (err, result) {
+      if (result) {
+        resultObject = obj[k]
+        return cb(err, resultObject)
+      }
+      done(err, resultObject)
+    })
+  }, cb)
+}
+
+function asyncEvery (obj, limit, iterator, done) {
+  asyncReduce(obj, limit, true, function (resultObject, v, k, done) {
+    iterator(v, k, function (err, result) {
+      if (!result) resultObject = false
+      done(err, resultObject)
+    })
+  }, done)
+}
+
+function asyncSome (obj, limit, iterator, done) {
+  asyncReduce(obj, limit, false, function (resultObject, v, k, done) {
+    iterator(v, k, function (err, result) {
+      if (result) resultObject = true
+      done(err, resultObject)
+    })
+  }, done)
+}
+
+function asyncTimes (num, limit, iterator, done) {
+  var obj = Array(num)
+  asyncReduce(obj, limit, [], function (resultObject, v, k, done) {
+    iterator(num, function (err, res) {
+      resultObject.push(res)
+      done(null, resultObject)
+    })
+  }, done)
+}
+
+function asyncParallel (obj, limit, done) {
+  asyncReduce(obj, limit, [], function (resultObject, v, k, done) {
+    v.call(null, function (err, res) {
+      resultObject.push(res)
+      done(null, resultObject)
+    })
+  }, done)
+}
+
+function once (fn) {
+  return function () {
+    var ret = fn.apply(this, arguments)
+    fn = noop
+    return ret
+  }
+}
+
+function noop () {}
+
+exports.each     = asyncEach
+exports.map      = asyncMap
+exports.filter   = asyncFilter
+exports.reject   = asyncReject
+exports.detect   = asyncDetect
+exports.every    = asyncEvery
+exports.some     = asyncSome
+exports.times    = asyncTimes
+exports.parallel = asyncParallel
