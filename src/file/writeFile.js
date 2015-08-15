a3ad956c90417230e028ab187897bf8dbd5755c6
akileez <git@keeda.net>
2015-08-15 05:34:53 -0400

    source file -- writeFile

---
+++ src/file/writeFile.js
@@ -0,0 +1,32 @@
+var path   = require('path')
+var fs     = require('fs')
+var mkdirp = require('./mkdirp')
+var exists = require('./exists')
+
+function writeFile (file, data, enc, cb) {
+  if ((arguments.length === 3 && typeof enc !== 'function') || arguments.length === 2) {
+    enc = enc || 'utf8'
+    var dir = path.dirname(file)
+    if (exists(dir)) return fs.writeFileSync.apply(fs, arguments)
+
+    mkdirp.sync(dir)
+    return fs.writeFileSync.apply(fs, arguments)
+  }
+
+  if (typeof enc === 'function') {
+    cb = enc
+    enc = 'utf8'
+  }
+
+  var dir = path.dirname(file)
+  exists(dir, function (res) {
+    if (res) return fs.writeFile(file, data, enc, cb)
+
+    mkdirp.async(dir, function (err) {
+      if (err) return cb(err)
+      fs.writeFile(file, data, enc, callback)
+    })
+  })
+}
+
+module.exports = writeFile
