9df131bdd62103c71fa8650d3fe1171b4130b0b5
akileez <git@keeda.net>
2015-08-18 16:24:22 -0400

    update file/modStats.js
    uncomfortable with key named `data`. changed to `content`

--- src/file/modStats.js
+++ src/file/modStats.js
@@ -10,14 +10,14 @@ var fs   = require('fs')
 
 function modifyStatsObject (stats, fp) {
   return {
-    abs   : fs.realpathSync(fp),
-    rel   : fp,
-    dir   : path.dirname(fp),
-    base  : path.basename(fp),
-    file  : path.basename(fp, path.extname(fp)),
-    ext   : path.extname(fp),
-    stats : stats,
-    data  : fs.readFileSync(fp, 'utf8')
+    abs     : fs.realpathSync(fp),
+    rel     : fp,
+    dir     : path.dirname(fp),
+    base    : path.basename(fp),
+    file    : path.basename(fp, path.extname(fp)),
+    ext     : path.extname(fp),
+    stats   : stats,
+    content : fs.readFileSync(fp, 'utf8')
   }
 }
 
