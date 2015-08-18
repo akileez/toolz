ab1b6eb1185cf48b97b81e700025b55bd5e326b3
akileez <git@keeda.net>
2015-08-18 12:41:48 -0400

    update file/modStats.js
    testing comments out. just testing. will not use most likely.

--- src/file/modStats.js
+++ src/file/modStats.js
@@ -1,6 +1,13 @@
 var path = require('path')
 var fs   = require('fs')
 
+/**
+ * modify the stats object with more filepath info and content
+ * @param  {Object} stats the return object from fs.[stats|lstat]
+ * @param  {String} fp    filepath of object to be stated
+ * @return {Object}       expanded stats with file content
+ */
+
 function modifyStatsObject (stats, fp) {
   return {
     abs   : fs.realpathSync(fp),
