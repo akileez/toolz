54c9c9049d32a93a5e58bbc8ee7dfc0f0bcd303a
akileez <git@keeda.net>
2015-08-28 04:47:35 -0400

    code lint and style adjustments

--- src/lang/isStream.js
+++ src/lang/isStream.js
@@ -2,25 +2,25 @@ function isStream (val) {
   return val !== null && typeof val === 'object' && typeof val.pipe === 'function'
 }
 
-function isStreamWritable (val) {
+function writable (val) {
   return isStream(val)
     && val.writable !== false
     && typeof val._write == 'function'
-    && typeof val._writableState == 'object';
-};
+    && typeof val._writableState == 'object'
+}
 
-function isStreamReadable (val) {
+function readable (val) {
   return isStream(val)
     && val.readable !== false
     && typeof val._read == 'function'
-    && typeof val._readableState == 'object';
-};
+    && typeof val._readableState == 'object'
+}
 
-function isStreamDuplex (val) {
-  return isStream.writable(val) && isStream.readable(val);
-};
+function duplex (val) {
+  return writable(val) && readable(val)
+}
 
 module.exports = isStream
-module.exports.isStreamReadable = isStreamReadable
-module.exports.isStreamWritable = isStreamWritable
-module.exports.isStreamDuplex = isStreamDuplex
+module.exports.readable = readable
+module.exports.writable = writable
+module.exports.duplex = duplex
