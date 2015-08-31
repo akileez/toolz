fcf7e16d8b13875d0356df2b6b7a275d5f145dcb
akileez <git@keeda.net>
2015-08-31 14:43:30 -0400

    async version file/rmFile.js
    proof of concept

--- src/file/rmFile.js
+++ src/file/rmFile.js
@@ -3,8 +3,19 @@ var exists = require('./exists')
 
 // make async version
 
-function rmFile (file) {
+function rmFile (file, cb) {
+  exists(file, function (res) {
+    if (!res) return cb('File does not exist')
+    // figure out callback parameters for fs.unlink. don't think its err, res
+    fs.unlink(file, function (err, result) {
+      cb('File removed successfully.')
+    })
+  })
+}
+
+function rmFileSync (file) {
   if (exists(file)) fs.unlinkSync(file)
 }
 
 module.exports = rmFile
+module.exports.sync = rmFileSync
