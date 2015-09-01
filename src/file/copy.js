7ad53d461e44f047d2bd2ee7860fa08d3e83686e
akileez <git@keeda.net>
2015-09-01 17:08:59 -0400

    update file/copy.js
    add async version. Need to redo this. copying over entire directory
    structure. I just want the files. will re-visit when I am in a better
    state of mind.

--- src/file/copy.js
+++ src/file/copy.js
@@ -1,10 +1,28 @@
 var path      = require('path')
+var assert    = require('assert')
 var readFile  = require('./readFile')
 var writeFile = require('./writeFile')
+var eachAsync = require('../async/iterate').each
 
-// make async version
+// THIS NEEDS WORK, but not today.
+function copy (files, dest, cb) {
+  files = Array.isArray(files) ? files : [files]
 
-function copy (files, dest) {
+  eachAsync(files, function (file, key, done) {
+    readFile(file, function (err, data) {
+      assert.ifError(err)
+      writeFile(path.resolve(dest, file), data, function (err) {
+        assert.ifError(err)
+        done(null, 'File ' + file + 'copied to ' + dest)
+      })
+    })
+  }, function (err, res) {
+    assert.ifError(err)
+    cb(res)
+  })
+}
+
+function copySync (files, dest) {
   files = Array.isArray(files) ? files : [files]
 
   files.forEach(function (file) {
@@ -15,3 +33,4 @@ function copy (files, dest) {
 }
 
 module.exports = copy
+module.exports.sync = copySync
