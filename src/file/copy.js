84e7b1f9d1a4e9e8d5c406dd810dc34c685dc59b
akileez <git@keeda.net>
2015-09-02 19:58:58 -0400

    update file/copy.js
    using segments.fromFirst. this will remove the root directory of the
    file to be copied allowing the directory structure to be preserved.
    need to work in options to flatten dir structure and no clobber to
    avoid overwriting files of the same name.

--- src/file/copy.js
+++ src/file/copy.js
@@ -5,14 +5,15 @@ var writeFile = require('./writeFile')
 var segments  = require('../path/segments')
 var eachAsync = require('../async/iterate').map
 
-// THIS NEEDS WORK, but not today.
+// THIS NEEDS WORK!!
+// options: flatten, preserve [dir structure], noclobber.
 function copy (files, dest, cb) {
   files = Array.isArray(files) ? files : [files]
 
   eachAsync(files, function (file, key, done) {
     readFile(file, function (err, data) {
       assert.ifError(err)
-      writeFile(path.resolve(dest, segments.last(file)), data, function (err) {
+      writeFile(path.resolve(dest, segments.fromFirst(file)), data, function (err) {
         assert.ifError(err)
         done(null, 'File ' + file + ' copied to ' + dest)
       })
@@ -27,7 +28,7 @@ function copySync (files, dest) {
   files = Array.isArray(files) ? files : [files]
 
   files.forEach(function (file) {
-    var destFile = path.resolve(dest, segments.last(file))
+    var destFile = path.resolve(dest, segments.fromFirst(file))
     var content = readFile(file)
     writeFile(destFile, content)
   })
