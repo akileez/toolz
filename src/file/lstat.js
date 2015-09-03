441ce64162ce3b33a6ea351746954062adc248a6
akileez <git@keeda.net>
2015-09-03 12:02:52 -0400

    bug fix -- typo

--- src/file/lstat.js
+++ src/file/lstat.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var modStats = require('./modStats')
 
 function lstat (fp, cb) {
-  if (arguments.length === 1) return modStats(fs.statSync(fp), fp)
+  if (arguments.length === 1) return modStats(fs.lstatSync(fp), fp)
   fs.lstat(fp, function (err, stats) {
     cb(err, stats ? modStats(stats, fp) : stats)
   })
