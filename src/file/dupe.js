5c4d8bc44344a56eff2cabb3148bdfa652163cff
akileez <git@keeda.net>
2015-09-02 20:32:52 -0400

    revert file/dupe.js back to original

--- src/file/dupe.js
+++ src/file/dupe.js
@@ -1,12 +1,8 @@
-var readFile = require('./readFile').stream
-var writeFile = require('./writeFile').stream
-
-// function dupe (from, to) {
-//   fs.createReadStream(from).pipe(fs.createWriteStream(to))
-// }
+// Duplicate a file within the same directory
+var fs = require('fs')
 
 function dupe (from, to) {
-  readFile(from).pipe(writeFile(to))
+  fs.createReadStream(from).pipe(fs.createWriteStream(to))
 }
 
 module.exports = dupe
