de1fddec1b0b419b7a19e9376374440489ff88d0
akileez <git@keeda.net>
2015-09-05 18:36:45 -0400

    rename file object collection
    assignValue.js —> assign.js

--- src/object/assign.js
+++ src/object/assign.js
@@ -1,17 +1,23 @@
+// adopted from: <https://github.com/jonschlinkert/assign-value>
+// Copyright (c) 2015, Jon Schlinkert. (MIT)
+
 var isObject = require('../lang/isObject')
 var extend = require('./extend')
 var get = require('./get')
 var set = require('./set')
 
-// Copyright (c) 2015, Jon Schlinkert. <https://github.com/jonschlinkert/assign-value> (MIT)
-
 function assignValue (obj, prop, value) {
   if (!isObject(obj))
     throw new TypeError('assignValue expects first arg to be an object')
-  if (typeof prop === 'undefined' && typeof value === 'undefined') return obj
-  if (typeof value === 'undefined' && isObject(prop)) return extend(obj, prop)
+
+  if (typeof prop === 'undefined' && typeof value === 'undefined')
+    return obj
+
+  if (typeof value === 'undefined' && isObject(prop))
+    return extend(obj, prop)
 
   set(obj, prop, extend({}, get(obj, prop), value))
+
   return obj
 }
 
