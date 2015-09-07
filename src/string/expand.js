0b62583ee2747dcc43a9c8efaade5423eb51debc
akileez <git@keeda.net>
2015-09-07 18:01:26 -0400

    refactor string/expand.js
    removed resolveArray —> not needed. Object.keys can handle
    replaced get() with look()

--- src/string/expand.js
+++ src/string/expand.js
@@ -2,16 +2,16 @@
 // on string replacements within objects/arrays/strings.
 
 var kindOf = require('../lang/kindOf')
-var get = require('../object/get')
+var look   = require('../object/look')
 
 function interpolate (template, replacements, syntax) {
   return resolve(template, replacements || template, syntax)
 
   function resolve (template, data, opts) {
-    switch(kindOf(template)) {
-      case 'string': return resolveString(template, data, opts)
+    switch (kindOf(template)) {
+      case 'array' :
       case 'object': return resolveObject(template, data, opts)
-      case 'array' : return resolveArray(template, data, opts)
+      case 'string': return resolveString(template, data, opts)
       default      : return template
     }
   }
@@ -23,18 +23,11 @@ function interpolate (template, replacements, syntax) {
     return obj
   }
 
-  function resolveArray (arr, data, opts) {
-    var len = arr.length
-    var i = - 1
-    while (++i < len) arr[i] = resolve(arr[i], data, opts)
-    return arr
-  }
-
   function resolveString (str, data, opts) {
     var stache = opts || /\{\{([^\}]+)\}\}/g // mustache-like
 
     function replaceFn (match, prop) {
-      var template = String(get(data, prop))
+      var template = look(data, prop)
 
       return stache.test(template)
         ? template.replace(stache, replaceFn)
