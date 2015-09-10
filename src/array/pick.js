619cab89f4a3a5e82074f8c6ca21fd7b4601e28f
akileez <git@keeda.net>
2015-09-10 18:20:22 -0400

    update array/pick.js
    incorporated picking a random array item

--- src/array/pick.js
+++ src/array/pick.js
@@ -1,4 +1,5 @@
 var slice = require('./sliced')
+var randInt = require('../random/randInt')
 
 /**
  *  var arr = [1,2,3,4,5,6,7,8]
@@ -10,6 +11,8 @@ var slice = require('./sliced')
  *  pick.before(arr, 4) // [ 1, 2, 3, 4 ]
  *  pick.after(arr, 3)  // [ 4, 5, 6, 7, 8 ]
  *  pick.between(arr, 1, 3) // [ 2, 3 ]
+ *  pick.rand(arr)      // [ 4 ]
+ *  pick.rand(arr, 3)   // [ 8, 3, 6 ]
  */
 
 // Returns the first item, or first `n` items of an array.
@@ -37,8 +40,28 @@ function between (arr, strt, end) {
   return slice(arr, strt || 0, end || arr.length - 1)
 }
 
+function rand (arr, num) {
+  if (num != null) {
+    var result = []
+    if (num > 0 && arr && arr.length) {
+      num = num > arr.length ? arr.length : num
+      while (num--) {
+        result.push(pickOne(arr))
+      }
+    }
+    return result
+  }
+  return (arr && arr.length) ? pickOne(arr) : void (0)
+}
+
+function pickOne (arr) {
+  var idx = randInt(0, arr.length - 1)
+  return arr.splice(idx, 1)[0]
+}
+
 exports.first   = first
 exports.last    = last
 exports.before  = before
 exports.after   = after
 exports.between = between
+exports.rand    = rand
