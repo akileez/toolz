2095ba5b8e0abf42cc5f7cc0e88a56a6c203a217
akileez <git@keeda.net>
2015-09-14 02:13:43 -0400

    update object/omit.js
    adds a list of properties to delete.

--- src/object/omit.js
+++ src/object/omit.js
@@ -1,8 +1,8 @@
-var toArray = require('../lang/toArray')
 var clone   = require('../lang/clone')
+var slice   = require('../array/sliced')
 
 function omit (obj, toOmit) {
-  toOmit = toArray(toOmit)
+  toOmit = Array.isArray(toOmit) ? toOmit : slice(arguments, 1)
   var output = clone(obj)
   toOmit.forEach(function (omit) {
     delete output[omit]
