34673a84ad3deacb01b0506f1b526a5d2eb7cf7c
akileez <git@keeda.net>
2015-09-15 06:54:36 -0400

    update string/expander.js
     <https://github.com/jonschlinkert/expand> refactored.

--- src/string/expander.js
+++ src/string/expander.js
@@ -1,93 +1,109 @@
 // adopted from: https://github.com/jonschlinkert/expand
 // Copyright (c) 2015, Jon Schlinkert (MIT)
 
-var get = require('../object/get')
 var kindOf = require('../lang/kindOf')
-var isPrimitive = require('../lang/isPrimitive')
+var look   = require('../object/look')
 
-var cache = {prev: null}
+// Curried function:
+// var expand = require('expand') or require('expand')()
+// var fn = expand({regex: /abc/})
+// fn('{{a}}', {a: 'apple'}) or expand('{{a}}', {a: 'apple'})
 
-function expand (val, data, opts) {
-  return resolve(val, data || val, opts)
-}
+function expand (opts) {
+  opts = opts || {}
+  var regex = opts.regex || /\{\{([^\}]+)\}\}/
 
-function resolve (val, data, opts) {
-  switch (kindOf(val)) {
-    case 'array' :
-    case 'object': return resolveObject(val, data, opts)
-    case 'string': return resolveString(val, data, opts)
-    default      : return val
-  }
-}
+  function resolve (val, data) {
+    data = data || val
 
-function resolveObject (obj, data, opts) {
-  Object.keys(obj).forEach(function (key, idx, arr) {
-    obj[key] = resolve(obj[key], data, opts)
-  })
-
-  return obj
-}
-
-function resolveString (str, data, opts) {
-  // pass a regex as an option. not an object.
-  var regex = opts || /\{\{([^\}]+)\}\}/g
-
-  if (!regex.test(str)) return str
-  var result = str
-
-  str.replace(regex, function (match, es6, erb, index) {
-    var prop = trim(es6 || erb)
-    var val
-
-    if (kindOf(erb) === 'number') index = erb
-
-    // return if prop is undefined
-    if (!prop) return
-
-    // if prop is a function try and execute it. This is a hack!!
-    // Not using method from https://github.com/jonschlinkert/expand.
-    // this does not cover all cases as with above. But then again,
-    // I have no to little use calling functions in this type
-    // of situation
-    if (/[()]/.test(prop)) {
-      var firstparens = prop.indexOf('(')
-      var lastparens = prop.indexOf(')')
-      var exp = prop.slice(0, firstparens)
-      var expResult = prop.slice(firstparens + 1, lastparens)
-      val = typeof data[exp] === 'function'
-        ? data[exp](data[expResult])
-        : resolve('{{' + exp + '}}', data, opts)(expResult)
-    } else {
-      val = data[prop] || get(data, prop)
+    switch (kindOf(val)) {
+      case 'array'  :
+      case 'object' : return resolveObject(val, data)
+      case 'string' : return resolveString(val, data)
+      default       : return val
     }
+  }
 
-    if (isPrimitive(val)) {
-      if (kindOf(index) !== 'number') index = str.indexOf(match)
+  function resolveObject (obj, data) {
+    Object.keys(obj).forEach(function (key, idx, arr) {
+      obj[key] = resolve(obj[key], data)
+    })
+
+    return obj
+  }
+
+  function resolveString (str, data) {
+    var m
+
+    while (m = regex.exec(str)) {
+      var orig = str
+      var prop = (m[1] || m[2] || '').trim()
+      var match = m[0]
+
+      if (!match) return str
 
       var len = match.length
-      if (str.length > len) {
-        var head = str.slice(0, index)
-        var tail = str.slice(index + len)
-        result = head + val + tail
-      } else {
-        result = val
-      }
-    } else if (val) {
-      // prevent inifinte loops
-      if (result === cache.prev) return
+      var i = m.index
 
-      cache.prev = result
-      result = resolve(val, data, opts)
+      var val = match
+
+      if (/[()]/.test(prop)) {
+        val = resolveFunc(prop, data)
+      } else {
+        val = look(data, prop)
+      }
+
+      // if no value was retured from `look` or `resolveFunc`,
+      // reset the value to `match`
+      val = val || match
+
+
+      if (typeof val === 'object') {
+        // if the value is an object, recurse to resolve
+        // templates in the keys and values of the object
+        val = resolve(val, data)
+      } else if (typeof val !== 'function') {
+        // ensure we have a string. numbers are the most
+        // likely thing to blow this up at this point
+        val = val.toString()
+      }
+
+      if (typeof val === 'function') return val.bind(data)
+
+      // could be an array, object, etc. if so, just
+      // break and return the value. we could add one
+      // more `resolve` here if there is a reason to
+      if (typeof val !== 'string') {
+        str = val
+        break
+      }
+
+      var head = str.slice(0, i)
+      var tail = str.slice(i + len)
+
+      str = head + val + tail
+      if (str === orig) break
     }
 
-    if (typeof result === 'string') result = resolveString(result, data, opts)
-  })
+    return str
+  }
 
-  return result
-}
+  function resolveFunc (prop, data) {
+    // This is a hack!! this does not cover all cases as with
+    // https://github.com/jonschlinkert/expand. But then again,
+    // I have no to little use calling functions in this type
+    // of situation
+    var firstparens = prop.indexOf('(')
+    var lastparens = prop.indexOf(')')
+    var expr = prop.slice(0, firstparens)
+    var exprParam = prop.slice(firstparens + 1, lastparens)
 
-function trim (str) {
-  return str == null ? '' : String(str).trim()
+    return typeof data[expr] === 'function'
+      ? data[expr](data[exprParam])
+      : resolve('{{' + expr + '}}', data)(exprParam)
+  }
+
+  return resolve
 }
 
 module.exports = expand
