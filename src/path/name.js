bc23dd6ccc6d48e887f04db172895b5345356413
akileez <git@keeda.net>
2015-09-17 09:24:39 -0400

    update path/name.js
    replicate path.parse so that the `root` key gives the current working
    directory

--- src/path/name.js
+++ src/path/name.js
@@ -1,5 +1,9 @@
-var path = require('path')
+var path   = require('path')
 var isFile = require('../file/isFile')
+
+function root () {
+  return process.cwd().split(/[\\\/]/g).slice(-1)[0]
+}
 
 function extname (fp) {
   return path.extname(fp)
@@ -19,7 +23,19 @@ function filename (fp) {
   return path.basename(fp, path.extname(fp))
 }
 
-exports.file = filename
-exports.base = basename
-exports.dir  = dirname
-exports.ext  = extname
+function parse (fp) {
+  return {
+    root : root(),
+    dir  : dirname(fp),
+    base : basename(fp),
+    ext  : extname(fp),
+    name : filename(fp)
+  }
+}
+
+exports.file  = filename
+exports.base  = basename
+exports.dir   = dirname
+exports.ext   = extname
+exports.root  = root
+exports.parse = parse
