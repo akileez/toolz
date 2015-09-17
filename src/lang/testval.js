00bfdc35c239ee0b83c5bfedd1e941355ee96fb7
akileez <git@keeda.net>
2015-09-17 08:00:48 -0400

    source file -- lang/testval.js

---
+++ src/lang/testval.js
@@ -0,0 +1,58 @@
+// adopted from: test-value <https://github.com/75lb/test-value>
+// Copyright (c) 2015 Lloyd Brookes <75pound@gmail.com> (MIT)
+
+var toArray       = require('./toArray')
+var isObject      = require('./isObject')
+var isPlainObject = require('./isPlainObject')
+
+function testValue (val, test) {
+  if (isPlainObject(test) && isObject(val)) {
+    return Object.keys(test).every(function (prop) {
+      var queryValue = test[prop]
+
+      var isNegated = false
+      var isContains = false
+
+      // get flags
+      if (prop.charAt(0) === '!') isNegated = true
+      else if (prop.charAt(0) === '+') isContains = true
+
+      // strip flag char
+      prop = (isNegated || isContains) ? prop.slice(1) : prop
+      var objectValue = val[prop]
+
+      if (isContains) {
+        queryValue  = toArray(queryValue)
+        objectValue = toArray(objectValue)
+      }
+
+      var result = testValue(objectValue, queryValue)
+
+      return isNegated ? !result : result
+    })
+
+  } else if (Array.isArray(test)) {
+    var tests = test
+    if (!Array.isArray(val)) val = [val]
+
+    return val.some(function (value) {
+      return tests.some(function (test) {
+        return testValue(value, test)
+      })
+    })
+
+  } else if (test instanceof RegExp) {
+    // regexes queries will always return `false` for `null`, `undefined`, `NaN`.
+    // This is to prevent a query like `/.+/` matching the string `undefined`.
+    if (['boolean', 'string', 'number'].indexOf(typeof val) === -1) return false
+    else return test.test(val)
+
+  } else if (typeof test === 'function') {
+    return test(val)
+
+  } else {
+    return test === val
+  }
+}
+
+module.exports = testValue
