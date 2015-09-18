6749ffa8c06ecfab48875adee467e7ff43effdae
akileez <git@keeda.net>
2015-09-18 13:49:15 -0400

    update array/where,js
    add comment

--- src/array/where.js
+++ src/array/where.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015 Lloyd Brookes <75pound@gmail.com> (MIT)
 // https://github.com/75lb/array-tools
 
+// Deep query an array
+
 var toArray = require('../lang/toArray')
 var testval = require('../lang/testval')
 
