b3517f31108fa75328c4ad41fec3072c2502f354
akileez <git@keeda.net>
2015-09-19 00:15:31 -0400

    source file math/binomial.js

---
+++ src/math/binomial.js
@@ -0,0 +1,17 @@
+function binomial (n, k) {
+  var a = []
+
+  function binome(n, k) {
+    if (n >= 0 && k === 0) return 1
+    if (n === 0 && k > 0) return 0
+    if (a[n] && a[n][k] > 0) return a[n][k]
+    if (!a[n]) a[n] = []
+
+    a[n][k] = binome(n - 1, k - 1) + binome(n - 1, k)
+    return a[n][k]
+  }
+
+  return binome(n, k)
+}
+
+module.exports = binomial
