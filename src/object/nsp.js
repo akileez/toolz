74fc534cae4642fb98576592e2d96fee077998c0
akileez <git@keeda.net>
2015-09-21 13:45:37 -0400

    update object/nsp.js
    really, this is the final version!!

--- src/object/nsp.js
+++ src/object/nsp.js
@@ -1,9 +1,10 @@
 // namespace a value and assign to a given object.
 // convenience function. I tend to do this often.
 
-// set a top-level (flatten) object property
-// differs from `set` in that `obj` is optional and does
-// not create nested properties
+// Oh well, required `set`... so this is super `set`
+// `obj` is optional. returns modified obj
+
+var set = require('./set')
 
 function nsp (obj, key, val) {
   if (!key && !val) return obj
@@ -13,7 +14,12 @@ function nsp (obj, key, val) {
     obj = {}
   }
 
-  obj[key] = val
+  if (key.indexOf('.') > -1) {
+    set(obj, key, val)
+  } else {
+    obj[key] = val
+  }
+
   return obj
 }
 
