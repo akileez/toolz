49c84b9fab0ff97aeb9659fbef844f685e780f6e
akileez <git@keeda.net>
2015-09-21 12:01:41 -0400

    update object/nsp.js
    add flexibility

--- src/object/nsp.js
+++ src/object/nsp.js
@@ -3,6 +3,13 @@
 // enhance as necessary.
 
 function nsp (obj, key, val) {
+  if (!key && !val) return obj
+  if (arguments.length === 2 && val == null) {
+    val = key
+    key = obj
+    obj = {}
+  }
+
   obj[key] = val
   return obj
 }
