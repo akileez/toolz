17c62caf824c401481eaca83d3e2dd0209cab8e2
akileez <git@keeda.net>
2015-09-21 11:16:28 -0400

    source files - math collections
    base package
    stats package

---
+++ src/math/base.js
@@ -0,0 +1,10 @@
+// base Math package
+
+exports.add = require('./add')
+exports.sub = require('./subtract')
+exports.sqr = require('./sqr')
+exports.tmx = require('./multiply')
+exports.bio = require('./binomial')
+exports.fac = require('./factorial')
+exports.gcd = require('./gcd')
+exports.lcm = require('./lcm')
---
+++ src/math/stats.js
@@ -0,0 +1,8 @@
+// base statistics package
+
+exports.mean = require('./mean')
+exports.mode = require('./mode')
+exports.median = require('./median')
+exports.stdDev = require('./standardDev')
+exports.report = require('./report')
+exports.quantile = require('./quantile')
