19d7a79059e50e72a034935df0890dec5ae35559
akileez <git@keeda.net>
2015-09-21 13:58:07 -0400

    update object/nsp.js
    handle condition where object (variable) is undefined

--- src/object/nsp.js
+++ src/object/nsp.js
@@ -1,18 +1,18 @@
 // namespace a value and assign to a given object.
-// convenience function. I tend to do this often.
-
-// Oh well, required `set`... so this is super `set`
 // `obj` is optional. returns modified obj
 
 var set = require('./set')
 
 function nsp (obj, key, val) {
   if (!key && !val) return obj
+
   if (val == null) {
     val = key
     key = obj
     obj = {}
   }
+
+  if (obj === undefined) obj = {}
 
   if (key.indexOf('.') > -1) {
     set(obj, key, val)
