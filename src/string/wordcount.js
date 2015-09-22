14a7a45dabdd779846d154e20a31704e5e028cc6
akileez <git@keeda.net>
2015-09-22 07:50:35 -0400

    source file string/wordcount.js
    refactor in accordance with Jon Schlinkert -- wordcount
    <https://github.com/jonschlinkert/wordcount>

---
+++ src/string/wordcount.js
@@ -0,0 +1,23 @@
+// concept adopted from: https://github.com/lepture/editor/blob/master/src/intro.js#L343
+// and Jon Schlinkert -- wordcount <https://github.com/jonschlinkert/wordcount>
+
+var matches = require('./wordmatch')
+
+function wordcount (str) {
+  var n = matches(str)
+  var count = 0
+
+  if (n === null) return count
+
+  var i = -1
+  var len = n.length
+
+  while (++i < len) {
+    if (n[i].charCodeAt(0) >= 0x4E00) count += n[i].length
+    else count += 1
+  }
+
+  return count
+}
+
+module.exports = wordcount
