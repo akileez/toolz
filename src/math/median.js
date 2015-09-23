ce6b8a776bdf8b0a1593eab34a90fb23c2b218c5
akileez <git@keeda.net>
2015-09-23 19:15:54 -0400

    update math/median.js
    add math/clean.js

--- src/math/median.js
+++ src/math/median.js
@@ -1,9 +1,10 @@
 // Calculate the median value of a set of numbers in array.
 
 var quantile = require('./quantile')
+var clean = require('./clean')
 
 function median (arr) {
-  return quantile(arr, 1, 2)
+  return quantile(clean(arr), 1, 2)
 }
 
 module.exports = median
