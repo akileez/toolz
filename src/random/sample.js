6a12171e9d773e64360f03f0aaa317e86636b580
akileez <git@keeda.net>
2015-09-24 09:42:48 -0400

    update random/sample.js
    add decimal precision

--- src/random/sample.js
+++ src/random/sample.js
@@ -1,17 +1,26 @@
 // Return a random sample of values over a set of bounds with
 // a specified quantity.
-// add options for integer output or decimal percision..
 
 var rand = require('./rand')
 var randInt = require('./randInt')
 var map = require('../array/map')
+var precise = require('../number/enforcePrecision')
 
-function sample (lower, upper, num) {
-  var sampl = []
-  sampl.length = num
+function sampleNumbers (lower, upper, num) {
+  var sample = []
+  sample.length = num
 
-  return map(sampl, function (val, key) {
+  return map(sample, function (val, key) {
     return rand(lower, upper)
+  })
+}
+
+function samplePrecision (lower, upper, num, precision) {
+  var sample = []
+  sample.length = num
+
+  return map(sample, function (val, key) {
+    return precise(rand(lower, upper), precision || 3)
   })
 }
 
@@ -24,5 +33,6 @@ function sampleIntegers (lower, upper, num) {
   })
 }
 
-module.exports = sample
+module.exports = sampleNumbers
 module.exports.int = sampleIntegers
+module.exports.precise = samplePrecision
