7cf0c48aa792ad6bf101e62146f6763ba8ff98c8
akileez <git@keeda.net>
2015-09-24 08:50:59 -0400

    source file math/divMod.js

---
+++ src/math/divMod.js
@@ -0,0 +1,12 @@
+// Calculate the divisor and modulus of two integers
+
+var isInteger = require('../lang/isInteger')
+
+function divMod (a, b) {
+  if (b <= 0) throw new Error('b cannot be zero. Undefined')
+  if (!isInteger(a) || !isInteger(b)) throw new Error('A or B are not integers')
+
+  return [Math.floor(a / b), a % b]
+}
+
+module.exports = divMod
