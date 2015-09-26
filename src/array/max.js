79a4eb291ea78c9d77421023b8326dee69a740e2
akileez <git@keeda.net>
2015-09-26 00:21:14 -0400

    reformat array/max.js

--- src/array/max.js
+++ src/array/max.js
@@ -1,26 +1,35 @@
 var makeIterator = require('../function/makeIterator_')
 
 function max (arr, iterator, thisObj) {
-  if (arr == null || !arr.length) return Infinity
-  else if (arr.length && !iterator) return Math.max.apply(Math, arr)
-  else {
-    iterator = makeIterator(iterator, thisObj)
-    var result
-    var compare = -Infinity
-    var value
-    var temp
-    var i = -1
-    var len = arr.length
-    while (i++ < len) {
-      value = arr[i]
-      temp = iterator(value, i, arr)
-      if (temp > compare) {
-        compare = temp
-        result = value
-      }
-    }
-    return result
+  if (arr == null || !arr.length) {
+    return Infinity
   }
+
+  if (arr.length && !iterator) {
+    return Math.max.apply(Math, arr)
+  }
+
+  iterator = makeIterator(iterator, thisObj)
+
+  var compare = -Infinity
+  var value
+  var temp
+  var result
+
+  var i = -1
+  var len = arr.length
+
+  while (i++ < len) {
+    value = arr[i]
+    temp = iterator(value, i, arr)
+
+    if (temp > compare) {
+      compare = temp
+      result = value
+    }
+  }
+
+  return result
 }
 
 module.exports = max
