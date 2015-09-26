cf2b7e0e8fe32cbc8743a17cee7cb9473ac8d6f2
akileez <git@keeda.net>
2015-09-26 00:23:22 -0400

    reformat math/divMod.js

--- src/math/divMod.js
+++ src/math/divMod.js
@@ -3,8 +3,13 @@
 var isInteger = require('../lang/isInteger')
 
 function divMod (a, b) {
-  if (b <= 0) throw new Error('b cannot be zero. Undefined')
-  if (!isInteger(a) || !isInteger(b)) throw new Error('A or B are not integers')
+  if (b <= 0) {
+    throw new Error('b cannot be zero. Undefined')
+  }
+
+  if (!isInteger(a) || !isInteger(b)) {
+    throw new Error('A or B are not integers')
+  }
 
   return [Math.floor(a / b), a % b]
 }
