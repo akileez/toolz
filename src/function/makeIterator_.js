829f3e15c7566bf5c0b95fdf251c1d7d9ea4f997
akileez <git@keeda.net>
2015-09-26 00:20:56 -0400

    minor formatting -- function/makeIterator_.js

--- src/function/makeIterator_.js
+++ src/function/makeIterator_.js
@@ -6,9 +6,11 @@ function makeIterator (src, thisObj) {
   if (src == null) return identity
   switch (typeof src) {
     case 'function' :
-      return (typeof thisObj !== 'undefined') ? function (value, idx, arr) {
-        return src.call(thisObj, value, idx, arr)
-      } : src
+      return (typeof thisObj !== 'undefined')
+        ? function (value, idx, arr) {
+            return src.call(thisObj, value, idx, arr)
+          }
+        : src
     case 'object' :
       return function (value) {
         return deepMatches(value, src)
