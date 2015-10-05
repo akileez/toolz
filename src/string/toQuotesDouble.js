77d51df025e816d33248329e47f54d0c5407048e
akileez <git@keeda.net>
2015-10-05 19:20:15 -0400

    source file string/toQuotesDouble.js
    from sindresorhus

---
+++ src/string/toQuotesDouble.js
@@ -0,0 +1,19 @@
+var replace = require('../regex/replace2')
+
+function doubleQuotes (str) {
+  var re = /(?:\\*)?'([^'\\]*\\.)*[^']*'/g
+
+  return replace(str, re, function (match) {
+    return match
+      // unescape single-quotes
+      .replace(/\\'/g, '\'')
+      // escape escapes
+      .replace(/(^|[^\\])(\\+)"/g, '$1$2\\\"')
+      // escape double-quotes
+      .replace(/([^\\])"/g, '$1\\\"')
+      // convert
+      .replace(/^'|'$/g, '"');
+  })
+}
+
+module.exports = doubleQuotes
