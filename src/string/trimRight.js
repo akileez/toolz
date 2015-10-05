d553a7aab89ae747fb5e939cfd634c9b80687480
akileez <git@keeda.net>
2015-10-05 19:21:06 -0400

    source file string/trimRight.js

---
+++ src/string/trimRight.js
@@ -0,0 +1,18 @@
+// adopted from: trim-right <sindresorhus/trim-right>
+// Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com) (MIT)
+// Similar to String#trim() but removes only whitespace on the right
+
+var contains = require('../regex/contains')
+
+function trimRight (str) {
+  var tail = str.length
+  var re = /[\s\uFEFF\u00A0]/
+
+  while (contains(re, str[tail - 1])) {
+    tail--
+  }
+
+  return str.slice(0, tail)
+}
+
+module.exports = trimLeft
