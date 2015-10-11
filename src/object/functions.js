d7dcb390701ea28628b474d041e047c0186cd427
akileez <git@keeda.net>
2015-10-11 15:10:38 -0400

    update object/functions.js
    remove ronin function call. just use for-in loop

--- src/object/functions.js
+++ src/object/functions.js
@@ -1,13 +1,14 @@
-var forIn = require('./forIn')
-
 // return a list of all enummerable properties that have function values
 
 function functions (obj) {
-  var keys = []
-  forIn(obj, function (value, key) {
-    if (typeof value === 'function') keys.push(key)
-  })
-  return keys.sort()
+  var result = []
+  var key
+
+  for (key in obj) {
+    if (typeof key === 'function') result.push(key)
+  }
+
+  return result.sort()
 }
 
 module.exports = functions
