aa85a993a9c98f508280e8fa8dac60535573a44b
akileez <git@keeda.net>
2015-10-11 15:06:51 -0400

    update object/unset.js
    consistency across functions get, look and unset

--- src/object/unset.js
+++ src/object/unset.js
@@ -4,10 +4,11 @@ var has = require('./has')
 
 function unset (obj, prop) {
   if (has(obj, prop)) {
-    var parts = prop.split('.')
-    var last = parts.pop()
-    while ((prop = parts.shift())) {
-      obj = obj[prop]
+    var segs = prop.split('.')
+    var last = segs.pop()
+
+    while (segs.length) {
+      obj = obj[segs.shift()]
     }
     return (delete obj[last])
   } else {
