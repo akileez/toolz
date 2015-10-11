58de9cbda98182040bf0ff226213adf874c29716
akileez <git@keeda.net>
2015-10-11 14:07:55 -0400

    refactor: object/forOwn.js
    re-write of object tool set

--- src/object/forOwn.js
+++ src/object/forOwn.js
@@ -1,10 +1,19 @@
-var forIn = require('./forIn')
-var hasOwn = require('./hasOwn')
-
 function forOwn (obj, fn, thisObj) {
-  forIn(obj, function (value, key) {
-    if (hasOwn(obj, key)) return fn.call(thisObj, obj[key], key, obj)
-  })
+  var key
+
+  if (thisObj === undefined) {
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        fn(obj[key], key, obj)
+      }
+    }
+  } else {
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        fn.call(thisObj, obj[key], key, obj)
+      }
+    }
+  }
 }
 
 module.exports = forOwn
