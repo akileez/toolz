464b0051260425fa8d5da9a27fe6fa3cc602344a
akileez <git@keeda.net>
2015-10-12 02:33:48 -0400

    format array/indicesOf.js

--- src/array/indicesOf.js
+++ src/array/indicesOf.js
@@ -2,10 +2,14 @@ function indicesOf (arr, item, fromIndex) {
   var results = []
   if (arr == null) return results
 
-  fromIndex = typeof fromIndex === 'number' ? fromIndex : 0
+  fromIndex = typeof fromIndex === 'number'
+    ? fromIndex
+    : 0
 
   var len = arr.length
-  var cursor = fromIndex >= 0 ? fromIndex : len + fromIndex
+  var cursor = fromIndex >= 0
+    ? fromIndex
+    : len + fromIndex
 
   while (cursor < len) {
     if (arr[cursor] === item) results.push(cursor)
