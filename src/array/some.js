32b143751912e0f513e21bfa09b5c3301d632a59
akileez <git@keeda.net>
2015-10-12 14:51:31 -0400

    remove makeIterator: array/some.js

--- src/array/some.js
+++ src/array/some.js
@@ -1,18 +1,17 @@
-var makeIterator = require('../function/makeIterator_')
-
-function some (arr, fn, thisObj) {
-  fn = makeIterator(fn, thisObj)
+function some (arr, fn) {
   var result = false
   if (arr == null) return result
 
   var i = -1
   var len = arr.length
+
   while (++i < len) {
     if (fn(arr[i], i, arr)) {
       result = true
       break
     }
   }
+
   return result
 }
 
