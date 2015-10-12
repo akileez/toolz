25a7cd4d75d4fa22e22ade51f1aa09890ec38523
akileez <git@keeda.net>
2015-10-12 02:25:49 -0400

    update array/collect.js
    remove makeIterator

--- src/array/collect.js
+++ src/array/collect.js
@@ -1,18 +1,18 @@
-var makeIterator = require('../function/makeIterator_')
 var append = require('./append')
 
-function collect (arr, fn, thisObj) {
-  fn = makeIterator(fn, thisObj)
-  var results = []
-  if (arr == null) return results
+function collect (arr, fn) {
+  var result = []
+  if (arr == null) return result
 
+  var val
   var i = -1
   var len = arr.length
+
   while (++i < len) {
-    var value = fn(arr[i], i, arr)
-    if (value != null) append(results, value)
+    val = fn(arr[i], i, arr)
+    if (val != null) append(result, val)
   }
-  return results
+  return result
 }
 
 module.exports = collect
