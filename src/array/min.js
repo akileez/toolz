5e12c435463cd76c62a13c1784d627bff8145a1a
akileez <git@keeda.net>
2015-10-12 02:35:17 -0400

    update array/min.js
    remove makeIterator

--- src/array/min.js
+++ src/array/min.js
@@ -1,26 +1,31 @@
-var makeIterator = require('../function/makeIterator_')
-
-function min (arr, iterator, thisObj) {
-  if (arr == null || !arr.length) return -Infinity
-  else if (arr.length && !iterator) return Math.min.apply(Math, arr)
-  else {
-    iterator = makeIterator(iterator, thisObj)
-    var result
-    var compare = Infinity
-    var value
-    var temp
-    var i = -1
-    var len = arr.length
-    while (i++ < len) {
-      value = arr[i]
-      temp = iterator(value, i, arr)
-      if (temp < compare) {
-        compare = temp
-        result = value
-      }
-    }
-    return result
+function min (arr, fn) {
+  if (arr == null || !arr.length) {
+    return -Infinity
   }
+
+  if (arr.length && !fn) {
+    return Math.min.apply(Math, arr)
+  }
+
+  var compare = Infinity
+  var value
+  var temp
+  var result
+
+  var i = -1
+  var len = arr.length
+
+  while (i++ < len) {
+    value = arr[i]
+    temp = fn(value, i, arr)
+
+    if (temp < compare) {
+      compare = temp
+      result = value
+    }
+  }
+
+  return result
 }
 
 module.exports = min
