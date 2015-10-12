e45b3fc59a0040cc7820a4330ca87a138e6c5b99
akileez <git@keeda.net>
2015-10-12 02:24:39 -0400

    reformat array/append.js
    no other changes

--- src/array/append.js
+++ src/array/append.js
@@ -1,10 +1,12 @@
 function append (arr1, arr2) {
   if (arr2 == null) return arr1
+
   var len1 = arr1.length
   var len2 = arr2.length
-  var iter = -1
-  while (++iter < len2) {
-    arr1[len1 + iter] = arr2[iter]
+  var i = -1
+
+  while (++i < len2) {
+    arr1[len1 + i] = arr2[i]
   }
   return arr1
 }
