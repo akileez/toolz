85b839fbce6c028a6b8071c765802ecc24df9615
akileez <git@keeda.net>
2015-10-12 02:33:27 -0400

    format array/indexOf.js

--- src/array/indexOf.js
+++ src/array/indexOf.js
@@ -1,8 +1,12 @@
 function indexOf (arr, item, fromIndex) {
   fromIndex = fromIndex || 0
   if (arr == null) return -1
+
   var len = arr.length
-  var iter = fromIndex < 0 ? len + fromIndex : fromIndex
+  var iter = fromIndex < 0
+    ? len + fromIndex
+    : fromIndex
+
   while (iter < len) {
     if (arr[iter] === item) return iter
     iter++
