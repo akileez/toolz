76bc4480f897dd9e7a96e2aefd7b04a76ff4dd22
akileez <git@keeda.net>
2015-10-12 02:34:40 -0400

    update array/map.js
    remove makeIterator

--- src/array/map.js
+++ src/array/map.js
@@ -1,11 +1,10 @@
-var makeIterator = require('../function/makeIterator_')
-
-function map (arr, fn, thisObj) {
-  fn = makeIterator(fn, thisObj)
+function map (arr, fn) {
   var results = []
   if (arr == null) return results
+
   var idx = -1
   var len = arr.length
+
   while (++idx < len) {
     results[idx] = fn(arr[idx], idx, arr)
   }
