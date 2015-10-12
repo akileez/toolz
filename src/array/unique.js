506f7dd4a3fdc3277730e9f0055936f36fdb5392
akileez <git@keeda.net>
2015-10-12 14:52:05 -0400

    minor edits: array/unique.js

--- src/array/unique.js
+++ src/array/unique.js
@@ -2,11 +2,14 @@ var filter = require('./filter')
 
 function unique (arr, compare) {
   compare = compare || isEqual
+
   return filter(arr, function (item, i, arr) {
     var len = arr.length
+
     while (++i < len) {
       if (compare(item, arr[i])) return false
     }
+
     return true
   })
 }
