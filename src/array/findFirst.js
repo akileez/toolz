583337fb1ddc008b251a0c0da4c916bc36d09c52
akileez <git@keeda.net>
2015-10-13 17:15:03 -0400

    comment array/findFirst.js

--- src/array/findFirst.js
+++ src/array/findFirst.js
@@ -1,4 +1,6 @@
 // Deep query an array but only returns the first item found
+// this differs from array/find where the qurey item
+// can be a string, regex, array, plain object, number
 
 var where = require('./where')
 
