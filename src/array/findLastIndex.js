0316bcb52d5128619ccd992e18d695d57c61b618
akileez <git@keeda.net>
2015-10-13 17:15:53 -0400

    comment array/findLastIndex.js

--- src/array/findLastIndex.js
+++ src/array/findLastIndex.js
@@ -1,3 +1,6 @@
+// Loops through the items in the Array on the reverse order and returns
+// the index of the first one that passes a truth test (callback).
+
 function findLastIndex (arr, fn) {
   if (arr == null) return -1
 
