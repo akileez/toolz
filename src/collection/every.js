040c31cb59a31d2e0ac35af38002fd84a856e93f
akileez <git@keeda.net>
2015-10-13 15:27:56 -0400

    refactor example: collection/every.js
    example of how I plan to use the collection set of functions, restoring
    makeIterator. This will introduce some duplication but hell, I never
    cared too much about that in this library.

--- src/collection/every.js
+++ src/collection/every.js
@@ -1,5 +1,39 @@
 var make = require('./make_')
-var arrEvery = require('../array/every')
-var objEvery = require('../object/every')
+var makeIterator = require('../function/makeIterator_')
+
+function arrEvery (arr, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+
+  var result = true
+  if (arr == null) return result
+
+  var i = -1
+  var len = arr.length
+
+  while (++i < len) {
+    if (!fn(arr[i], i, arr)) {
+      result = false
+      break
+    }
+  }
+  return result
+}
+
+function objEvery (obj, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+
+  var result = true
+  var key
+
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (!fn(obj[key], key, obj)) {
+        result = false
+        break
+      }
+    }
+  }
+  return result
+}
 
 module.exports = make(arrEvery, objEvery)
