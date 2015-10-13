04d2706ff2272da0268f94a2cad448b8abb3170c
akileez <git@keeda.net>
2015-10-13 17:12:42 -0400

    comment array/compactor.js

--- src/array/compactor.js
+++ src/array/compactor.js
@@ -1,6 +1,6 @@
-// my edit to mimic lodash version
-// // Creates an array with all falsey values removed. The values `false`, `null`,
-// `0`, `""`, `undefined`, and `NaN` are falsey.
+// Returns a new Array with all falsey values removed.
+// The values `false`, `null`, `0`, `""`, `undefined`, and `NaN` are falsey.
+
 var filter = require('./filter')
 
 function compact (arr) {
