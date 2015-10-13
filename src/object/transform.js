0c35b3da771f48c9fe883642a74d8739f94a1358
akileez <git@keeda.net>
2015-10-13 04:25:48 -0400

    update object/transform.js
    remove Ojbect.keys()
    add adopted comment

--- src/object/transform.js
+++ src/object/transform.js
@@ -1,5 +1,10 @@
-var kindOf = require('../lang/kindOf')
-var look   = require('./look')
+// adopted from: https://github.com/djelic/node-object-utils (MIT)
+
+var kindOf  = require('toolz/src/lang/kindOf')
+var look    = require('toolz/src/object/look')
+var keys    = require('toolz/src/object/keys')
+var forEach = require('toolz/src/array/forEach')
+var map     = require('toolz/src/array/map')
 
 function transform (src, schema) {
   if (!src || !schema) return
@@ -9,13 +14,13 @@ function transform (src, schema) {
   function transformer (val) {
     switch (kindOf(val)) {
       case 'object'   : return transform(src, val)
-      case 'array'    : return val.map(transformer)
+      case 'array'    : return map(val, transformer)
       case 'function' : return val.call(obj, src)
       default         : return look(src, val)
     }
   }
 
-  Object.keys(schema).forEach(function (key) {
+  forEach(keys(schema), function (key) {
     obj[key] = transformer(schema[key])
   })
 
