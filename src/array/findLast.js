7a732fdae7bb23fba6bc19a8ae0aa7dcabceb66e
akileez <git@keeda.net>
2015-10-13 17:15:17 -0400

    comment array/findLast.js

--- src/array/findLast.js
+++ src/array/findLast.js
@@ -1,3 +1,6 @@
+// Loops through all the items in the Array (starting from last item)
+// and returns the first one that passes a truth test (callback).
+
 var findLastIndex = require('./findLastIndex')
 
 function findLast (arr, fn) {
