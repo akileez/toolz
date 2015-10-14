31955b5f049b554752232126fb0757e342761ee2
akileez <git@keeda.net>
2015-10-14 03:14:26 -0400

    update array/last.js
    Alias for pick.last. no breakage of older code.

--- src/array/last.js
+++ src/array/last.js
@@ -1,6 +1,9 @@
+// Alias for pick.last()
+
+var pick = require('./pick').last
+
 function last (arr) {
-  if (arr == null || arr.length < 1) return undefined
-  return arr[arr.length - 1]
+  return pick(arr)
 }
 
 module.exports = last
