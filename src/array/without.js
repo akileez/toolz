267e94565ecaac65ebe780cf56981379cdb0dac0
akileez <git@keeda.net>
2015-10-14 16:09:13 -0400

    update array/without
    using array/filter

--- src/array/without.js
+++ src/array/without.js
@@ -1,9 +1,11 @@
 var toArray = require('../lang/toArray')
 var testval = require('../lang/testval')
+var filter  = require('./filter')
 
 function without (arr, removeItems) {
   removeItems = toArray(removeItems) // check this for a string of args
-  return arr.filter(function (item) {
+
+  return filter(arr, function (item) {
     return !testval(item, removeItems)
   })
 }
