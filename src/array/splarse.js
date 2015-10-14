581cae66592d7982a44f69fbe97d43e61ca85597
akileez <git@keeda.net>
2015-10-14 03:17:42 -0400

    source file array/splarse.js
    sparse splice

---
+++ src/array/splarse.js
@@ -0,0 +1,27 @@
+// adopted from: https://github.com/keis/sparse-splice
+
+// original implementation:
+// module.exports = function sparseSplice(arr, remove) {
+//   remove.sort().reverse().forEach(function (idx) {
+//     arr.splice(idx, 1)
+//   })
+// }
+
+var isArray = require('../lang/isArray')
+var slice   = require('./sliced')
+var forEach = require('./forEach')
+var sort    = require('./sort')
+
+function sparseSplice (arr, remove) {
+  remove = isArray(remove) ? remove : slice(arguments, 1)
+
+  forEach(sort(remove, function (a, b) {
+    return b - a // reverse sort
+  }), function (idx) {
+    arr.splice(idx, 1)
+  })
+
+  return arr
+}
+
+module.exports = sparseSplice
