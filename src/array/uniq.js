39e754d64ee4e63ae48998750ab2820dd6412840
akileez <git@keeda.net>
2015-10-14 16:08:28 -0400

    update array/uniq.js
    using array/reduce.js and array/push.js

--- src/array/uniq.js
+++ src/array/uniq.js
@@ -1,6 +1,9 @@
+var reduce = require('./reduce')
+var push = require('./push')
+
 function uniq (arr) {
-  return arr.reduce(function (prev, curr) {
-    if (prev.indexOf(curr) === -1) prev.push(curr)
+  return reduce(arr, function (prev, curr) {
+    if (prev.indexOf(curr) === -1) push(prev, curr)
     return prev
   }, [])
 }
