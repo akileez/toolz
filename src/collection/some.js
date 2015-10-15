7bebf4625ec2c6ca93eb624b231e0b3d6d05797a
akileez <git@keeda.net>
2015-10-15 05:25:19 -0400

    restore makeIterator -- collection/some

--- src/collection/some.js
+++ src/collection/some.js
@@ -1,5 +1,32 @@
 var make = require('./make_')
-var arrSome = require('../array/some')
-var objSome = require('../object/some')
+var makeIterator = require('../function/makeIterator_')
+
+function arrSome (arr, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+  var i = -1
+  var len = arr.length
+
+  while (++i < len) {
+    if (fn(arr[i], i, arr)) return true
+  }
+
+  return false
+}
+
+function objSome (obj, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+  var result = false
+  var key
+
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (fn(obj[key], key, obj)) {
+        result = true
+        break
+      }
+    }
+  }
+  return result
+}
 
 module.exports = make(arrSome, objSome)
