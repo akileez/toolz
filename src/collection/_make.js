9583a7e13fb511045f0ff022b621d7e43c589ddc
akileez <git@keeda.net>
2015-10-15 07:38:45 -0400

    update collection/_make
    remove switch statement. using if statement

--- src/collection/_make.js
+++ src/collection/_make.js
@@ -2,13 +2,14 @@ var kindOf = require('../lang/kindOf')
 
 function makeCollectionMethod (arr, obj, str, rex, def) {
   return function (list) {
-    switch (kindOf(list)) {
-      case 'array': return arr.apply(null, arguments)
-      case 'object': return obj.apply(null, arguments)
-      case 'string': return str.apply(null, arguments)
-      case 'regexp': return rex.apply(null, arguments)
-      default: return def
-    }
+    // this will be an excellent use of the spread operator.
+    // args = slice(arguments, 1)
+    var test = kindOf(list)
+    if (test === 'array')  return arr.apply(null, arguments)
+    if (test === 'object') return obj.apply(null, arguments)
+    if (test === 'string') return str.apply(null, arguments)
+    if (test === 'regexp') return rex.apply(null, arguments)
+    if (test == null) return def
   }
 }
 
