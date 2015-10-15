d1af7f9af18705a21befa8415458010c38c705d3
akileez <git@keeda.net>
2015-10-15 05:24:12 -0400

    restore makeIterator -- collection/max

--- src/collection/max.js
+++ src/collection/max.js
@@ -1,7 +1,42 @@
+var values = require('../object/values')
 var make = require('./make_')
-var arrMax = require('../array/max')
-var objMax = require('../object/max')
+var makeIterator = require('../function/makeIterator_')
 
 // Get maximum value inside collection
+
+function arrMax (arr, fn, thisObj) {
+  if (arr == null || !arr.length) {
+    return Infinity
+  }
+
+  if (arr.length && !fn) {
+    return Math.max.apply(Math, arr)
+  }
+
+  fn = makeIterator(fn, thisObj)
+  var compare = -Infinity
+  var value
+  var temp
+  var result
+
+  var i = -1
+  var len = arr.length
+
+  while (i++ < len) {
+    value = arr[i]
+    temp = fn(value, i, arr)
+
+    if (temp > compare) {
+      compare = temp
+      result = value
+    }
+  }
+
+  return result
+}
+
+function objMax (obj, fn) {
+  return arrMax(values(obj), fn)
+}
 
 module.exports = make(arrMax, objMax)
