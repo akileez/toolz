ef00f8ebe3f4950e8dac55b1dd1edbf275fa264d
akileez <git@keeda.net>
2015-10-15 05:25:04 -0400

    restore makeIterator -- collection/reject

--- src/collection/reject.js
+++ src/collection/reject.js
@@ -1,5 +1,34 @@
 var make = require('./make_')
-var arrReject = require('../array/reject')
-var objReject = require('../object/reject')
+var makeIterator = require('../function/makeIterator_')
+
+function arrReject (arr, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+  var result = []
+  if (arr == null) return result
+
+  var i = -1
+  var len = arr.length
+  var val
+
+  while (++i < len) {
+    val = arr[i]
+    if (!fn(val, i, arr)) result.push(val)
+  }
+
+  return result
+}
+
+function objReject (obj, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+  var result = {}
+  var key
+
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (!fn(obj[key], key, obj)) result[key] = obj[key]
+    }
+  }
+  return result
+}
 
 module.exports = make(arrReject, objReject)
