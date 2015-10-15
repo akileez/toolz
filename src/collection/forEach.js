98226b899501b5a2059b091af68d89ef12d8f39b
akileez <git@keeda.net>
2015-10-15 05:23:27 -0400

    update collection/forEach
    restore makeIterator

--- src/collection/forEach.js
+++ src/collection/forEach.js
@@ -1,5 +1,28 @@
 var make = require('./make_')
-var arrForEach = require('../array/forEach')
-var objForEach = require('../object/forOwn')
+var makeIterator = require('../function/makeIterator_')
+
+function arrForEach (arr, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+  if (arr == null) return
+
+  var i = -1
+  var len = arr.length
+
+  while (++i < len) {
+    if (fn(arr[i], i, arr) === false) break
+  }
+}
+
+function objForEach (obj, fn, thisObj) {
+  fn = makeIterator(fn, thisObj)
+  var key
+
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      fn(obj[key], key, obj)
+    }
+  }
+}
+
 
 module.exports = make(arrForEach, objForEach)
