c8ebd02703a9f85d05d5ada2c1771eb94334a6a7
akileez <git@keeda.net>
2015-10-15 05:23:57 -0400

    restore makeIterator -- collection/map

--- src/collection/map.js
+++ src/collection/map.js
@@ -1,5 +1,32 @@
 var make = require('./make_')
-var arrMap = require('../array/map')
-var objMap = require('../object/map')
+var makeIterator = require('../function/makeIterator_')
+
+function arrMap (arr, fn, thisObj) {
+  fn = makeIterator(callback, thisObj)
+
+  var results = []
+  if (arr == null) return results
+
+  var i = -1
+  var len = arr.length
+
+  while (++i < len) {
+    results[i] = fn(arr[i], i, arr)
+  }
+  return results
+}
+
+function objMap (obj, fn, thisObj) {
+  fn = makeIterator(callback, thisObj)
+  var result = {}
+  var key
+
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = fn(obj[key], key, obj)
+    }
+  }
+  return result
+}
 
 module.exports = make(arrMap, objMap)
