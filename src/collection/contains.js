fadb7be6457d2e984d507a3b193626636e642e5b
akileez <git@keeda.net>
2015-10-15 07:15:59 -0400

    update collection/contains
    able to handle strings and regexps

--- src/collection/contains.js
+++ src/collection/contains.js
@@ -1,5 +1,7 @@
-var make = require('./make_')
+var make = require('./_make')
 var arrContains = require('../array/contains')
 var objContains = require('../object/contains')
+var strContains = require('../string/contains')
+var rexContains = require('../regex/contains')
 
-module.exports = make(arrContains, objContains)
+module.exports = make(arrContains, objContains, strContains, rexContains)
