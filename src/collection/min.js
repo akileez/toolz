71f4adef1740239aefca6640215447bee30f8138
akileez <git@keeda.net>
2015-10-15 05:24:27 -0400

    restore makeIterator -- collection/min

--- src/collection/min.js
+++ src/collection/min.js
@@ -1,7 +1,42 @@
+var values = require('../object/values')
 var make = require('./make_')
-var arrMin = require('../array/min')
-var objMin = require('../object/min')
+var makeIterator = require('../function/makeIterator_')
 
 // Get minimum value inside collection.
+
+function arrMin (arr, fn, thisObj) {
+  if (arr == null || !arr.length) {
+    return -Infinity
+  }
+
+  if (arr.length && !fn) {
+    return Math.min.apply(Math, arr)
+  }
+
+  fn = makeIterator(iterator, thisObj)
+  var compare = Infinity
+  var value
+  var temp
+  var result
+
+  var i = -1
+  var len = arr.length
+
+  while (i++ < len) {
+    value = arr[i]
+    temp = fn(value, i, arr)
+
+    if (temp < compare) {
+      compare = temp
+      result = value
+    }
+  }
+
+  return result
+}
+
+function objMin (obj, fn) {
+  return arrMin(values(obj), fn)
+}
 
 module.exports = make(arrMin, objMin)
