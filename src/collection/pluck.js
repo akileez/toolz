769328a7b5a42f3fe05987e45e9c1f20e7eb0fd8
akileez <git@keeda.net>
2015-10-15 05:24:46 -0400

    restore makeIterator -- collection/pluck

--- src/collection/pluck.js
+++ src/collection/pluck.js
@@ -1,5 +1,14 @@
 var make = require('./make_')
-var arrPluck = require('../array/pluck')
-var objPluck = require('../object/pluck')
+var amap = require('../array/map')
+var omap = require('../object/map')
+var prop = require('../function/prop')
+
+function arrPluck (arr, propName) {
+  return amap(arr, propName)
+}
+
+function objPluck (obj, propName) {
+  return omap(obj, prop(propName))
+}
 
 module.exports = make(arrPluck, objPluck)
