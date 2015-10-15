363c6e06d8b89ad15fe5ce3fd873ca4cb93da9d8
akileez <git@keeda.net>
2015-10-15 07:39:50 -0400

    reformat collection/matches
    this is used for file/readDir only. need to test different combination
    of matches function

--- src/collection/matches.js
+++ src/collection/matches.js
@@ -1,12 +1,13 @@
+// a dupe of collection/contains.js although not as eloquent. will develop
+// this version further for strings and/or functions then build
+// a comprehensive make function.
+
 var isPlainObject = require('../lang/isPlainObject')
 var objContains = require('../object/contains')
 var arrContains = require('../array/contains')
 var reContains = require('../regex/contains')
 var isRegExp = require('../lang/isRegExp')
 
-// a dupe of collection/contains.js although not as eloquent. will develop
-// this version further for strings and/or functions then build
-// a comprehensive make function.
 function matches (obj, val) {
   if (Array.isArray(obj)) return arrContains(obj, val)
   if (isRegExp(obj)) return reContains(obj, val)
