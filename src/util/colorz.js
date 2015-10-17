6b0851564b6f803d75df3261a700af28a7d722f2
akileez <git@keeda.net>
2015-10-17 00:25:49 -0400

    update utils/colorz
    internal version of colorz to match npm version about to publish

--- src/util/colorz.js
+++ src/util/colorz.js
@@ -1,24 +1,37 @@
-var styles = require('../string/ansiStyles')
-var keys = require('../object/keys')
+var styles  = require('../string/ansiStyles')
 var forEach = require('../array/forEach')
+var keys    = require('../object/keys')
 
-var ansi = {}
+var colorz = {}
 
 forEach(keys(styles), function (style) {
-  var open = ['\u001b[', styles[style][0], 'm'].join('')
-  var clos = ['\u001b[', styles[style][1], 'm'].join('')
+  var open = '\u001b[' + styles[style][0] + 'm'
+  var clos = '\u001b[' + styles[style][1] + 'm'
 
-  ansi[style] = function (msg) {
-    return [open, msg, clos].join('')
+  colorz[style] = function (msg) {
+    return open + msg + clos
   }
 })
 
-ansi.strip = function (str) {
+function strip (str) {
   return str.replace(/(?:\u001b\[)\d+m/g, '')
 }
 
-ansi.expose = function (style) {
-  return [style, ': ', JSON.stringify(ansi[style](' ++string++ '))].join('')
+function expose (style, str, noColor) {
+  if (typeof str === 'boolean') {
+    noColor = str
+    str = 'Hello World'
+  }
+
+  str = str || 'Hello World'
+
+  return noColor
+    ? style + ': ' + JSON.stringify(colorz[style](str))
+    : style + ': '
+      + JSON.stringify(colorz[style](str))
+      .replace(/(\\u001b\[\d+m)/g, colorz[style](['$1']))
 }
 
-module.exports = ansi
+module.exports = colorz
+module.exports.strip = strip
+module.exports.expose = expose
