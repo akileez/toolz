d3835f1040bb6400d1b6322e73b44376317c4342
akileez <git@keeda.net>
2015-10-17 00:21:54 -0400

    update collection/pluck
    simplify

--- src/collection/pluck.js
+++ src/collection/pluck.js
@@ -1,14 +1,5 @@
 var make = require('./make_')
-var amap = require('../array/map')
-var omap = require('../object/map')
-var prop = require('../function/prop')
-
-function arrPluck (arr, propName) {
-  return amap(arr, propName)
-}
-
-function objPluck (obj, propName) {
-  return omap(obj, prop(propName))
-}
+var arrPluck = require('../array/pluck')
+var objPluck = require('../object/pluck')
 
 module.exports = make(arrPluck, objPluck)
