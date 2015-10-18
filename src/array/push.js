d05f61d56e7de24372595d51013f84d516989565
akileez <git@keeda.net>
2015-10-18 16:56:44 -0400

    update array/push
    it is now to the point where I can say “I like it…” most likely though
    it will be used little…a convenience function…like array/join. only
    used when most appropriate

--- src/array/push.js
+++ src/array/push.js
@@ -2,22 +2,36 @@
 // adds one or more elements to the end of an array
 // and returns the new length of the array
 
+// Not as fast as [].prototype.push but it has the ability to parse arrays
+// without using call/apply
+
 var slice = require('./sliced')
+var isPrimitive = require('../lang/isPrimitive')
+var isArray = require('../lang/isArray')
 
 function push (arr, args) {
-  args = Array.isArray(args)
-    ? args
-    : slice(arguments, 1)
+  if (!args) return arr
+  var len = arr.length
 
-  var i = -1
-  var len1 = arr.length
-  var len2 = args.length
-
-  while (++i < len2) {
-    arr[len1 + i] = args[i]
+  if (arguments.length === 2) {
+    if (isPrimitive(args) || !isArray(args)) {
+      arr[len] = args
+      return arr
+    } else {
+      var len2 = args.length
+    }
+  } else {
+    args = slice(arguments, 1)
+    len2 = args.length
   }
 
-  return arr
+  var i = -1
+
+  while (++i < len2) {
+    arr[len + i] = args[i]
+  }
+
+  return arr.length
 }
 
 module.exports = push
