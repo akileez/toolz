5504434f2bceed3f5f4f8e4fd38dd9cb2444422d
akileez <git@keeda.net>
2015-10-18 05:03:21 -0400

    source code upload -- test/tta
    tiny test assertion

---
+++ src/test/tta.js
@@ -0,0 +1,112 @@
+// adopted from: claim <https://github.com/kevva/claim/blob/master/index.js>
+// Copyright (c) Kevin Mårtensson <kevinmartensson@gmail.com> (MIT)
+
+// tta -- tiny test assertion
+
+var assert = require('./assert')
+var AssertionError = assert.AssertionError
+
+function create (val, expected, operator, msg, fn) {
+  return {
+    actual: val,
+    expected: expected,
+    message: msg,
+    operator: operator,
+    stackStartFunction: fn
+  }
+}
+
+function test (ok, opts) {
+  if (!ok) throw new AssertionError(opts)
+}
+
+function pass (msg) {
+  test(true, create(true, true, 'pass', msg, pass))
+}
+
+function fail (msg) {
+  test(false, create(false, false, 'fail', msg, fail))
+}
+
+function ok (val, msg) {
+  test(val, create(val, true, '==', msg, ok))
+}
+
+function notOk (val, msg) {
+  test(!val, create(val, true, '==', msg, notOk))
+}
+
+function istrue (val, msg) {
+  test(val === true, create(val, true, '===', msg, istrue))
+}
+
+function isFalse (val, msg) {
+  test(val === false, create(val, false, '===', msg, isFalse))
+}
+
+function is (val, expected, msg) {
+  test(val === expected, create(val, expected, '===', msg, is))
+}
+
+function not (val, expected, msg) {
+  test(val !== expected, create(val, expected, '!==', msg, not))
+}
+
+function same (val, expected, msg) {
+  try {
+    assert.deepEqual(val, expected, msg)
+  } catch (err) {
+    test(false, create(val, expected, '!==', msg, same))
+  }
+}
+
+function notSame (val, expected, msg) {
+  try {
+    assert.notDeepEqual(val, expected, msg)
+  } catch (err) {
+    test(false, create(val, expected, '!==', msg, notSame))
+  }
+}
+
+function throws (fn, err, msg) {
+  try {
+    assert.throws(fn, err, msg)
+  } catch (err) {
+    test(false, create(err.actual, err.expected, err.operator, err.message, throws))
+  }
+}
+
+function doesNotThrow (fn, msg) {
+  try {
+    assert.doesNotThrow(fn, msg)
+  } catch (err) {
+    test(false, create(err.actual, err.expected, err.operator, err.message, doesNotThrow))
+  }
+}
+
+function regexTest (regex, contents, msg) {
+  test(regex.test(contents), create(regex, contents, '===', msg, regexTest))
+}
+
+function ifError (err, msg) {
+  test(!err, create(err, 'Error', '!==', msg, ifError))
+}
+
+module.exports = {
+  pass: pass,
+  fail: fail,
+  ok: ok,
+  assert: ok,
+  notOk: notOk,
+  istrue: istrue,
+  isFalse: isFalse,
+  is: is,
+  not: not,
+  same: same,
+  notSame: notSame,
+  throws: throws,
+  doesNotThrow: doesNotThrow,
+  regexTest: regexTest,
+  ifError: ifError,
+  ifErr: ifError
+}
