38c6347d03e3d822af67f2e3f6f72f1062dd7719
akileez <git@keeda.net>
2015-10-22 17:05:35 -0400

    update array/unionized
    removing array.push

--- src/array/unionized.js
+++ src/array/unionized.js
@@ -2,13 +2,14 @@ function unionized (arrs) {
   var results = []
   var i = -1
   var j = -1
+  var k = -1
   var len = arguments.length
 
   while (++i < len) {
     var arg = toArray(arguments[i])
     while (++j < arg.length) {
       var ele = arg[j]
-      if (results.indexOf(ele) === -1) results.push(ele)
+      if (results.indexOf(ele) === -1) results[++k] = ele
     }
   }
   return results
