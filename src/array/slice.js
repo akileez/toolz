f443ce73dc4120973454a0286dabe981dad24fe0
akileez <git@keeda.net>
2015-10-22 17:06:04 -0400

    reformat array/slice
    move vars to top of function

--- src/array/slice.js
+++ src/array/slice.js
@@ -9,6 +9,8 @@
 
 function slice (arr, start, end) {
   var len = arr.length
+  var result = []
+  var j = -1
 
   if (start == null) start = 0
   else if (start < 0) start = Math.max(len + start, 0)
@@ -17,9 +19,6 @@ function slice (arr, start, end) {
   if (end == null) end = len
   else if (end < 0) end = Math.max(len + end, 0)
   else end = Math.min(end, len)
-
-  var result = []
-  var j = -1
 
   while (start < end) {
     result[++j] = arr[start++]
