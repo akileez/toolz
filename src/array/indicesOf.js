7d82ba6eb4974df1942405124979f6576fb1cca3
akileez <git@keeda.net>
2015-10-25 03:37:56 -0400

    update array/indiciesOf -- add comments

--- src/array/indicesOf.js
+++ src/array/indicesOf.js
@@ -1,3 +1,17 @@
+// Returns an array of indices where item is found in the array.
+// Like array/indexOf it does loop over sparse items in the array.
+// The optional fromIndex parameter can limit the scope, the same way as it does in indexOf.
+
+/*
+    var items = ['lorem', 'ipsum', 'foo', 'ipsum', 'ipsum'];
+
+    indicesOf(items, 'ipsum');
+    // > [1, 3, 4]
+
+    indicesOf(items, 'ipsum', 1);
+    // > [3, 4]
+ */
+
 function indicesOf (arr, item, fromIndex) {
   var results = []
   if (arr == null) return results
