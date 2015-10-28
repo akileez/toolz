acd63d702417724c4122a25e0fad48c99d42b632
akileez <git@keeda.net>
2015-10-28 05:15:39 -0400

    update collection/_make
    check for only two args

--- src/collection/_make.js
+++ src/collection/_make.js
@@ -1,6 +1,14 @@
 var isInteger = require('../lang/isInteger')
 
 function makeCollectionMethod (arr, obj, str, rex, def) {
+  if (arguments.length === 2) {
+    return function (list) {
+      return (isInteger(list.length))
+      ? arr.apply(null, arguments)
+      : obj.apply(null, arguments)
+    }
+  }
+
   return function (list) {
     // this will be an excellent use of the spread operator.
     // will be able to get rid of apply
