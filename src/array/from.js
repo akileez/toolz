1460fc21d687eece57dd0647c646bd5aae5357ce
akileez <git@keeda.net>
2015-10-29 11:25:31 -0400

    update array/from
    just got it working how I expected. need to improve on or just upgrade
    to ES6

--- src/array/from.js
+++ src/array/from.js
@@ -1,10 +1,17 @@
 // in lieu of the spread operator until I upgrade
 // to ES6 !!NOT TESTED!!
-var slice = require('./sliced')
+var slice = require('./slice')
+var map = require('../object/map')
+var foreach = require('../object/foreach')
 
-function from (args) {
-  args = slice(arguments)
-  return args
+function from () {
+  return map(slice(arguments), function (val) {
+    var res = []
+    foreach(val, function (prop) {
+      return res.push(prop)
+    })
+    return res
+  })['0']
 }
 
 module.exports = from
