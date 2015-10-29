9833b9661ede08f0583e59507e70179852e9da0f
akileez <git@keeda.net>
2015-10-29 07:56:38 -0400

    update object/transform -- use object/get, fix requires

--- src/object/transform.js
+++ src/object/transform.js
@@ -1,10 +1,10 @@
 // adopted from: https://github.com/djelic/node-object-utils (MIT)
 
-var kindOf  = require('toolz/src/lang/kindOf')
-var look    = require('toolz/src/object/look')
-var keys    = require('toolz/src/object/keys')
-var forEach = require('toolz/src/array/forEach')
-var map     = require('toolz/src/array/map')
+var kindOf  = require('../lang/kindOf')
+var get     = require('../object/get')
+var keys    = require('../object/keys')
+var forEach = require('../array/forEach')
+var map     = require('../array/map')
 
 function transform (src, schema) {
   if (!src || !schema) return
@@ -16,7 +16,7 @@ function transform (src, schema) {
       case 'object'   : return transform(src, val)
       case 'array'    : return map(val, transformer)
       case 'function' : return val.call(obj, src)
-      default         : return look(src, val)
+      default         : return get(src, val)
     }
   }
 
