ca349efc5cd00a0f775c0ed93f3c33fc4034852a
akileez <git@keeda.net>
2015-10-29 07:57:54 -0400

    update object/get
    can use an array as the property values. speed up thanks to Jon
    Schlinkert

--- src/object/get.js
+++ src/object/get.js
@@ -1,15 +1,30 @@
-// get nested object property
+// adopted from: get-value <https://github.com/jonschlinkert/get-value>
+// Copyright (c) 2014-2015, Jon Schlinkert. (MIT)
+
+// get nested object property. supports an array of properties
+
+var isString = require('toolz/src/lang/isString')
+var isArray = require('toolz/src/lang/isArray')
 
 function getValue (obj, prop) {
-  var segs = prop.split('.')
-  var last = segs.pop()
+  var segs = toSegemnts(prop)
 
-  while (segs.length) {
-    obj = obj[segs.shift()]
-    if (obj == null) return
+  if (segs === null) return obj
+
+  var len = segs.length
+  var i = -1
+
+  while (obj && (++i < len)) {
+    obj = obj[segs[i]]
   }
 
-  return obj[last]
+  return obj
+}
+
+function toSegemnts (val) {
+  if (isArray(val)) return val
+  if (isString(val)) return val.split('.')
+  return null
 }
 
 module.exports = getValue
