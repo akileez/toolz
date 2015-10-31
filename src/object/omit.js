651db092e99ecd4dea0deb0d15a3e1e32ad401db
akileez <git@keeda.net>
2015-10-31 14:54:37 -0400

    update object/omit -- use object/extend

--- src/object/omit.js
+++ src/object/omit.js
@@ -1,17 +1,18 @@
-var clone   = require('../lang/clone')
+var extend  = require('../object/extend')
 var slice   = require('../array/slice')
-var forEach = require('../array/forEach')
 
 function omit (obj, toOmit) {
   toOmit = Array.isArray(toOmit)
     ? toOmit
     : slice(arguments, 1)
 
-  var output = clone(obj)
+  var output = extend({}, obj)
+  var i = -1
+  var len = toOmit.length
 
-  forEach(toOmit, function (omit) {
-    delete output[omit]
-  })
+  while (++i < len) {
+    delete output[toOmit[i]]
+  }
 
   return output
 }
