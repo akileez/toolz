ab6c6ba697a75df9ebf8463ed6503549697ae334
akileez <git@keeda.net>
2015-10-31 13:06:57 -0400

    update object/assign -- use isObjectLike()

--- src/object/assign.js
+++ src/object/assign.js
@@ -2,7 +2,7 @@
 // Copyright (c) 2015, Jon Schlinkert. (MIT)
 
 var isUndefined = require('../lang/isUndefined')
-var isObject    = require('../lang/isObject')
+var isObject    = require('../lang/isObjectLike')
 var isString    = require('../lang/isString')
 var kindOf      = require('../lang/kindOf')
 var yoda        = require('../lang/yoda')
