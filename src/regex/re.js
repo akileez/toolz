d8fca571f74ab3c7175770c5a878f72c031c307b
akileez <git@keeda.net>
2015-11-01 15:31:13 -0500

    source file regex/re
    testing a collection of regexp’s as one file

---
+++ src/regex/re.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ansiEscape: /\u001b.*?m/g,
+  dotnotation: /^(.+)\.(.+)$/,
+  emptylines: /( ){1,}(\r\n|\n){1,}/g,
+  whitespace: /^[\s\f\n\r\t\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000\ufeff\x09\x0a\x0b\x0c\x0d\x20\xa0]+$/,
+  words: /[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+/g
+}
