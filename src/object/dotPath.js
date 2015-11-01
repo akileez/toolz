618433ccae52b5b7af7b6c50b480dd9816a14d25
akileez <git@keeda.net>
2015-10-31 22:31:52 -0400

    rename file object/toPath --> dotPath

--- src/object/dotPath.js
+++ src/object/dotPath.js
@@ -5,11 +5,11 @@ var isArguments = require('../lang/isArguments')
 var flatten     = require('../array/flatten')
 var slice       = require('../array/slice')
 
-function topath (args) {
+function dotPath (args) {
   if (isArguments(args)) args = slice(args)
   else args = slice(arguments)
 
   return flatten(args).join('.')
 }
 
-module.exports = topath
+module.exports = dotPath
