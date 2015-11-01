628fe59083fbbcc6c0ae931296cbf5d41ab7d05b
akileez <git@keeda.net>
2015-11-01 15:01:54 -0500

    refactor object/namespace
    handles an array input or string

--- src/object/namespace.js
+++ src/object/namespace.js
@@ -1,15 +1,23 @@
-var forEach = require('../array/forEach')
-
 // create nested object if non-existent
 
 function namespace (obj, path) {
   if (!path) return obj
+  var segs = toSegments(path)
 
-  forEach(path.split('.'), function (key) {
-    if (!obj[key]) obj[key] = {}
-    obj = obj[key]
-  })
+  var i = -1
+  var len = segs.length
+
+  while (++i < len) {
+    if (!obj[segs[i]]) obj[segs[i]] = {}
+    obj = obj[segs[i]]
+  }
+
   return obj
+}
+
+function toSegments (val) {
+  if (Array.isArray(val)) return val
+  if (typeof val === 'string') return val.split('.')
 }
 
 module.exports = namespace
