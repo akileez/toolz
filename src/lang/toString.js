6a1899cf5c4d3c471e00e333eafb82d0a08f891e
akileez <git@keeda.net>
2015-11-02 14:47:28 -0500

    update lang/toString

--- src/lang/toString.js
+++ src/lang/toString.js
@@ -1,5 +1,9 @@
+// converts "value" to string if it's not one. An empty string is
+// returned for "null" or "undefined" values
+
 function toString (value) {
-  return value == null ? '' : value.toString()
+  if (typeof value === 'string') return value
+  return value == null ? '' : (value + '')
 }
 
 module.exports = toString
