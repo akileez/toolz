2276041c753de72d9a9bed62156c27f6598177dd
akileez <git@keeda.net>
2015-11-03 14:31:50 -0500

    update lang/yoda
    the answer is the question

--- src/lang/yoda.js
+++ src/lang/yoda.js
@@ -5,14 +5,14 @@ var some = require('../array/some')
 function and (ans) {
   var args = slice(arguments, 1)
   return every(args, function (question) {
-    return question === ans
+    return (ans === question)
   })
 }
 
 function or (ans) {
   var args = slice(arguments, 1)
   return some(args, function (question) {
-    return question === ans
+    return (ans === question)
   })
 }
 
