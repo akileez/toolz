5b970850f421ca3b0a6d8c08973bffb0e0645457
akileez <git@keeda.net>
2015-11-05 03:44:59 -0500

    move file lang/kindIs --> lang/kindOf

--- src/lang/kindOf.js
+++ src/lang/kindOf.js
@@ -6,4 +6,80 @@ function kindOf (value) {
     : typeof value
 }
 
+function kindOfType (value) {
+  // Return the constructor name if not a primative, array or function
+  // A more performant `kindOf` with cavets. Must test ES6 after upgrade.
+  if (value === null) return 'null'
+  if (value === undefined) return 'undefined'
+
+  if (Array.isArray(value)) return 'array'
+
+  if (typeof value === 'string') return 'string'
+  if (typeof value === 'boolean') return 'boolean'
+  if (typeof value === 'number') return 'number'
+  if (typeof value === 'symbol') return 'symbol'
+  if (typeof value === 'function') return 'function'
+
+  // this will pick out PlainObjects, regexps, and dates
+  // more quickly than a toString.call(). Fails on arguments,
+  // not really though --> arguments [object]
+  var type = value.constructor.name
+
+  if (type === 'Object') return 'object'
+  if (type === 'Date') return 'date'
+  if (type === 'RegExp') return 'regexp'
+
+  return type.toLowerCase()
+}
+
+function kindOfObjs (value) {
+  // a speedy version to obtain common object "types"
+  if (typeof value === 'function') return 'function'
+  if (Array.isArray(value)) return 'array'
+  if (typeof value === 'string') return 'string'
+
+  var type = value.constructor.name
+
+  if (type === 'Object') return 'object'
+  if (type === 'Date') return 'date'
+  if (type === 'RegExp') return 'regexp'
+
+  return type.toLowerCase()
+}
+
+function kindOfSafe (value) {
+  // Object.create(null) throws an error. Code below
+  // prevents this from happening.
+  try {
+    if (value === null) return 'null'
+    if (value === undefined) return 'undefined'
+
+    if (Array.isArray(value)) return 'array'
+
+    var type = value.constructor.name
+
+    if (type === 'String') return 'string'
+    if (type === 'Boolean') return 'boolean'
+    if (type === 'Number') return 'number'
+    if (type === 'Symbol') return 'symbol'
+    if (type === 'Function') return 'function'
+    if (type === 'RegExp') return 'regexp'
+    if (type === 'Date') return 'date'
+    if (type === 'Object') {
+      return Object.prototype.toString.call(value) === '[object Arguments]'
+        ? 'arguments'
+        : 'object'
+    }
+
+    return type.toLowerCase()
+  } catch (err) {
+    return Object.prototype.toString.call(value)
+      .replace(/^\[object |\]$/g, '')
+      .toLowerCase()
+  }
+}
+
 module.exports = kindOf
+module.exports.type = kindOfType
+module.exports.objs = kindOfObjs
+module.exports.safe = kindOfSafe
