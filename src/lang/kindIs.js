0154df27a9b27c047970519bfd0ddd4af67cabfc
akileez <git@keeda.net>
2015-11-05 02:50:14 -0500

    update lang/kindIs -- improve performance

--- src/lang/kindIs.js
+++ src/lang/kindIs.js
@@ -1,39 +1,83 @@
 // Return the constructor name if not a primative, array or function
-// A more performant kindOf with cavets. Must test ES6 after upgrade.
+// A more performant `kindOf` with cavets. Must test ES6 after upgrade.
 
-var kindOf = require('./kindOf')
-
-function konstrutor (value, safemode) {
-  if (value === undefined) return 'undefined'
+function konstrutor (value) {
   if (value === null) return 'null'
+  if (value === undefined) return 'undefined'
+
   if (Array.isArray(value)) return 'array'
-  if (typeof value === 'function') return 'function'
+
   if (typeof value === 'string') return 'string'
   if (typeof value === 'boolean') return 'boolean'
   if (typeof value === 'number') return 'number'
   if (typeof value === 'symbol') return 'symbol'
+  if (typeof value === 'function') return 'function'
 
   // this will pick out PlainObjects, regexps, and dates
   // more quickly than a toString.call(). Fails on arguments,
   // not really though --> arguments [object]
-  return value.constructor.name.toLowerCase()
+  var type = value.constructor.name
 
-  // Object.create(null) throws an error. Code below
-  // prevents this from happening with a performance
-  // hit.
-  // try {
-  //   return value.constructor.name.toLowerCase()
-  // } catch (err) {
-  //   return kindOf(value)
-  // }
+  if (type === 'Object') return 'object'
+  if (type === 'Date') return 'date'
+  if (type === 'RegExp') return 'regexp'
 
+  return type.toLowerCase()
 }
 
 function konstrutorArgs (value) {
-  return kindOf(value) === 'arguments'
+  // handles arguments. Same as isArguments() and kindOf.
+  return Object.prototype.toString.call(value) === '[object Arguments]'
     ? 'arguments'
-    : kindIs(value)
+    : Object.prototype.toString.call(value)
+      .replace(/^\[object |\]$/g, '')
+      .toLowerCase()
+}
+
+function konstructorObjs (value) {
+  // a speedy version to obtain common object "types"
+  if (typeof value === 'function') return 'function'
+  if (Array.isArray(value)) return 'array'
+  if (typeof value === 'string') return 'string'
+
+  var type = value.constructor.name
+
+  if (type === 'Object') return 'object'
+  if (type === 'Date') return 'date'
+  if (type === 'RegExp') return 'regexp'
+
+  return type.toLowerCase()
+}
+
+function konstructorSafe (value) {
+  // Object.create(null) throws an error. Code below
+  // prevents this from happening.
+  try {
+    if (value === null) return 'null'
+    if (value === undefined) return 'undefined'
+
+    if (Array.isArray(value)) return 'array'
+
+    var type = value.constructor.name
+
+    if (type === 'String') return 'string'
+    if (type === 'Boolean') return 'boolean'
+    if (type === 'Number') return 'number'
+    if (type === 'Symbol') return 'symbol'
+    if (type === 'Function') return 'function'
+    if (type === 'Object') return 'object'
+    if (type === 'Date') return 'date'
+    if (type === 'RegExp') return 'regexp'
+
+    return type.toLowerCase()
+  } catch (err) {
+    return Object.prototype.toString.call(value)
+      .replace(/^\[object |\]$/g, '')
+      .toLowerCase()
+  }
 }
 
 module.exports = konstrutor
 module.exports.args = konstrutorArgs
+module.exports.safe = konstructorSafe
+module.exports.objs = konstructorObjs
