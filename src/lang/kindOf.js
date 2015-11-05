d3e94d1d2ddeb0d414c461f5f2f072470c318e2a
akileez <git@keeda.net>
2015-11-05 12:57:25 -0500

    update lang/kindOf -- add base(), optimized safe()

--- src/lang/kindOf.js
+++ src/lang/kindOf.js
@@ -1,9 +1,34 @@
+var isArrayLike = require('toolz/src/lang/isArrayLike')
+
 function kindOf (value) {
   return typeof value === 'object'
     ? Object.prototype.toString.call(value)
       .replace(/^\[object |\]$/g, '')
       .toLowerCase()
     : typeof value
+}
+
+function kindOfBase (value) {
+  // Base implementation which all other kindOf
+  // functions were morphed from and compared to.
+  // Does not handle arguments
+  if (value === null) return 'null'
+  if (value === undefined) return 'undefined'
+
+  if (Array.isArray(value)) return 'array'
+
+  var type = value.constructor.name
+
+  if (type === 'String') return 'string'
+  if (type === 'Boolean') return 'boolean'
+  if (type === 'Number') return 'number'
+  if (type === 'Symbol') return 'symbol'
+  if (type === 'Function') return 'function'
+  if (type === 'Object') return 'object'
+  if (type === 'Date') return 'date'
+  if (type === 'RegExp') return 'regexp'
+
+  return type.toLowerCase()
 }
 
 function kindOfType (value) {
@@ -22,7 +47,8 @@ function kindOfType (value) {
   if (typeof value === 'function') return 'function'
 
   // this will pick out PlainObjects, regexps, and dates
-  // more quickly than a toString.call(). Fails on arguments,
+  // quicker than a toString.call() but marginally slower
+  // than the base implementation. Fails on arguments,
   // not really though --> arguments [object]
   var type = value.constructor.name
 
@@ -50,22 +76,28 @@ function kindOfObjs (value) {
 }
 
 function kindOfSafe (value) {
+  if (value === null) return 'null'
+  if (value === undefined) return 'undefined'
+
+  if (Array.isArray(value)) return 'array'
+
+  if (typeof value === 'string') return 'string'
+  if (typeof value === 'boolean') return 'boolean'
+  if (typeof value === 'number') return 'number'
+  if (typeof value === 'symbol') return 'symbol'
+  if (typeof value === 'function') return 'function'
+
   // Object.create(null) throws an error. Code below
-  // prevents this from happening. Does not handle arguments.
+  // prevents this from happening. Handles arguments.
   try {
-    if (value === null) return 'null'
-    if (value === undefined) return 'undefined'
-
-    if (Array.isArray(value)) return 'array'
-
     var type = value.constructor.name
 
-    if (type === 'String') return 'string'
-    if (type === 'Boolean') return 'boolean'
-    if (type === 'Number') return 'number'
-    if (type === 'Symbol') return 'symbol'
-    if (type === 'Function') return 'function'
-    if (type === 'Object') return 'object'
+    if (type === 'Object') {
+      return isArrayLike(value)
+        ? 'arguments'
+        : 'object'
+    }
+
     if (type === 'RegExp') return 'regexp'
     if (type === 'Date') return 'date'
 
@@ -78,6 +110,7 @@ function kindOfSafe (value) {
 }
 
 module.exports = kindOf
+module.exports.base = kindOfBase
 module.exports.type = kindOfType
 module.exports.objs = kindOfObjs
 module.exports.safe = kindOfSafe
