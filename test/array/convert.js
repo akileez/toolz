a8372c052620b3b92bccaa274de5402b8b0b8f5d
akileez <git@keeda.net>
2015-11-06 15:44:39 -0500

    source test -- array/convert

---
+++ test/array/convert.js
@@ -0,0 +1,82 @@
+var test = require('../../src/assertion/ttr')
+var convert = require('../../src/array/convert')
+
+test('convert array like objects into array', function (t) {
+  var obj = {
+    '0': 'foo',
+    '1': 'bar',
+    '2': 'baz',
+    length: 3
+  }
+
+  t.same(convert.toArray(obj), ['foo', 'bar', 'baz'])
+  t.diff(convert(obj), ['foo', 'bar', 'baz'])
+})
+
+test('convert arguments object', function (t) {
+  var res1
+  var res2
+  var fn = function (a, b, c) {
+    res1 = convert.toArray(arguments)
+  }
+
+  var fm = function (a, b, c) {
+    res2 = convert(arguments)
+  }
+
+  fn('foo', 'bar', 123)
+  fm('foo', 'bar', 123)
+
+  t.same(res1, ['foo', 'bar', 123])
+  t.diff(res2, ['foo', 'bar', 123])
+})
+
+test('handle primitives and other objects', function (t) {
+  var fn = function () {}
+
+  t.same(convert('lorem'), ['lorem'])
+  t.same(convert(''), [''])
+  t.same(convert(123), [123])
+  t.same(convert(0), [0])
+  t.same(convert(/\w+/), [/\w+/])
+  t.same(convert(new RegExp('\\w+')), [/\w+/])
+  t.same(convert({foo:"bar", lorem:123}), [{foo:"bar", lorem:123}])
+  t.same(convert(true), [true])
+  t.same(convert(false), [false])
+  t.same(convert(fn), [fn])
+
+  t.same(convert.toArray('lorem'), ['lorem'])
+  t.same(convert.toArray(''), [''])
+  t.same(convert.toArray(123), [123])
+  t.same(convert.toArray(0), [0])
+  t.same(convert.toArray(/\w+/), [/\w+/])
+  t.same(convert.toArray(new RegExp('\\w+')), [/\w+/])
+  t.same(convert.toArray({foo:"bar", lorem:123}), [{foo:"bar", lorem:123}])
+  t.same(convert.toArray(true), [true])
+  t.same(convert.toArray(false), [false])
+  t.same(convert.toArray(fn), [fn])
+})
+
+test('handle null or undefined values', function (t) {
+  t.same(convert(null), [])
+  t.same(convert(undefined), [])
+  t.same(convert(), [])
+
+  t.same(convert.toArray(null), [])
+  t.same(convert.toArray(undefined), [])
+  t.same(convert.toArray(), [])
+})
+
+test('assertion test', function (t) {
+  t.assert(convert('foo')[0] === 'foo')
+  t.assert(convert(['foo'])[0] === 'foo')
+  t.assert(convert(null)[0] === undefined)
+  t.assert(convert(undefined)[0] === undefined)
+
+  t.assert(convert.toArray('foo')[0] === 'foo')
+  t.assert(convert.toArray(['foo'])[0] === 'foo')
+  t.assert(convert.toArray(null)[0] === undefined)
+  t.assert(convert.toArray(undefined)[0] === undefined)
+})
+
+test.result()
