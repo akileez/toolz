0c80ee14818abc682949a997534bdf1e84e7dfca
akileez <git@keeda.net>
2015-11-06 15:43:17 -0500

    update lang/toArray -- performance tuning

--- src/lang/toArray.js
+++ src/lang/toArray.js
@@ -1,34 +1,34 @@
+// convert array-like object into array
+
 var kindOf = require('./kindOf').type
 var GLOBAL = require('./GLOBAL')
 
-// convert array-like object into array
-
 function toArray (value) {
-  var ret = []
-  var kind = kindOf(value)
-  var n
+  if (value == null) return []
 
-  if (value != null) {
-    if (value.length == null
-      || kind === 'string'
-      || kind === 'function'
-      || kind === 'regexp'
-      || value === GLOBAL
-      ) {
-      // string, regexp, function have .length but user probably just want
-      // to wrap value into array..
-      ret[ret.length] = value
-    } else {
-      // window returns true on isObject in IE7 and may have length
-      // property. `typeof NodeList` returns function on Safari so
-      // we can't use it (#58)
-      n = value.length
-      while (n--) {
-        ret[n] = value[n]
-      }
-    }
+  var kind = kindOf(value)
+
+  if (value.length == null
+    || kind === 'string'
+    || kind === 'function'
+    || kind === 'regexp'
+    || value === GLOBAL
+    ) {
+    // string, regexp, function have .length but user probably just want
+    // to wrap value into array..
+    return [value]
+  } else {
+    if (Array.isArray(value)) return value
+    // window returns true on isObject in IE7 and may have length
+    // property. `typeof NodeList` returns function on Safari so
+    // we can't use it (#58)
+    var res = []
+    var n = value.length
+
+    while (n--) res[n] = value[n]
+
+    return res
   }
-  return ret
 }
 
 module.exports = toArray
