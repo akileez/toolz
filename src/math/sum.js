98dab1da122019f55ec528a4665f57d7ed3d7f91
akileez <git@keeda.net>
2015-11-07 07:54:54 -0500

    update math/sum

--- src/math/sum.js
+++ src/math/sum.js
@@ -1,16 +1,16 @@
-var isNumber = require('./isNumber')
-var slice = require('../array/sliced')
+var isNumber = require('toolz/src/number/isFinite')
+var slice = require('toolz/src/array/slice')
+var filter = require('toolz/src/array/filter')
+var reduce = require('toolz/src/array/reduce')
 
 function sum (arr) {
   arr = Array.isArray(arr) ? arr : slice(arguments)
 
-  return arr
-    .filter(function (value) {
-      return isNumber(value) && (value != null)
-    })
-    .reduce(function (sum, item) {
-      return sum + item
-    })
+  return reduce(filter(arr, function (value) {
+      return isNumber(value)
+    }), function (sum, item) {
+    return sum + item
+  }, 0)
 }
 
 module.exports = sum
