5a41c0db0f5d64e881e600e4822f4c2d13aedd04
akileez <git@keeda.net>
2015-11-08 03:19:13 -0500

    update math/subtract

--- src/math/subtract.js
+++ src/math/subtract.js
@@ -1,11 +1,14 @@
 var scrub = require('./scrub')
+var slice = require('../array/slice')
+var reduce = require('../array/reduce')
 
 function sub (arr) {
-  return scrub(arr)
-    .reduce(function (diff, item, key) {
-      if (key === 0) return item
-      return diff - item
-    })
+  arr = Array.isArray(arr) ? arr : slice(arguments)
+
+  return reduce(scrub(arr), function (diff, item, key) {
+    if (key === 0) return item
+    return diff - item
+  }, 0)
 }
 
 module.exports = sub
