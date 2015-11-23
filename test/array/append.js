e28ef94e9e23c39baa3f9da12f13554b86def0b9
akileez <git@keeda.net>
2015-11-23 11:00:20 -0500

    spec file test/array/append

---
+++ test/array/append.js
@@ -0,0 +1,28 @@
+var test = require('../../src/assertion/ttr')
+var append = require('../../src/array/append')
+
+test('append all items of second array to end of first array', function (t) {
+  var arr1 = [1, 2, 3]
+  var arr2 = [3, 4, 5]
+  var res
+
+  res = append(arr1, arr2)
+  t.is(arr1, res)
+  t.same(arr1, [1,2,3,3,4,5])
+})
+
+test('append null array', function (t) {
+  var arr1 = [1, 2]
+
+  append(arr1, null)
+  t.same(arr1, [1, 2])
+})
+
+test('append undefined array', function (t) {
+  var arr1 = [1, 2, 3]
+
+  append(arr1, undefined)
+  t.same(arr1, [1, 2, 3])
+})
+
+test.result()
