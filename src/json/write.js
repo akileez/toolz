db27786ed245bd7af405d27c36681fc3bdbeaadf
akileez <git@keeda.net>
2015-12-03 12:41:59 -0500

    source file json/write -- dupe of file/writeJSON

---
+++ src/json/write.js
@@ -0,0 +1,49 @@
+var writeFile = require('../file/writeFile')
+var extend = require('../object/extend')
+
+var defaults = {
+  replacer: null,
+  spaces: 2
+}
+
+function writeJSON (file, obj, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = defaults
+  } else {
+    opts = extend(defaults, opts)
+  }
+
+  try {
+    var json = JSON.stringify(
+      obj,
+      opts.replacer,
+      opts.spaces
+    ) + '\n'
+  } catch (err) {
+    if (cb) return cb(err, null)
+  }
+
+  writeFile(file, json, cb)
+}
+
+function writeJSONsync (file, obj, opts) {
+  opts = opts
+    ? extend(defaults, opts)
+    : defaults
+
+  try {
+    var json = JSON.stringify(
+      obj,
+      opts.replacer,
+      opts.spaces
+    ) + '\n'
+  } catch (err) {
+    throw err
+  }
+
+  writeFile(file, json)
+}
+
+module.exports = writeJSON
+module.exports.sync = writeJSONsync
