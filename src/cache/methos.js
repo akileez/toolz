6a047bd0de3d1882e9ddfa5541254b4f3b0e3e8a
akileez <git@keeda.net>
2015-12-27 03:43:35 -0500

    source file cache/methos
    eventual replacement of cache/cfg.
    stampit_v3

---
+++ src/cache/methos.js
@@ -0,0 +1,50 @@
+var stampit  = require('../stamp/stampit')
+var set      = require('../object/set')
+var get      = require('../object/get')
+var has      = require('../object/has')
+var visit    = require('../object/visit')
+
+module.exports = stampit()
+  .methods({
+    set: setter,
+    get: getter,
+    has: hasit,
+    del: removeit
+  })
+  .initializers([
+    function () {
+      this.cache = {}
+    }
+  ])
+
+
+// sets 'value' to 'key' of the cache
+function setter (key, value) {
+  if (arguments.length === 1 && typeof key === 'object')
+    visit(this, 'set', key)
+  else set(this.cache, key, value)
+  if (this.emit) this.emit('set', key, value)
+  return this
+}
+
+// gets the cached value for 'key' or entire cache
+function getter (key) {
+  return key ? get(this.cache, key) : this.cache
+}
+
+// checks if a cached value for 'key exists'
+function hasit (key) {
+  if (key.indexOf('.') === -1) return this.cache.hasOwnProperty(key)
+  return has(this.cache, key)
+}
+
+// remove 'keys' from the cache. if no value specified, the entire cache is reset
+function removeit (key) {
+  if (!key) this.cache = {}
+  if (typeof key === 'object') visit(this, 'del', key)
+  else if (this.has(key)) {
+    delete this.cache[key]
+    if (this.emit) this.emit('del', key)
+  }
+  return this
+}
