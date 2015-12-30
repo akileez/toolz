a6c1a8864dc13bb9364c06d49a9d4f2583d263e8
akileez <git@keeda.net>
2015-12-30 01:55:54 -0500

    source file lang/defineProperty

--- src/lang/defineProperty.js
+++ src/lang/defineProperty.js
@@ -1,11 +1,13 @@
 // adopted from: define-property <https://github.com/jonschlinkert/define-property>
 // Copyright (c) 2015, Jon Schlinkert (MIT)
 
-function defineProperty (receiver, key, val) {
-  if (typeof val === 'object' && ('set' in val || 'get' in val))
-    return Object.defineProperty(receiver, key, val)
+var isDescriptor = require('./isDescriptor')
 
-  return Object.defineProperty(receiver, key, {
+function defineProperty (obj, prop, val) {
+  if (isDescriptor(val) && ('set' in val || 'get' in val))
+    return Object.defineProperty(obj, prop, val)
+
+  return Object.defineProperty(obj, prop, {
     configurable: true,
     enumerable: false,
     writable: true,
