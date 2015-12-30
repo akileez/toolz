2a6488aa1755f2e67869008b856012cb9c470262
akileez <git@keeda.net>
2015-12-30 01:59:22 -0500

    update object/hasOwn
    use Object.prototype.hasOwnProperty.call — this is to keep things safe
    in the event of ever using Object.create(null), which I may do.

--- src/object/hasOwn.js
+++ src/object/hasOwn.js
@@ -1,7 +1,7 @@
 function hasOwn (obj, prop) {
   // trying to eliminate call where I can:
-  // return Object.prototype.hasOwnProperty.call(obj, prop)
-  return obj.hasOwnProperty(prop)
+  return Object.prototype.hasOwnProperty.call(obj, prop)
+  // return obj.hasOwnProperty(prop)
 }
 
 module.exports = hasOwn
