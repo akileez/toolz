200aec887537b044c402830753db0bec614f24cd
akileez <git@keeda.net>
2015-12-31 12:56:40 -0500

    remove unused files

--- src/lang/isDescriptor.js
+++
@@ -1,8 +0,0 @@
-var isData = require('./isDescriptorData')
-var isAccessor = require('./isDescriptorAccessor')
-
-function isDescriptor (obj, key) {
-  return isData(obj, key) || isAccessor(obj, key)
-}
-
-module.exports = isDescriptor
