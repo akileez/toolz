e3e5e8eff5d638cab913cac2b1a0bfc73e4b6b02
akileez <git@keeda.net>
2016-01-02 11:09:25 -0500

    source file cache/config-flags

---
+++ src/cache/config-flags.js
@@ -0,0 +1,13 @@
+var stampit  = require('../object/stampit')
+var toFlags  = require('../string/toFlags')
+var keys = require('../object/keys')
+
+module.exports = stampit()
+  .methods({
+    toArgs: flags
+  })
+
+function flags (keyz) {
+  keyz = keyz || keys(this.config)
+  return toFlags(this.config, keyz)
+}
