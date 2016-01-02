c89ed50197eb0f871a4f9cc5cacb3fe0b5bc3add
akileez <git@keeda.net>
2016-01-02 11:09:49 -0500

    source file cache/config-opts

---
+++ src/cache/config-opts.js
@@ -0,0 +1,53 @@
+var stampit  = require('../object/stampit')
+var has      = require('../object/has')
+
+module.exports = stampit()
+  .methods({
+    isEnabled: enabled,
+    isDisabled: disabled,
+    isTrue: isTrue,
+    isFalse: isFalse,
+    isBoolean: isBoolean,
+    hasOption: hasOption,
+    canDisable: disableCheck,
+    canEnable: enableCheck
+  })
+
+function enabled (key) {
+  return Boolean(this.config[key])
+}
+
+function disabled (key) {
+  return !Boolean(this.config[key])
+}
+
+function isTrue (key) {
+  return this.config[key] === true
+}
+
+function isFalse (key) {
+  return this.config[key] === false
+}
+
+function isBoolean (key) {
+  return typeof this.config[key] === 'boolean'
+}
+
+function hasOption (key) {
+  if (key.indexOf('.') === -1) return this.config.hasOwnProperty(key)
+  return has(this.config, key)
+}
+
+function disableCheck (key) {
+  if (this.hasOption(key) && this.isTrue(key)) {
+    this.disable(key)
+  }
+
+  return this
+}
+
+function enableCheck (key) {
+  return (this.hasOption(key) && this.isFalse(key))
+    ? this.enable(key)
+    : false
+}
