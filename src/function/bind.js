dc491d28c3b4243cc4bef3712dd6261d7b725994
akileez <git@keeda.net>
2016-01-03 05:53:45 -0500

    update function/bind -- use function/apply

--- src/function/bind.js
+++ src/function/bind.js
@@ -1,9 +1,10 @@
 var slice = require('../array/slice')
+var apply = require('./apply')
 
 function bind (fn, context, args) {
   var argsArr = slice(arguments, 2) // curried args
   return function () {
-    return fn.apply(context, argsArr.concat(slice(arguments)))
+    return apply(fn, context, argsArr.concat(slice(arguments)))
   }
 }
 
