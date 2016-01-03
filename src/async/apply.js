3354def9e990d1d8e1961379ff3e23ac3c8034a5
akileez <git@keeda.net>
2016-01-03 05:53:09 -0500

    update async/apply -- use function/apply

--- src/async/apply.js
+++ src/async/apply.js
@@ -1,11 +1,12 @@
 var rest = require('../function/rest')
+var apply = require('../function/apply')
 
 // adopted from async.apply
 
 function apply (fn, args) {
   return rest(function (fn, args) {
     return rest(function (callArgs) {
-      return fn.apply(null, args.concat(callArgs))
+      return apply(fn, null, args.concat(callArgs))
     })
   })
 }
