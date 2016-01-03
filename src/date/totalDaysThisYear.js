0af3d4982db150758fc43ba1f79a344b311fe78e
akileez <git@keeda.net>
2016-01-03 15:26:17 -0500

    update date/totalDaysThisYear
    bug fix .. something gets thrown off because of leap year. this format
    clears the problem.

--- src/date/totalDaysThisYear.js
+++ src/date/totalDaysThisYear.js
@@ -1,7 +1,7 @@
 var isLeapYear = require('./isLeapYear')
 
 function daysInTheYear (date) {
-  var thisYear = new Date(date.getFullYear())
+  var thisYear = new Date(date).getFullYear()
   return isLeapYear(thisYear) ? 366 : 365
 }
 
