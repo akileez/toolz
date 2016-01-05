cd04ad00bc3c5d9563b183b0e6c99cd8a3e7b4b3
akileez <git@keeda.net>
2016-01-05 16:37:26 -0500

    update cache/utils -- add comments

--- src/cache/utils.js
+++ src/cache/utils.js
@@ -1,12 +1,14 @@
 var stamp  = require('../object/stampit')
 
 module.exports = stamp.methods({
+  // reset keys on the cache property
   reset: function (key) {
     if (this.has(key)) this.del(key)
     this.set(key, {})
     if (this.emit) this.emit('set', key, 'reset')
     return this
   },
+  // look up any enumerable property on a given factory function
   look: function (key) {
     return this[key]
   }
