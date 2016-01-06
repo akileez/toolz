b1997a9ce409baf48c6185fb3c661f4ac1cf99ad
akileez <git@keeda.net>
2016-01-06 05:26:44 -0500

    string collection refactor: to-space-case

---
+++ src/string/to-space-case.js
@@ -0,0 +1,12 @@
+var toNoCase = require('./to-no-case')
+
+// ianstormtaylor/to-space-case
+// convert a 'string' to space case
+
+function toSpaceCase (str) {
+  return toNoCase(str).replace(/[\W_]+(.|$)/g, function (matches, match) {
+    return match ? ' ' + match : ''
+  })
+}
+
+module.exports = toSpaceCase
