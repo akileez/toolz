b36f66a9b9d42552fb0b98a0ee8623bd70540ca9
akileez <git@keeda.net>
2016-01-06 08:38:38 -0500

    source file glob/multimatch

---
+++ src/glob/multimatch.js
@@ -0,0 +1,28 @@
+'use strict';
+var minimatch = require('./minimatch');
+var arrayUnion = require('../array/union');
+var arrayDiffer = require('../array/difference');
+var arrify = require('../array/convert');
+
+module.exports = function (list, patterns, options) {
+  list = arrify(list);
+  patterns = arrify(patterns);
+
+  if (list.length === 0 || patterns.length === 0) {
+    return [];
+  }
+
+  options = options || {};
+
+  return patterns.reduce(function (ret, pattern) {
+    var process = arrayUnion;
+
+    if (pattern[0] === '!') {
+      pattern = pattern.slice(1);
+      process = arrayDiffer;
+    }
+
+    return process(ret, minimatch.match(list, pattern, options));
+  }, []);
+};
+
