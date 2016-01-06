7fae2297800c9367a4a2cb1074412caa6da7b1b8
akileez <git@keeda.net>
2016-01-06 05:26:15 -0500

    string collection refactor: to-slug-case

---
+++ src/string/to-slug-case.js
@@ -0,0 +1,18 @@
+var toSpaceCase = require('./to-space-case')
+
+// convert a 'string' to slug case
+
+function toSlugCase (str) {
+  return toSpaceCase(str).replace(/\s/g, '-')
+}
+
+module.exports = toSlugCase
+
+/*
+    var slug = require('to-slug-case');
+
+    slug('camelCase');  // "camel-case"
+    slug('space case'); // "snake-case"
+    slug('dot.case');   // "dot-case"
+    slug('weird[case'); // "weird-case"
+ */
