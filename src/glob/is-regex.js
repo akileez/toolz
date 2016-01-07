d5b698ea860fcf70165b55f1650834648b2452f4
akileez <git@keeda.net>
2016-01-07 15:14:14 -0500

    source file globe/is-regex

---
+++ src/glob/is-regex.js
@@ -0,0 +1,29 @@
+// adopted from: is-regex <https://github.com/ljharb/is-regex>
+// Copyright (c) 2014 Jordan Harband (MIT)
+
+'use strict';
+
+function isRegex (value) {
+  if (typeof value !== 'object') return false
+
+  var toStr = Object.prototype.toString
+  var regexClass = '[object RegExp]'
+  var hasToStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag === 'symbol'
+
+  return hasToStringTag
+    ? tryRegexExec(value)
+    : toStr.call(value) === regexClass
+}
+
+function tryRegexExec (value) {
+  var regexExec = RegExp.prototype.exec
+
+  try {
+    regexExec.call(value)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+module.exports = isRegex
