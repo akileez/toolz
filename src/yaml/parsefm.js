f76c33b43bc7ecdec7fff900543e04aa55107925
akileez <git@keeda.net>
2016-01-09 15:32:55 -0500

    refactor yaml/parsefm -- strip down to essentials
    use file/vfo to recreate same effects

--- src/yaml/parsefm.js
+++ src/yaml/parsefm.js
@@ -1,52 +1,15 @@
-var fs      = require('fs')
-var path    = require('path')
-
-// yamljs
+var readFile = require('../file/readFile')
 var extract = require('./fme')
 
-// front-matter-extractor
-
-function parse (input, opts, cb) {
-  if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
-  }
-
-  fs.readFile(input, 'utf8', function (err, res) {
+function parse (input, cb) {
+  readFile(input, 'utf8', function (err, res) {
     if (err) return cb(err)
-    var fm = extract(res)
-
-    cb(null, opts.extend ? extend(fm, input) : fm)
+    cb(null, extract(res))
   })
 }
 
-function parseSync (input, opts) {
-  opts = opts || {}
-  var str = fs.readFileSync(input, 'utf8')
-  var fm = extract(str)
-
-  return opts.extend ? extend(fm, input) : fm
-}
-
-function extend (obj, fp) {
-  return {
-    abs     : fs.realpathSync(fp),
-    rel     : fp,
-    file    : pstats(fp),
-    stats   : fs.lstatSync(fp),
-    data    : obj.data,
-    content : obj.content
-  }
-}
-
-function pstats (fp) {
-  return {
-    root : process.cwd().split(/[\\\/]/g).slice(-1)[0],
-    dir  : path.dirname(fp),
-    base : path.basename(fp),
-    ext  : path.extname(fp),
-    name : path.basename(fp, path.extname(fp))
-  }
+function parseSync (input) {
+  return extract(readFile(input))
 }
 
 module.exports = parse
