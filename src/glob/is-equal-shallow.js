4b2666e1f7e8168ed59621a5c93ab7f749e78dfe
akileez <git@keeda.net>
2016-01-11 13:23:56 -0500

    source file glob/is-equal-shallow -- (micromatch)

---
+++ src/glob/is-equal-shallow.js
@@ -0,0 +1,30 @@
+// adopted from: is-equal-shallow <https://github.com/jonschlinkert/is-equal-shallow>
+//
+'use strict'
+
+var isPrimitive = require('../lang/isPrimitive')
+
+function isEqual (a, b) {
+  if (!a && !b) return true
+  if (!a && b || a && !b) return false
+
+  var numA = 0
+  var numB = 0
+  var key
+
+  for (key in b) {
+    numB++
+    if (isPrimitive.Not(b[key])
+      || !a.hasOwnProperty(key)
+      || (a[key] !== b[key])
+    ) return false
+  }
+
+  for (key in a) {
+    numA++
+  }
+
+  return numA === numB
+}
+
+module.exports = isEqual
