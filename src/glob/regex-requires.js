06eb606de0a9503f1ab5872919299ab8a673bd67
akileez <git@keeda.net>
2016-01-11 13:17:14 -0500

    source file glob/regex-requires -- (micromatch)

---
+++ src/glob/regex-requires.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/**
+ * Regular expression for matching require statements.
+ */
+
+module.exports = function () {
+  return /(?:(?:var|const)\s*(.*?)\s*=\s*)?require\(['"]([^'"]+)['"](?:, ['"]([^'"]+)['"])?\);?/
+}
