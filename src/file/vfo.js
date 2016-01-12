c7989adb5cbb951eda54c7935731f9e7027a99de
akileez <git@keeda.net>
2016-01-12 02:37:14 -0500

    update file/vfo
    consolidate info of vfp and vfs into vfo and make two exports. some
    duplication but worth it since two files are removed.

--- src/file/vfo.js
+++ src/file/vfo.js
@@ -7,28 +7,39 @@ var fs     = require('fs')
 
   fp (filepath) [string] file path to read
   fc (filecontent) [object|function] object or function returning an object to mixin
-  s (stats?) [boolean] include file stats
 
 */
 
-function virtualFileObject (fp, fc, s) {
+// virtualFileObject: compose normalize file object
+function vfo (fp, fc) {
   if (typeof fc === 'function') {
-    return extend(vfp(fp), vfs(fp, s), fc(fp))
+    return extend(vfp(fp), fc(fp))
   }
 
   if (typeof fc === 'object' && fc.constructor === Object) {
-    return extend(vfp(fp), vfs(fp, s), fc)
+    return extend(vfp(fp), fc)
   }
 }
 
- // virtualFilePath
+// virtualFileStats: include file stats metadata
+function vfs (fp, fc) {
+  if (typeof fc === 'function') {
+    return extend(vfp(fp), {stats: lstat(fp)}, fc(fp))
+  }
+
+  if (typeof fc === 'object' && fc.constructor === Object) {
+    return extend(vfp(fp), {stats: lstat(fp)}, fc)
+  }
+}
+
+ // virtualFilePath: file path metadata
 function vfp (fp) {
   return {
     abs  : fs.realpathSync(fp),
     rel  : fp,
     file : {
       root : process.cwd().split(/[\\\/]/g).slice(-1)[0],
-      dir  : path.dirname(fp),
+      home : path.dirname(fp),
       name : path.basename(fp),
       base : path.basename(fp, path.extname(fp)),
       ext  : path.extname(fp)
@@ -36,9 +47,5 @@ function vfp (fp) {
   }
 }
 
-// virtualFileStats
-function vfs (fp, bool) {
-  return !!bool ? {stats: lstat(fp)} : {}
-}
-
-module.exports = virtualFileObject
+module.exports = vfo
+module.exports.stats = vfs
