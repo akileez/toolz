a77857c8fbd702f09ecfa856b6ba6128a6491e4a
akileez <git@keeda.net>
2016-01-14 12:28:34 -0500

    refactor cli/execa-commands -- much better flow.

--- src/cli/execa-commands.js
+++ src/cli/execa-commands.js
@@ -12,55 +12,67 @@ var npmRunPath = require('../path/npm-run-path')
 var TEN_MEBIBYTE = 1024 * 1024 * 10
 
 // cmds: arrary/object of objects {cmd: 'something', args: ['some', 'things']}
-// cp: concurrent processing?. [boolean]
+// opts: options object
 // cb: callback [function]
 
-function commands(cmds, cp, cb) {
-  if (arguments.length === 2) {
-    cb = cp
-    cp = false
+function commands(cmds, opts, cb) {
+  function defs(opts) {
+    opts = opts || {}
+    return {
+      maxBuffer: TEN_MEBIBYTE,
+      preferLocal: false,
+      concurrent: opts.concurrent || false
+    }
   }
 
-  var iterate = cp ? concurrent : serial
+  if (arguments.length === 1) {
+    cb = function () {}
+    opts = defs()
+  }
 
+  if (arguments.length === 2) {
+    cb = opts
+    opts = defs()
+  }
+
+  if (arguments.length === 3) opts = defs(opts)
+
+  if (opts.preferLocal) {
+    opts.env = extend({}, opts.env || process.env)
+
+    opts.env[pathKey] = npmRunPath({
+      cwd: opts.cwd,
+      path: opts.env[pathKey]
+    })
+  }
+
+  var iterate = opts.concurrent ? concurrent : serial
+
+  // this may be converted to a Promise-like function
   iterate(cmds, function (cmd, idx, next) {
     var command =  cmd.cmd
     var args = cmd.args
 
-    var opts = extend({
-      maxBuffer: TEN_MEBIBYTE,
-      preferLocal: false
-    }, cmd.opts || {})
-
-    if (opts.preferLocal) {
-      opts.env = extend({}, opts.env || process.env)
-
-      opts.env[pathKey] = npmRunPath({
-        cwd: opts.cwd,
-        path: opts.env[pathKey]
-      })
-    }
-
     child.execFile(command, args, opts, function (err, stdout, stderr) {
-      if (err) console.log(err)
-      if (stdout) console.log(stdout)
-      if (stderr) console.log(stderr)
+      if (err) process.stderr.write('\u001b[31m' + err.message + '\u001b[39m')
+      if (stdout) process.stdout.write(stdout)
+      if (stderr) process.stdout.write(stderr)
       next()
     })
   }, cb)
 }
 
-// this is crap right now. will fix soon.
+// need to deal with objects and strings (visit pattern?)
 commands.sync = function (cmds) {
   cmds = Array.isArray(cmds) ? cmds : [cmds]
 
   forEach(cmds, function (cmd) {
-    var command = ['-c', cmd.cmd].concat(cmd.args)
-    var opts = {}
-    return module.exports([{cmd: '/bin/sh', args: command}], function () {
+    var opts = extend({
+      maxBuffer: TEN_MEBIBYTE,
+      stdio: 'inherit'
+    }, cmd.opts || {})
 
-    })
-    // child.execFileSync(command.join(' '), opts)
+    child.execFileSync(cmd.cmd, cmd.args, opts)
   })
 }
 
