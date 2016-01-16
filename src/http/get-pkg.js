33981e086de20eebc4137dd333f26c03b3272770
akileez <git@keeda.net>
2016-01-16 06:19:25 -0500

    lint http/get-pkg

--- src/http/get-pkg.js
+++ src/http/get-pkg.js
@@ -1,32 +1,31 @@
-/*!
- * get-pkgs <https://github.com/jonschlinkert/get-pkgs>
- *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
- */
+// get-pkgs <https://github.com/jonschlinkert/get-pkgs>
+// Copyright (c) 2014-2015, Jon Schlinkert.
 
-'use strict';
+'use strict'
 
 var request = require('./min-request')
 
-module.exports = function getPkg(name, version, cb) {
+module.exports = function getPkg (name, version, cb) {
   if (typeof version === 'function') {
-    cb = version;
-    version = '';
+    cb = version
+    version = ''
   }
 
-  var url = 'https://registry.npmjs.org/' + name + '/';
-  if (!version) version = 'latest';
+  var url = 'https://registry.npmjs.org/' + name + '/'
+  if (!version) version = 'latest'
 
   request(url + version, {}, function (err, res) {
-    if (err) return cb(err);
+    if (err) return cb(err)
     if (res.statusCode === 500) {
-      return cb(new Error(res.statusMessage));
+      return cb(new Error(res.statusMessage))
     }
-    var pkg = JSON.parse(res.body);
+
+    var pkg = JSON.parse(res.body)
+
     if (pkg.error && pkg.reason) {
-      return cb(new Error(pkg.reason));
+      return cb(new Error(pkg.reason))
     }
-    cb(null, pkg);
-  });
+
+    cb(null, pkg)
+  })
 }
