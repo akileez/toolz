26efe88819e28dde2dc7496aacf06ffc15d223a4
akileez <git@keeda.net>
2016-01-17 13:18:56 -0500

    refactor time/clockin -- date/dateFormat in one file

--- src/time/clockin.js
+++ src/time/clockin.js
@@ -1,36 +1,35 @@
 /*!
  * clockin <https://github.com/akileez/clockin>
  *
- * Copyright (c) 2015 Keith Williams.
+ * Copyright (c) 2015-2016 Keith Williams.
  * Licensed under the ISC license.
  */
 
 var frmt = {
-    'default'             : 'ddd mmm dd yyyy HH:MM:SS',
-    'now'                 : 'dddd, mmmm o, yyyy HH:MM:SS \'GMT\'Z',
-    'shortDate'           : 'm/d/yy',
-    'mediumDate'          : 'mmm d, yyyy',
-    'longDate'            : 'mmmm d, yyyy',
-    'fullDate'            : 'dddd, mmmm d, yyyy',
-    'shortTime'           : 'h:MM TT',
-    'mediumTime'          : 'h:MM:SS TT',
-    'longTime'            : 'h:MM:SS TT Z',
-    'isoDate'             : 'yyyy-mm-dd',
-    'isoTime'             : 'HH:MM:SS',
-    'iso'                 : 'yyyy-mm-dd\'T\'HH:MM:SSZ',
-    'isoDateTime'         : 'yyyy-mm-dd\'T\'HH:MM:SS:ss',
-    'isoUtcDateTime'      : 'yyyy-mm-dd\'T\'HH:MM:SS\'Z\'',
-    'expiresHeaderFormat' : 'ddd, dd mmm yyyy HH:MM:SS Z',
-    'timestamp'           : 'yyyymmddHHMMSSss'
+  'default'             : 'ddd mmm dd yyyy HH:MM:SS',
+  'now'                 : 'dddd, mmmm o, yyyy HH:MM:SS \'GMT\'Z',
+  'shortDate'           : 'm/d/yy',
+  'mediumDate'          : 'mmm d, yyyy',
+  'longDate'            : 'mmmm d, yyyy',
+  'fullDate'            : 'dddd, mmmm d, yyyy',
+  'shortTime'           : 'h:MM TT',
+  'mediumTime'          : 'h:MM:SS TT',
+  'longTime'            : 'h:MM:SS TT Z',
+  'isoDate'             : 'yyyy-mm-dd',
+  'isoTime'             : 'HH:MM:SS',
+  'iso'                 : 'yyyy-mm-dd\'T\'HH:MM:SSZ',
+  'isoDateTime'         : 'yyyy-mm-dd\'T\'HH:MM:SS:ss',
+  'isoUtcDateTime'      : 'yyyy-mm-dd\'T\'HH:MM:SS\'Z\'',
+  'expiresHeaderFormat' : 'ddd, dd mmm yyyy HH:MM:SS Z',
+  'timestamp'           : 'yyyymmddHHMMSSss'
 }
 
-var cal = {
-  days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  mons: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-}
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-module.exports = function (date, mask) {
-  if (arguments.length === 1 && kindOf(date) === 'string' && !/\d/.test(date)) {
+var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+function clockin (date, mask) {
+  if (arguments.length === 1 && typeof date === 'string' && !/\d/.test(date)) {
     mask = date
     date = undefined
   }
@@ -41,92 +40,122 @@ module.exports = function (date, mask) {
   else date = new Date(Date.parse(date))
   if (isNaN(date)) throw TypeError('Invalid date')
 
-  var thisday = date.getDay()
-  var thismonth = date.getMonth()
-  var thisyear = date.getFullYear()
+  // what we wish to match
+  var token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMSsTt])\1?|[oZWQNv]|O{1,2}|u{1,2}|x{1,3}|'[^']*'/g
+  // what we don't wish to match -- need to optimize!!
+  var antiToken = /[AaBbCcDEeFfGgIiJjKkLlnPpqRrUVwXYz0-9\?\.\,\!@#\$%\^\&\*\(\)\+\-]/g
 
-  var day = date.getDate()
-  var month = date.getMonth() + 1
-  var year = thisyear.toString()
-  var hours = date.getHours()
-  var hr = hours % 12 || 12
-  var mins = date.getMinutes()
-  var secs = date.getSeconds()
-  var ms = date.getMilliseconds()
+  // what mask to use
+  mask = String(frmt[mask] || antiPattern(mask, antiToken) || frmt.default)
 
-  var offset = date.getTimezoneOffset()
-  var GMT = (offset > 0 ? '-' : '+') + pad(Math.floor(Math.abs(offset) / 60) * 100 + Math.abs(offset) % 60, 4)
-
-  var ordinal = day + ['th', 'st', 'nd', 'rd'][day % 10 > 3 ? 0 : (day % 100 - day % 10 != 10) * day % 10]
-
-  var dayNames = cal.days.filter(dayOfWeek).toString()
-  var monNames = cal.mons.filter(monthOfYear).toString()
-  var thisweek = getWeek()
-  var thisquarter = getQuarter()
-  var daythisyear = getDayNumOfYear()
-  var daysthisyear = daysInYear()
-  var daysthismonth = daysInMonth()
-  var daysleftthisweek = daysLeftThisWeek()
-  var daysleftthismonth = daysLeftThisMonth()
-  var daysleftthisyear = daysLeftThisYear()
-
-  var token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMSsTt])\1?|[LloOZWQNv]|u{1,2}|x{1,3}|'[^']*'/g
-  var flags = {
-    d        : day,
-    dd       : pad(day),
-    ddd      : dayNames.slice(0, 3),
-    dddd     : dayNames,
-    m        : month,
-    mm       : pad(month),
-    mmm      : monNames.slice(0, 3),
-    mmmm     : monNames,
-    yyyy     : year,
-    yy       : year.slice(2),
-    h        : hr,
-    hh       : pad(hr),
-    H        : hours,
-    HH       : pad(hours),
-    M        : mins,
-    MM       : pad(mins),
-    S        : secs,
-    SS       : pad(secs),
-    s        : ms,
-    ss       : pad(ms, 3),
-    t        : hours < 12 ? ' a' : ' p',
-    tt       : hours < 12 ? ' am' : ' pm',
-    T        : hours < 12 ? ' A' : ' P',
-    TT       : hours < 12 ? ' AM' : ' PM',
-    N        : daythisyear,
-    W        : thisweek,
-    Q        : thisquarter,
-    Z        : GMT,
-    u        : daysthismonth,
-    uu       : daysthisyear,
-    x        : daysleftthisweek,
-    xx       : daysleftthismonth,
-    xxx      : daysleftthisyear,
-    v        : isLeapYear(),
-    o        : ordinal,
-    O        : dayNames.slice(0, 2),
-    L        : dayNames.slice(0, 1)
+  function makeIterator (fn) {
+    return function (mask, token) {
+      return fn(date, mask)
+    }
   }
 
-  mask = String(frmt[mask] || mask || frmt.default)
-  // process.stdout.write('mask: ' + mask + '  ')
-  return mask.replace(token, function (match) {
-    if (match in flags) return flags[match]
-    return match.slice(1, match.length - 1)
-  })
+  return mask.replace(token, makeIterator(convertFlag))
 
-  function monthOfYear (val, idx, arr) {
-    if (idx === thismonth) return val
+  function convertFlag (date, token) {
+    var tokens = {
+      d     : () => date.getDate(),
+      dd    : () => pad(date.getDate(), 2),
+      ddd   : () => nameOfDay(date, 3),
+      dddd  : () => nameOfDay(date),
+      m     : () => date.getMonth() + 1,
+      mm    : () => pad(date.getMonth() + 1, 2),
+      mmm   : () => nameOfMonth(date, 3),
+      mmmm  : () => nameOfMonth(date),
+      yyyy  : () => pad(date.getFullYear(), 4),
+      yy    : () => pad(date.getFullYear() % 100, 2),
+      h     : () => date.getHours() % 12 || 12,
+      hh    : () => pad(date.getHours() % 12 || 12, 2),
+      H     : () => date.getHours(),
+      HH    : () => pad(date.getHours(), 2),
+      M     : () => date.getMinutes(),
+      MM    : () => pad(date.getMinutes(), 2),
+      S     : () => date.getSeconds(),
+      SS    : () => pad(date.getSeconds(), 2),
+      s     : () => date.getMilliseconds(),
+      ss    : () => pad(date.getMilliseconds(), 3),
+      t     : () => date.getHours() < 12 ? 'a' : 'p',
+      tt    : () => date.getHours() < 12 ? 'am' : 'pm',
+      T     : () => date.getHours() < 12 ? 'A' : 'P',
+      TT    : () => date.getHours() < 12 ? 'AM' : 'PM',
+      N     : () => dayOfTheYear(date),
+      W     : () => weekOfTheYear(date),
+      Q     : () => quarterOfTheYear(date),
+      Z     : () => timezoneOffset(date),
+      u     : () => totalDaysThisMonth(date),
+      uu    : () => totalDaysThisYear(date),
+      x     : () => daysLeftThisWeek(date),
+      xx    : () => daysLeftThisMonth(date),
+      xxx   : () => daysLeftThisYear(date),
+      v     : () => isLeapYear(date),
+      o     : () => ordinal(date.getDate()),
+      O     : () => nameOfDay(date, 1),
+      OO    : () => nameOfDay(date, 2),
+      defaults : () => token.replace(/'/g, '')
+    }
+    return typeof tokens[token] !== 'function'
+      ? tokens.defaults()
+      : tokens[token]()
   }
 
-  function dayOfWeek (val, idx, arr) {
-    if (idx === thisday) return val
+  function antiPattern (mask, token) {
+    if (!token.test(mask)) return mask
+    return false
   }
 
-  function getWeek() {
+  function nameOfDay (date, n) {
+    var thisday = date.getDay()
+    var name = dayNames.filter(dayOfWeek).toString()
+
+    function dayOfWeek (val, idx, arr) {
+      if (idx === thisday) return val
+    }
+
+    if (n == null) return name
+    else return abbreviateDayName(name, n)
+
+    function abbreviateDayName (name, num) {
+      if (num > 3) {
+        if (name === 'Tuesday') return name.slice(0, 4)
+        else if (name === 'Thursday' && num < 6) return name.slice(0, num)
+        else return name.slice(0, 3)
+      }
+      return name.slice(0, num)
+    }
+  }
+
+  function nameOfMonth (date, n) {
+    var thismonth = date.getMonth()
+    var name = monthNames.filter(monthOfYear).toString()
+
+    function monthOfYear (val, idx, arr) {
+      if (idx === thismonth) return val
+    }
+
+    if (n == null) return name
+    else return abbreviateName(name, n)
+
+    function abbreviateName (name, num) {
+      if (num > 3) num = 3
+      return name.slice(0, num)
+    }
+  }
+
+  function dayOfTheYear (date) {
+    // var now = new Date(date.getTime())
+    // var then = new Date(date.getFullYear(), 0, 1)
+
+    var now = date.getTime()
+    var then = new Date(date.getFullYear(), 0, 1)
+
+    return Math.ceil((now - then) / 86400000)
+  }
+
+  function weekOfTheYear () {
     // Remove time components of date
     var targetThursday = new Date(date.getFullYear(), date.getMonth(), date.getDate())
 
@@ -148,40 +177,66 @@ module.exports = function (date, mask) {
     return 1 + Math.floor(weekDiff)
   }
 
-  // testing new quarter calculation.
-  function getQuarter () {
+  function quarterOfTheYear (date) {
+    var month = date.getMonth() + 1
     return (Math.ceil(month / 3))
   }
 
-  function isLeapYear () {
-    return thisyear % 400 === 0 || (thisyear % 100 !== 0 && thisyear % 4 === 0)
+  function timezoneOffset (date) {
+    var offset = date.getTimezoneOffset()
+    var abs = Math.abs(offset)
+    var hr = pad(Math.floor(abs / 60), 2)
+    var min = pad(abs % 60, 2)
+    return [(offset > 0 ? '-' : '+'), hr, min].join('')
   }
 
-  function daysInMonth () {
+  function totalDaysThisMonth (date) {
+    var year = date.getFullYear()
+    var month = date.getMonth() + 1
+
     return new Date(year, month, 0).getDate()
   }
 
-  function daysInYear () {
-    return isLeapYear(thisyear) ? 366 : 365
+  function totalDaysThisYear (date) {
+    var thisYear = new Date(date).getFullYear()
+    return isLeapYear(thisYear) ? 366 : 365
   }
 
-  function daysLeftThisWeek () {
-    return 6 - thisday
+  function isLeapYear (date) {
+    var thisYear = isDate(date)
+      ? new Date(date).getFullYear()
+      : new Date().getFullYear()
+
+    return thisYear % 400 === 0 || (thisYear % 100 !== 0 && thisYear % 4 === 0)
   }
 
-  function daysLeftThisMonth () {
-    return daysInMonth() - day
+  function daysLeftThisWeek (date) {
+    return 6 - date.getDay()
   }
 
-  function daysLeftThisYear () {
-    return daysthisyear - daythisyear
+  function daysLeftThisMonth (date) {
+    return totalDaysThisMonth(date) - date.getDate()
   }
 
-  function getDayNumOfYear () {
-    var now = new Date(date.getTime())
-    var then = new Date(date.getFullYear(), 0, 1)
+  function daysLeftThisYear (date) {
+    return totalDaysThisYear(date) - dayOfTheYear(date)
+  }
 
-    return Math.ceil((now - then) / 86400000)
+  function ordinal (n) {
+    n = toInt(n)
+    return n + nth(n)
+  }
+
+  function nth (i) {
+    return ['th', 'st', 'nd', 'rd'][i % 10 > 3 ? 0 : (i % 100 - i % 10 !== 10) * i % 10]
+  }
+
+  function toInt (val) {
+    return ~~val
+  }
+
+  function isDate (value) {
+    return Object.prototype.toString.call(value) === '[object Date]'
   }
 
   function pad (val, len) {
@@ -192,25 +247,6 @@ module.exports = function (date, mask) {
     }
     return val
   }
-
-  function kindOf(val) {
-    if (val === null) {
-      return 'null'
-    }
-
-    if (val === undefined) {
-      return 'undefined'
-    }
-
-    if (typeof val !== 'object') {
-      return typeof val
-    }
-
-    if (Array.isArray(val)) {
-      return 'array'
-    }
-
-    return {}.toString.call(val)
-      .slice(8, -1).toLowerCase()
-  }
 }
+
+module.exports = clockin
