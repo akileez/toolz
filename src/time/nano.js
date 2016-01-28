a542d692bd5c46ff6a8d9b3fbde6247f216e26cc
akileez <git@keeda.net>
2016-01-28 13:10:15 -0500

    source file -- time/nano

---
+++ src/time/nano.js
@@ -0,0 +1,46 @@
+var precise = require('toolz/src/number/enforcePrecision')
+
+/*
+  convert a high resolution time in nanoseconds to another unit of time
+
+  val            : high resolution time diff (process.hrtime(process.hrtime())
+  conversionUnit : convert to specified time units. default: ms
+  precision      : number of decimal places. default: 9
+
+  ------------------------------------------------------------------------
+
+  var convert = require('nano')
+  var start = process.hrtime()
+
+  process.nextTick(() => {
+    var end = process.hrtime(start)
+    console.log('elasped time took %d seconds', convert(end, 's', 9))
+  })
+
+*/
+
+function convertDiff (diffHRTimes, conversionUnit, precision) {
+  function nano (time) {
+    return time[0] * 1e9 + time[1]
+  }
+
+  var getUnit = {
+    'm'      : 6e10,
+    'minute' : 6e10,
+    's'      : 1e9,
+    'second' : 1e9,
+    'ms'     : 1e6,
+    'milli'  : 1e6,
+    'µs'     : 1e3,
+    'micro'  : 1e3,
+    'ns'     : 1,
+    'nano'   : 1
+  }
+
+  conversionUnit = conversionUnit || 'ms'
+  precision = precision || 9
+
+  return precise(nano(diffHRTimes) / getUnit[conversionUnit], precision)
+}
+
+module.exports = convertDiff
