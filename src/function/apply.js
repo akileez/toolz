ce6e1d6695778bf6e9e1d5574e52cdde8a5b731a
akileez <git@keeda.net>
2016-01-30 14:33:32 -0500

    update function/apply -- reorder arguments for fastApplyNoThrow

--- src/function/apply.js
+++ src/function/apply.js
@@ -55,12 +55,12 @@ function fastApply (fn, context, args) {
 
 // concept adopted from: https://github.com/wilmoore/apply-or.js
 // reasoning: Function.prototype.apply is normally sufficient; however, there are situations where it is useful to treat a value as a Function (invoke it) only if it is indeed a Function; otherwise, return it as-is. Calling .apply on a value that is not a function would cause an error.
-function fastApplyNoThrow (fn, args, context) {
+function fastApplyNoThrow (fn, context, args) {
   fn = (typeof fn !== 'function')
     ? identity
     : fn
 
-  return fastApply(fn, args, context)
+  return fastApply(fn, context, args)
 }
 
 function identity (value) {
