a0d4386192ab3616268aaea427c0a44b8d3f9c49
akileez <git@keeda.net>
2016-01-31 16:43:38 -0500

    update text/stripJSON -- compatibility update

--- src/text/stripJSON.js
+++ src/text/stripJSON.js
@@ -4,12 +4,24 @@
 var singleComment = 1
 var multiComment  = 2
 
-function stripJSON (str) {
+function stripWithoutWhitespace () {
+  return ''
+}
+
+function stripWithWhitespace (str, start, end) {
+  return str.slice(start, end).replace(/\S/g, ' ')
+}
+
+function stripJSON (str, opts) {
+  opts = opts || {}
+
   var currentChar
   var nextChar
   var insideString = false
   var insideComment = false
+  var offset = 0
   var ret = ''
+  var strip = opts.whitespace === false ? stripWithoutWhitespace : stripWithWhitespace
   var i = -1
   var len = str.length
 
@@ -23,38 +35,42 @@ function stripJSON (str) {
     }
 
     if (insideString) {
-      ret += currentChar
       continue
     }
 
     if (!insideComment && currentChar + nextChar === '//') {
+      ret += str.slice(offset, i)
+      offset = i
       insideComment = singleComment
       i++
     } else if (insideComment === singleComment && currentChar + nextChar === '\r\n') {
-      insideComment = false
       i++
-      ret += currentChar
-      ret += nextChar
+      insideComment = false
+      ret += strip(str, offset, i)
+      offset = i
       continue
     } else if (insideComment === singleComment && currentChar === '\n') {
       insideComment = false
+      ret += strip(str, offset, i)
+      offset = i
     } else if (!insideComment && currentChar + nextChar === '/*') {
+      ret += str.slice(offset, i)
+      offset = i
       insideComment = multiComment
       i++
       continue
     } else if (insideComment === multiComment && currentChar + nextChar === '*/') {
-      insideComment = false
       i++
+      insideComment = false
+      ret += strip(str, offset, i + 1)
+      offset = i + 1
       continue
     }
-
-    if (insideComment) continue
-
-    ret += currentChar
   }
 
   // remove new line character when proceeded by one or more spaces
-  return ret.replace(/( ){1,}(\r\n|\n){1,}/g, '')
+  // return ret.replace(/( ){1,}(\r\n|\n){1,}/g, '')
+  return ret + str.substr(offset)
 }
 
 module.exports = stripJSON
