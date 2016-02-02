a80de4e2131f7f68ae1493743ba6b85bd6aa0250
akileez <git@keeda.net>
2016-02-02 13:15:03 -0500

    update test/object/dotPath -- using src/object/toPath, add tests

--- test/object/dotPath.js
+++ test/object/dotPath.js
@@ -1,5 +1,6 @@
 var test = require('../../src/assertion/ttr')
-var dotPath = require('../../src/object/dotPath')
+var dotPath = require('../../src/object/toPath')
+// var dotPath = require('../../src/object/dotPath')
 
 test('create object path from a list of strings', function (t) {
   t.is(dotPath('foo', 'bar', 'baz'), 'foo.bar.baz')
@@ -47,6 +48,26 @@ test('create object path from a mixture of arrays and strings as arguments', fun
   t.is(foo(['foo', 'bar'], ['baz']), 'foo.bar.baz')
   t.is(foo(['foo'], ['bar'], ['baz']), 'foo.bar.baz')
   t.is(foo(['foo'], 'bar', ['baz']), 'foo.bar.baz')
+})
+
+test('create object path when arguments is not the first value', function (t) {
+  function foo () {
+    return dotPath('options', arguments)
+  }
+
+  t.is(foo(['foo', 'bar', 'baz']), 'options.foo.bar.baz')
+})
+
+test('create object path discarding any non-string values', function (t) {
+  function foo () {
+    return dotPath('options', arguments)
+  }
+
+  var fn = function () {
+    return
+  }
+
+  t.is(foo([fn, 'foo', 'bar', 'baz'], fn, new Date(), {a: 1}, 6, null), 'options.foo.bar.baz')
 })
 
 test.result()
