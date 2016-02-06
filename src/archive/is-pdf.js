19fdb2c97ec7914cf8da292f0fbd22124462a8b1
akileez <git@keeda.net>
2016-02-06 15:14:26 -0500

    source archive/is-pdf

---
+++ src/archive/is-pdf.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function isPDF (buf) {
+  if (!buf || buf.length < 4) return false
+
+  return buf[0] === 0x25 && buf[1] === 0x50 && buf[2] === 0x44 && buf[3] === 0x46
+}
+
+module.exports = isPDF
