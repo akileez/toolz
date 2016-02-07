41bc1d7791f08c0ab0459f048455a66952157082
akileez <git@keeda.net>
2016-02-07 01:24:18 -0500

    source test/stamp/basic-props

---
+++ test/stamp/basic-props.js
@@ -0,0 +1,70 @@
+var test = require('../../src/assertion/ttr')
+var stampit = require('../../src/object/stampit')
+
+// Basics Props
+
+test('stampit({ props })', (t) => {
+  const obj = stampit({ props: { foo: { bar: 'bar' } } }).create();
+
+  t.is(obj.foo.bar, 'bar',
+    'Should set default props.');
+});
+
+test('stampit().props()', (t) => {
+  const obj = stampit().props({
+    foo: { bar: 'bar' },
+    propsOverride: false,
+    func1() {}
+  }).props({
+    bar: 'bar',
+    propsOverride: true,
+    func2() {}
+  }).create();
+
+  t.is(obj.foo.bar, 'bar',
+    'Should set default props.');
+  t.is(obj.bar, 'bar',
+    'Should set let you add by chaining .props().');
+  t.ok(obj.propsOverride,
+    'Should set let you override by chaining .props().');
+  t.ok(obj.func1,
+    'Should mix functions.');
+  t.ok(obj.func2,
+    'Should mix functions.');
+});
+
+test('stampit({ props }).props()', (t) => {
+  const obj = stampit({ props: {
+    foo: { bar: 'bar' },
+    propsOverride: false,
+    func1() {}
+  }}).props({
+    bar: 'bar',
+    propsOverride: true,
+    func2() {}
+  }).create();
+
+  t.is(obj.foo.bar, 'bar',
+    'Should set default props.');
+  t.is(obj.bar, 'bar',
+    'Should set let you add by chaining .props().');
+  t.ok(obj.propsOverride,
+    'Should set let you override by chaining .props().');
+  t.ok(obj.func1,
+    'Should mix functions.');
+  t.ok(obj.func2,
+    'Should mix functions.');
+});
+
+test('stampit().props(a, b)', (t) => {
+  const obj = stampit().props({
+    a: 'a'
+  }, {
+    b: 'b'
+  }).create();
+
+  t.ok(obj.a && obj.b,
+    'Should mixIn objects when multiple objects are passed.');
+});
+
+test.result()
