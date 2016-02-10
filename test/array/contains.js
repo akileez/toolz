6bf9dc4d6688fa1e7c86b2fd68fea281ea165c9e
akileez <git@keeda.net>
2016-02-10 03:56:56 -0500

    update test/array/contain -- test collection/contains, add log

--- test/array/contains.js
+++ test/array/contains.js
@@ -1,15 +1,24 @@
 var test = require('../../src/assertion/ttr')
 var contains = require('../../src/array/contains')
+var collection = require('../../src/collection/contains')
+
+test.log('Testing array/contains and collection/contains\n')
 
 test('contains should check for existance', (t) => {
   var arr = [1, 2, 3]
   t.is(contains(arr, 2), true)
   t.is(contains(arr, 4), false)
+
+  t.is(collection(arr, 2), true)
+  t.is(collection(arr, 4), false)
 })
 
 test('contains should return false when array is null/undefined', (t) => {
   t.is(contains(null, 1), false)
   t.is(contains(undefined, 1), false)
+
+  t.is(collection.arr(null, 1), false)
+  t.is(collection.arr(undefined, 1), false)
 })
 
 test.result()
