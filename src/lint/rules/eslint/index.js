56bbce3c9ba49fbadedee417c35040fe0ed888aa
akileez <git@keeda.net>
2016-02-11 00:23:46 -0500

    source lint/rules/eslint/index

---
+++ src/lint/rules/eslint/index.js
@@ -0,0 +1,10 @@
+module.exports = Object.assign(
+  Object.create(null),
+  require('./best-practices'),
+  require('./es2015'),
+  require('./node'),
+  require('./possible-errors'),
+  require('./strict-mode'),
+  require('./stylistic-issues'),
+  require('./variables')
+)
