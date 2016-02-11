c16a43153f81036dc351f136138f5b02e7a63d94
akileez <git@keeda.net>
2016-02-11 06:40:18 -0500

    source array/uneek -- for testing

---
+++ src/array/uneek.js
@@ -0,0 +1,64 @@
+// uniq <https://github.com/mikolalysenko/uniq>
+// Copyright (c) 2013 Mikola Lysenko (MIT)
+
+"use strict"
+
+function unique (list, compare, sorted) {
+  if(list.length === 0) return list
+
+  if(compare) {
+    if(!sorted) list.sort(compare)
+    return unique_pred(list, compare)
+  }
+
+  if(!sorted) list.sort()
+  return unique_eq(list)
+}
+
+function unique_pred (list, compare) {
+  var ptr = 1
+  var len = list.length
+  var a = list[0]
+  var b = list[0]
+
+  for(var i = 1; i < len; ++i) {
+    b = a
+    a = list[i]
+
+    if(compare(a, b)) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+
+  list.length = ptr
+  return list
+}
+
+function unique_eq (list) {
+  var ptr = 1
+  var len = list.length
+  var a = list[0]
+  var b = list[0]
+
+  for(var i = 1; i < len; ++i) {
+    b = a
+    a = list[i]
+
+    if(a !== b) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+
+  list.length = ptr
+  return list
+}
+
+module.exports = unique
