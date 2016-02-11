7fe0ae0f778afc61778c2320fe646a2d82b5382f
akileez <git@keeda.net>
2016-02-11 00:24:06 -0500

    source lint/rules/eslint/node

---
+++ src/lint/rules/eslint/node.js
@@ -0,0 +1,35 @@
+/**
+ * Node
+ */
+module.exports = {
+  // enforce return after a callback
+  'callback-return': [2, [
+    'callback',
+    'cb',
+    'next',
+    'done'
+  ]],
+
+  'global-require': 1,
+
+  // enforce error handling in callbacks
+  'handle-callback-err': 2,
+
+  // disallow mixing regular variable and require declarations
+  'no-mixed-requires': 2,
+
+  // disallow use of new operator with the require function
+  'no-new-require': 2,
+
+  // disallow string concatenation with __dirname and __filename
+  'no-path-concat': 2,
+
+  // disallow process.exit()
+  'no-process-exit': 2,
+
+  // restrict usage of specified node modules
+  'no-restricted-modules': 0,
+
+  // disallow use of synchronous methods
+  'no-sync': 1
+};
