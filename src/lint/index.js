d59f9d4278d3018081366eba2fd88468367e63ac
akileez <git@keeda.net>
2016-02-11 12:48:18 -0500

    update lint/index -- pull everything in here

--- src/lint/index.js
+++ src/lint/index.js
@@ -1,6 +1,16 @@
-module.exports = {
-  rules: Object.assign(
+module.exports = Object.assign(
+  Object.create(null),
+  {env: require('./env')},
+  {globals: require('./env/globals')},
+  {ecmaFeatures: require('./env/ecmaFeatures')},
+  {rules: Object.assign(
     Object.create(null),
-    require('./rules/eslint')
-  )
-}
+    require('./rules/best-practices'),
+    require('./rules/es2015'),
+    require('./rules/node'),
+    require('./rules/possible-errors'),
+    require('./rules/strict-mode'),
+    require('./rules/stylistic-issues'),
+    require('./rules/variables')
+  )}
+)
