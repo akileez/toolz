c336709541f98e91b1a0792f545f64fb0943eda4
akileez <git@keeda.net>
2016-02-12 05:04:08 -0500

    source error/create-error-class

---
+++ src/error/create-error-class.js
@@ -0,0 +1,36 @@
+'use strict';
+var captureStackTrace = require('capture-stack-trace');
+
+function inherits(ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+}
+
+module.exports = function createErrorClass(className, setup) {
+  if (typeof className !== 'string') {
+    throw new TypeError('Expected className to be a string');
+  }
+
+  if (/[^0-9a-zA-Z_$]/.test(className)) {
+    throw new Error('className contains invalid characters');
+  }
+
+  setup = setup || function (message) {
+    this.message = message;
+  };
+
+  /* jshint evil:true */
+  var ErrorClass = eval('(function ' + className + '() { captureStackTrace(this, this.constructor); setup.apply(this, arguments); })');
+
+  inherits(ErrorClass, Error);
+  ErrorClass.prototype.name = className;
+
+  return ErrorClass;
+};
