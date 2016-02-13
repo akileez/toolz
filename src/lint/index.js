c1230f766cbae544dd742e23b8a42b6f30d2d9c2
akileez <git@keeda.net>
2016-02-13 06:19:12 -0500

    update lint/index -- fix typo

--- src/lint/index.js
+++ src/lint/index.js
@@ -8,7 +8,7 @@ module.exports = Object.assign(
     require('./rules/possible-errors'),
     require('./rules/best-practices'),
     require('./rules/strict-mode'),
-    require('./rules/variables')
+    require('./rules/variables'),
     require('./rules/node'),
     require('./rules/stylistic-issues'),
     require('./rules/ecmascript6')
