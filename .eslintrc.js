8c323e93945a493e87f6267968db729bbaf74210
akileez <git@keeda.net>
2016-02-13 06:18:42 -0500

    update .eslintrc.js -- config for eslint 2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = Object.assign(
   Object.create(null),
+  {parserOptions: require('./src/lint/env/parserOptions')},
   {env: require('./src/lint/env')},
   {globals: require('./src/lint/env/globals')},
-  {ecmaFeatures: require('./src/lint/env/ecmaFeatures')},
   {rules: Object.assign(
     Object.create(null),
-    require('./src/lint/rules/best-practices'),
-    require('./src/lint/rules/es2015'),
-    require('./src/lint/rules/node'),
     require('./src/lint/rules/possible-errors'),
+    require('./src/lint/rules/best-practices'),
     require('./src/lint/rules/strict-mode'),
+    require('./src/lint/rules/variables'),
+    require('./src/lint/rules/node'),
     require('./src/lint/rules/stylistic-issues'),
-    require('./src/lint/rules/variables')
+    require('./src/lint/rules/ecmascript6')
   )}
 )
