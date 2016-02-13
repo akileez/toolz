31a67051183b2a11c42827878be4bc828c2a97de
akileez <git@keeda.net>
2016-02-13 11:00:35 -0500

    update cli/spawn-commands
    — add ability to pass option params.
    — define callback if none given (async)
    — ensure ‘cmd’ param is an array
    — allow multiple commands to be processed serially or concurrently
    (async)

--- src/cli/spawn-commands.js
+++ src/cli/spawn-commands.js
@@ -3,18 +3,46 @@
 
 'use strict'
 
-var iterate = require('../async/iterate').each
+var serial = require('../async/iterate').each
+var concurrent = require('../async/concurrent').each
 var forEach = require('../collection/forEach')
+var extend = require('../object/extend')
 var child = require('child_process')
 
 // cmds: arrary/object of objects {cmd: 'something', args: ['some', 'things']}
 // cb: callback [function]
 
-function commands (cmds, cb) {
-  if (arguments.length === 1) cb = function () {}
+function defs (opts) {
+  opts = opts || {}
+
+  return extend({
+    stdio: 'inherit',
+    concurrent: false
+  }, opts)
+}
+
+function commands (cmds, opts, cb) {
+  cmds = Array.isArray(cmds) ? cmds : [cmds]
+
+  if (arguments.length === 1) {
+    cb = function () {}
+    opts = defs()
+  } else if (arguments.length === 2) {
+    if (typeof opts === 'function') {
+      cb = opts
+      opts = defs()
+    } else {
+      cb = function () {}
+      opts = defs(opts)
+    }
+  } else {
+    opts = defs(opts)
+  }
+
+  var iterate = opts.concurrent ? concurrent : serial
 
   iterate(cmds, function (cmd, idx, next) {
-    child.spawn(cmd.cmd, cmd.args, {stdio: 'inherit'})
+    child.spawn(cmd.cmd, cmd.args, opts)
       .on('error', next)
       .on('close', next)
   }, cb)
@@ -24,7 +52,7 @@ commands.sync = function (cmds) {
   cmds = Array.isArray(cmds) ? cmds : [cmds]
 
   forEach(cmds, function (cmd) {
-    child.spawnSync(cmd.cmd, cmd.args, {stdio: 'inherit'})
+    child.spawnSync(cmd.cmd, cmd.args, opts)
   })
 }
 
