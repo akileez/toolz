98368eb0e0f3b8a32da12d9383c3eb812e0e612c
akileez <git@keeda.net>
2016-02-14 00:25:13 -0500

    update file/move -- ES6

--- src/file/move.js
+++ src/file/move.js
@@ -1,10 +1,12 @@
-var exists    = require('./exists')
-var dir       = require('../path/directory')
-var eachAsync = require('../async/iterate').each
-var mkdirp    = require('./mkdirp')
-var assert    = require('assert')
-var fs        = require('fs')
-var path      = require('path')
+'use strict'
+
+const exists    = require('./exists')
+const dir       = require('../path/directory')
+const iterate   = require('../async/iterate').each
+const mkdirp    = require('./mkdirp')
+const assert    = require('assert')
+const fs        = require('fs')
+const path      = require('path')
 
 function moveFiles (src, dest, opts, cb) {
   if (typeof opts === 'function') {
@@ -12,51 +14,48 @@ function moveFiles (src, dest, opts, cb) {
     opts = {empty: false}
   }
 
-  exists(src, function (dirExists) {
-    if (!dirExists) {
-      cb('Directory ' + src + ' does not exist.')
-    } else {
-      dir.files(src, 'all', function (err, collectionOf) {
-        assert.ifError(err)
+  exists(src, (dirExists) => {
+    if (!dirExists) return cb('Directory ' + src + ' does not exist.')
 
-        eachAsync(collectionOf.files, function (file, key, done) {
-          var destfile = file.replace(src + '/', '')
-          fs.rename(file, dest + destfile, function (err) {
-            if (err) {
-              mkdirp(dest + destfile, function (err) {
-                assert.ifError(err)
-              })
-            }
-            if (exists(file)) {
-              fs.unlink(file, function (err) {
+    dir.files(src, 'all', (err, collectionOf) => {
+      assert.ifError(err)
+
+      iterate(collectionOf.files, (file, key, done) => {
+        let destfile = dest + file.replace(src + '/', '')
+
+        fs.rename(file, destfile, (err) => {
+          if (err) {
+            mkdirp(destfile, (err) => {
+              assert.ifError(err)
+              fs.unlink(file, (err) => {
                 assert.ifError(err)
                 done(null, file)
               })
-            } else {
-              done(null, file)
-            }
+            })
+          } else {
+            done(null, file)
+          }
+        })
+      }, (err, results) => {
+        assert.ifError(err)
+
+        iterate(collectionOf.dirs.reverse(), (dirs, key, done) => {
+          fs.rmdir(dirs, (err) => {
+            assert.ifError(err)
+            done(null, dirs)
           })
-        }, function (err, results) {
+        }, (err, results) => {
           assert.ifError(err)
 
-          eachAsync(collectionOf.dirs.reverse(), function (dirs, key, done) {
-            fs.rmdir(dirs, function (err) {
+          if (!opts.empty)
+            fs.rmdir(src, (err) => {
               assert.ifError(err)
-              done(null, dirs)
+              return cb('Directory ' + src + ' removed')
             })
-          }, function (err, results) {
-            assert.ifError(err)
-
-            if (!opts.empty)
-              fs.rmdir(src, function (err) {
-                assert.ifError(err)
-                return cb('Directory ' + src + ' removed')
-              })
-            else return cb('Directory ' + src + ' emptied')
-          })
+          else return cb('Directory ' + src + ' emptied')
         })
       })
-    }
+    })
   })
 }
 
