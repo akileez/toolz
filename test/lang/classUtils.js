4de3e2eaf00520642570dcc90b26aea53798df8d
akileez <git@keeda.net>
2016-02-15 04:30:56 -0500

    test lang/classUtils

---
+++ test/lang/classUtils.js
@@ -0,0 +1,33 @@
+var test = require('../../src/assertion/ttr')
+var cu = require('../../src/lang/classUtils')
+
+test('cu should extend', function (t) {
+  var Parent;
+  var Ctor;
+  // var proto;
+
+  Parent = function() {}
+  Parent.foo = 'bar';
+  Parent.prototype.a = function() {};
+  Parent.prototype.b = function() {};
+  Parent.prototype.c = function() {};
+  Object.defineProperty(Parent.prototype, 'count', {
+    get: function() {
+      return Object.keys(this).length;
+    },
+    set: function() {}
+  });
+  Ctor = function() {
+    Parent.call(this);
+  };
+  // proto = App.prototype;
+  var extend = cu.extend(Parent)
+  extend(Ctor)
+  t.assert(typeof Ctor.extend === 'function')
+  t.assert(Ctor.foo === 'bar')
+  t.assert(typeof Ctor.prototype.a === 'function');
+  t.assert(typeof Ctor.prototype.b === 'function');
+  t.assert(typeof Ctor.prototype.c === 'function');
+})
+
+test.result()
