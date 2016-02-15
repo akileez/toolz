cff721581bc9dab3b57cd3ce747b0edfb9157c03
akileez <git@keeda.net>
2016-02-15 02:11:07 -0500

    update string/dashCase -- replacement rex to test

--- src/string/dashCase.js
+++ src/string/dashCase.js
@@ -1,6 +1,7 @@
 function dasher (str) {
   return str
     .replace(/([A-Z])/g, '-$1')
+    // .replace(/([a-z])([A-Z])/g, '$1-$2')
     .replace(/[-_\s]+/g, '-')
     .replace(/^-/g, '')
     .toLowerCase()
