c82476f37a98262160cbbdaae3a496a2c505b79f
akileez <git@keeda.net>
2016-02-16 04:16:50 -0500

    source stream/callback-stream

---
+++ src/stream/callback-stream.js
@@ -0,0 +1,55 @@
+'use strict'
+// https://github.com/mcollina/callback-stream
+// Copyright (c) 2013-2015 Matteo Collina, http://matteocollina.com (MIT)
+
+var Writable = require('readable-stream').Writable
+var inherits = require('inherits')
+
+// It is a safe variant of the concat-stream package that will always return an array.
+
+function CallbackStream (options, callback) {
+  if (!(this instanceof CallbackStream)) {
+    return new CallbackStream(options, callback)
+  }
+
+  if (typeof options === 'function') {
+    callback = options
+    options = {}
+  }
+
+  Writable.call(this, options)
+
+  this.results = []
+  this.callback = callback
+
+  this.on('finish', deliversCallback)
+  this.once('pipe', handlePipe)
+}
+
+function deliversCallback () {
+  this.callback(null, this.results)
+}
+
+function handlePipe (source) {
+  source.on('error', this.callback)
+}
+
+inherits(CallbackStream, Writable)
+
+CallbackStream.prototype._write = function (data, encoding, done) {
+  this.results.push(data)
+  done()
+}
+
+CallbackStream.obj = function (options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = {}
+  }
+
+  options.objectMode = true
+
+  return new CallbackStream(options, callback)
+}
+
+module.exports = CallbackStream
