54c05d4013da8c5a2944dd6ff520371f725555f0
akileez <git@keeda.net>
2016-02-17 16:47:38 -0500

    update lang/isArray -- add notes

--- src/lang/isArray.js
+++ src/lang/isArray.js
@@ -1,3 +1,11 @@
+/*
+  Note to self:
+  function (a) {
+    return typeof a === 'object' && !!a.length
+  }
+
+*/
+
 function isArray (value) {
   return Object.prototype.toString.call(value) === '[object Array]'
 }
