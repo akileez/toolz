0087ccb614b5e829cb8d0abff27b571bcc98fae2
akileez <git@keeda.net>
2016-02-19 13:44:56 -0500

    source regex/url-regex

---
+++ src/regex/url-regex.js
@@ -0,0 +1,27 @@
+// https://github.com/kevva/url-regex
+// Copyright (c) Kevin Mårtensson <kevinmartensson@gmail.com> and Diego Perini
+
+'use strict'
+
+var ipRegex = require('./ip-regex')
+
+module.exports = function (opts) {
+  opts = opts || {}
+
+  var protocol = '(?:(?:[a-z]+:)?//)'
+  var auth = '(?:\\S+(?::\\S*)?@)?'
+  var ip = ipRegex.v4().source
+  var host = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)'
+  var domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*'
+  var tld = '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))'
+  var port = '(?::\\d{2,5})?'
+  var path = '(?:[/?#]\\S*)?'
+  var regex = [
+    protocol, auth, '(?:localhost|' + ip + '|' + host + domain + tld + ')',
+    port, path
+  ].join('')
+
+  return opts.exact
+    ? new RegExp('(?:^' + regex + '$)', 'i')
+    : new RegExp(regex, 'ig');
+}
