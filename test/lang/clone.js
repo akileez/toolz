fcb2b19da2bae11460abe0cde5e2ac20d7c4aca0
akileez <git@keeda.net>
2016-02-20 00:13:21 -0500

    update test lang/clone -- convert to painless

--- test/lang/clone.js
+++ test/lang/clone.js
@@ -1,46 +1,48 @@
-var test = require('../../src/assertion/ttr')
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test lang/kind')
+var t = painless.assert
 var clone = require('../../src/lang/clone')
 
-test('should not return source object', function(t) {
+test('should not return source object', function() {
   var src = {};
   var result = clone(src);
   t.ne(result, src);
 });
 
-test('should copy source properties', function(t) {
+test('should copy source properties', function() {
   var src = { test: true };
   var result = clone(src);
   t.eq(result.test, true);
 });
 
-test('should copy deep properties', function(t) {
+test('should copy deep properties', function() {
   var src = { test: { me: true} };
   var result = clone(src);
   t.eq(result.test.me, true);
   t.ne(result, src)
 });
 
-test('should not clone child objects', function(t) {
+test('should not clone child objects', function() {
   var src = { test: {} };
   var result = clone(src);
   t.eq(result.test, src.test);
 });
 
-test('should clone arrays', function(t) {
+test('should clone arrays', function() {
   var src = [1, 2, 3];
   var result = clone(src);
   t.ne(result, src);
   t.same(result, src);
 });
 
-test('should clone functions', function(t) {
+test('should clone functions', function() {
   var src = function foo () {return 'foo'};
   var result = clone(src);
   t.eq(result, src);
   t.same(result, src);
 });
 
-test('should clone RegExps', function(t) {
+test('should clone RegExps', function() {
   var src = /test/gim;
   var result = clone(src);
   t.ne(result, src);
@@ -50,18 +52,16 @@ test('should clone RegExps', function(t) {
   t.is(result.global, true);
 });
 
-test('should clone Dates', function(t) {
+test('should clone Dates', function() {
   var src = new Date();
   var result = clone(src);
   t.ne(result, src);
   t.same(result, src);
 });
 
-test('should not clone objects created with custom constructor', function(t) {
+test('should not clone objects created with custom constructor', function() {
   function TestType() { }
   var src = new TestType();
   var result = clone(src);
   t.is(result, src);
 });
-
-test.result()
