67201d8f435c25f804910e74e1705bae60d53fcf
akileez <git@keeda.net>
2016-02-20 00:13:37 -0500

    source test lang/isEmpty

---
+++ test/lang/isEmpty.js
@@ -0,0 +1,40 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test lang/isEmpty')
+var t = painless.assert
+
+var isEmpty = require('../../src/lang/isEmpty')
+
+test('handles arrays', function () {
+  t.is(isEmpty([]), true)
+  t.is(isEmpty(['a', 'b']), false)
+});
+
+test('handles objects', function () {
+  t.is(isEmpty({}), true)
+  t.is(isEmpty({ a: 'b' }), false)
+});
+
+test('handles strings', function () {
+  t.is(isEmpty(''), true)
+  t.is(isEmpty('string'), false)
+});
+
+test('handles numbers', function () {
+  t.is(isEmpty(0), true)
+  t.is(isEmpty(42), false)
+});
+
+test('handles functions', function () {
+  t.is(isEmpty(function(){}), true)
+  t.is(isEmpty(function(a,b){}), false)
+});
+
+test('handles nulls', function () {
+  t.is(isEmpty(null), true)
+  t.is(isEmpty(undefined), true)
+});
+
+test('handles booleans', function () {
+  t.is(isEmpty(false), false)
+  t.is(isEmpty(true), false)
+});
