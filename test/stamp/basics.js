6b6dc9e743a0e0374ac5bb559345d21f948d3b30
akileez <git@keeda.net>
2016-02-20 00:30:35 -0500

    source test stamp/basics

---
+++ test/stamp/basics.js
@@ -0,0 +1,26 @@
+var painless = require('../../src/assertion/painless')
+var t        = painless.assert
+var test     = painless.createGroup('Test object/stampit (basic)')
+
+var stampit = require('../../src/object/stampit')
+
+// Basics
+//
+
+test('.create()', () => {
+  const stamp = stampit({ methods: {
+    foo() { return 'foo'; }
+  }});
+
+  t.is(stamp.create().foo(), 'foo',
+    'Should produce an object from specified prototypes.');
+});
+
+test('.create(properties)', () => {
+  'use strict'
+  let stamp = stampit({ refs: { foo: 'bar' } });
+  const obj = stamp.create({ foo: 'foo' });
+
+  t.is(obj.foo, 'foo',
+    'should override defaults.');
+});
