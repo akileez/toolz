c5c9835cf1f740a1c3ce83e47a828597dea6cd40
akileez <git@keeda.net>
2016-02-20 00:33:00 -0500

    update test/test-runner -- automation

--- test/testr.js
+++ test/testr.js
@@ -1,60 +1,82 @@
 // use this file to automate the running of tests.
-var runr = require('../src/util/runr')
-var execa = require('../src/cli/execa-commands')
-var spawn = require('../src/cli/spawn-commands')
+var runr     = require('../src/util/runr')
+var execa    = require('../src/cli/execa-commands')
+var spawn    = require('../src/cli/spawn-commands')
+var concur   = require('../src/async/concurrent').each
+var glob     = require('../src/glob/globby')
+var map      = require('../src/array/map')
+var segments = require('../src/path/segments')
+
+function mapr (v, k) {
+  return {cmd: 'node', args: [segments.last(v)]}
+}
+
+function opts (dir) {
+  return {cwd: dir, concurrent: false}
+}
 
 function arr () {
-  execa({
-    cmd1: {cmd: 'node', args: ['append.js']},
-    cmd2: {cmd: 'node', args: ['collect.js']},
-    cmd3: {cmd: 'node', args: ['combine.js']},
-    cmd4: {cmd: 'node', args: ['compact.js']},
-    cmd5: {cmd: 'node', args: ['contains.js']},
-    cmd6: {cmd: 'node', args: ['convert.js']},
-    cmd7: {cmd: 'node', args: ['difference.js']},
-    cmd7: {cmd: 'node', args: ['equals.js']},
-    cmd7: {cmd: 'node', args: ['every.js']}
-  }, {cwd: 'array', concurrent: true})
+  var dir = 'array'
+  var files = map(glob.sync(['array/*.js', '!array/lint*.js']), mapr)
+
+  // console.log(files)
+
+  concur([
+    // execute tests
+    execa(files, opts(dir)),
+    // lint files
+    spawn([{cmd: 'node', args: ['lint.js', '-a']}], opts(dir))
+  ], () => {
+    // noop
+  })
+}
+
+function async1 () {
+  var dir = 'async'
+  var files = map(glob.sync(['async/*.js', '!async/lint*.js']), mapr)
+  execa(files, opts(dir))
+}
+
+function func () {
+  var dir = 'function'
+  var files = map(glob.sync(['function/*.js', '!function/lint*.js']), mapr)
+  execa(files, opts(dir))
 }
 
 function lang () {
-  execa({
-    cmd1: {cmd: 'node', args: ['clone.js']},
-    cmd2: {cmd: 'node', args: ['kind.js']}
-  }, {cwd: 'lang'})
+  var dir = 'lang'
+  var files = map(glob.sync(['lang/*.js', '!lang/lint*.js']), mapr)
+  execa(files, opts(dir))
 }
 
 function num () {
-  execa({
-    cmd1: {cmd: 'node', args: ['humanize.js']}
-  }, {cwd: 'number'})
+  var dir = 'number'
+  var files = map(glob.sync(['number/*.js', '!number/lint*.js']), mapr)
+  execa(files, opts(dir))
 }
 
 function obj () {
-  execa({
-    cmd1: {cmd: 'node', args: ['dotPath.js']},
-    cmd2: {cmd: 'node', args: ['merge.js']}
-  }, {cwd: 'object'})
+  var dir = 'object'
+  var files = map(glob.sync(['object/*.js', '!object/lint*.js']), mapr)
+  execa(files, opts(dir))
 }
 
 function stamp () {
-  execa({
-    cmd1: {cmd: 'node', args: ['basic-init.js']},
-    cmd2: {cmd: 'node', args: ['basic-methods.js']},
-    cmd3: {cmd: 'node', args: ['basic-props.js']},
-    cmd4: {cmd: 'node', args: ['basic-refs.js']},
-    cmd5: {cmd: 'node', args: ['basic-statics.js']}
-  }, {cwd: 'stamp'})
+  var dir = 'stamp'
+  var files = map(glob.sync(['stamp/basic*.js', '!stamp/lint*.js']), mapr)
+  execa(files, opts(dir))
 }
 
 function lint () {
-  ['array', 'lang', 'number'].forEach((val) => {
-    spawn([{cmd: 'eslint', args: ['*.js']}], {cwd: '../src/' + val})
-  })
+  spawn([
+    {cmd: 'node', args: ['lint.js', '-a']}
+  ], {cwd: 'array', concurrent: true})
 }
 
 function defs () {
   arr()
+  async1()
+  func()
   lang()
   num()
   obj()
@@ -69,6 +91,8 @@ function all () {
 runr
   .task('default', defs)
   .task('arr', arr)
+  .task('async', async1)
+  .task('func', func)
   .task('lang', lang)
   .task('num', num)
   .task('obj', obj)
