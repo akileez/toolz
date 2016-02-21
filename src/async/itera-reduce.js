9d1f63cd9c0f8e44cdb1fcf5f825b1f4c88a2811
akileez <git@keeda.net>
2016-02-21 00:02:25 -0500

    update async/itera-reduce -- formatting

--- src/async/itera-reduce.js
+++ src/async/itera-reduce.js
@@ -1,29 +1,50 @@
 'use strict'
 
-const itera = require('./itera')
-const map   = require('../array/map')
-const ovals  = require('../object/values')
+const assert   = require('../assertion/claim')
+const itera    = require('./itera')
+const map      = require('../array/map')
+const øvals    = require('../object/values')
 const isObject = require('../lang/isObject')
 
 /**
- Calls provided async iterator function with the accumulator and each item.
- When all items have been iterated over calls done with a possible error or the final value of the accumulator.
+ Calls the provided async iterator function with an accumulator and `each` item value.
+ When all items have been iterated over, `done` is called with a possible error (reject)
+ or the final value of the accumulator (response/result).
 
   * @name exports
   * @function
-  *
-  * @param items {Array} the items to be reduced
-  * @param seed {T} the initial value that can be of any type and is passed along as the accumulator (acc) each time the iterator is called
-  * @param iterator {Function} function (acc, item, callback) {} - the iterator called for each item
-  * @param done {Function} function (err, acc) {} - called with final accumulated value or an error if one occurred
+
+  * @param items {Array}
+  * the items to be reduced
+    takes arrays and objects. object property values will be converted to an array.
+
+  * @param seed {Any}
+  * the initial value
+    can be of any type and is passed along as the accumulator (acc) each time the iterator is called
+
+  * @param iterator {Function}
+  * function (acc, item, callback) {} || (acc, item, callback) => {}
+    the iterator called for each item
+
+  * @param done {Function}
+  * callback: function (err, acc) {} || (err, acc) => {}
+    called with final accumulated value or an error if one occurred
 
 */
 
 function asyncReduce (items, seed, iterator, done) {
-  if (isObject(items)) items = ovals(items)
-  if (!Array.isArray(items)) throw new Error('items must be an Array')
-  if (typeof iterator !== 'function') throw new Error('iterator must be a function')
-  if (typeof done !== 'function') throw new Error('done must be a function')
+  // param conversion
+  if (isObject(items)) items = øvals(items)
+
+  // check requirements
+  assert.is(Array.isArray(items), true)
+  assert.is(typeof iterator, 'function', 'iterator must be a function')
+  assert.is(typeof done, 'function', 'done must be a function')
+
+  // // portable requirement checks
+  // if (!Array.isArray(items)) throw new Error('items must be an Array')
+  // if (typeof iterator !== 'function') throw new Error('iterator must be a function')
+  // if (typeof done !== 'function') throw new Error('done must be a function')
 
   let tasks
 
