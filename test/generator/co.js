91b7d04d24afc1aa6a135e694d8badce43cb0f22
akileez <git@keeda.net>
2016-02-24 00:53:50 -0500

    update test generator/co -- move location of pify

--- test/generator/co.js
+++ test/generator/co.js
@@ -5,7 +5,7 @@ var testProm = painless.createGroup('Test generator/co: Promises')
 var testArr  = painless.createGroup('Test generator/co: Arrays')
 
 var co = require('../../src/generator/co')
-var pify = require('../../src/async/pify')
+var pify = require('../../src/promise/pify')
 var read = require('fs').readFile
 
 // pify(read)
