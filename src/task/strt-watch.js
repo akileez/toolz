d2adad62b3a13b16106f0db8fb36ff20d48ced0f
akileez <git@keeda.net>
2016-02-26 03:40:41 -0500

    source task/strt-watch

---
+++ src/task/strt-watch.js
@@ -0,0 +1,26 @@
+function watchr (callback)  {
+  return (input) => {
+    return function watch(log) {
+      const reqf = require('../path/req-from')
+      const chokidar = reqf('/usr/local/lib/node_modules', 'chokidar')
+
+      return new Promise((resolve, reject) => {
+        function init() {
+          chokidar
+            .watch(input, { persistent: true })
+            .on('change', callback)
+            .on('error', reject)
+            .on('ready', () => {
+                log('press ctrl-c to exit')
+            })
+        }
+
+        callback(input)
+          .then(init)
+          .catch(init)
+      })
+    }
+  }
+}
+
+module.exports = watchr
