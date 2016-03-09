dfc8d16ec2241a01f0309e84033898e055aebdd5
akileez <git@keeda.net>
2016-03-09 14:26:05 -0500

    update array/uniq — add comments

--- src/array/uniq.js
+++ src/array/uniq.js
@@ -1,5 +1,8 @@
 var reduce = require('./reduce')
 
+// IMPORTANT: duplicates are removed starting from end of array.
+// and no custom compare function as in ./unique.js
+
 function uniq (arr) {
   return reduce(arr, function (prev, curr) {
     if (prev.indexOf(curr) === -1) prev.push(curr)
