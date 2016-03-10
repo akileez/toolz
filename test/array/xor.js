f0dc1e99613316d5bac3a262c39d609a82b8d6fc
akileez <git@keeda.net>
2016-03-10 00:18:47 -0500

    source test array/xor

---
+++ test/array/xor.js
@@ -0,0 +1,28 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/xor')
+var t = painless.assert
+
+var xor = require('../../src/array/xor')
+
+test('should keep only items that are present in a single array', function () {
+  var a = ['a', 'b', 1]
+  var b = ['c', 1]
+  t.same(xor(a, b), ['a', 'b', 'c'])
+})
+
+test('should remove duplicates', function () {
+  var a = ['a', 'b', 1, 'b']
+  var b = ['c', 'a', 1, 'c']
+  t.same(xor(a, b), ['b', 'c'])
+})
+
+test('should return an empty array if items are present on both arrays', function () {
+  var a = ['a', 'c']
+  var b = ['c', 'a']
+  t.same(xor(a, b), [])
+})
+
+test('should use empty array if array is null/undefined', function () {
+  t.same(xor(null, [1]), [1])
+  t.same(xor(undefined, [1]), [1])
+})
