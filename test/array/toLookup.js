12e1eb11b441d878c3210b20b477d46bd14513c5
akileez <git@keeda.net>
2016-03-10 00:19:25 -0500

    source test array/toLookup

---
+++ test/array/toLookup.js
@@ -0,0 +1,37 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/toLookup')
+var t = painless.assert
+
+var toLookup = require('../../src/array/toLookup')
+
+test('should create an object with the key specified as a string', function () {
+  var arr = [{ name: 'a', thing: 1 }, { name: 'b', thing: 2 }];
+
+  result = toLookup(arr, 'name');
+  t.same(result, {
+    a: { name: 'a', thing: 1 },
+    b: { name: 'b', thing: 2 }
+  });
+});
+
+test('should create an object with the key specified as a function', function () {
+  var arr = [{ name: 'a', thing: 1 }, null],
+      result;
+
+  result = toLookup(arr, function (v) {
+      if (v === null) {
+          return 'null';
+      } else {
+          return v.name;
+      }
+  });
+  t.same(result, {
+      'null': null,
+      a: { name: 'a', thing: 1 }
+  });
+});
+
+test('should return empty object when array is null/undefined', function () {
+  t.same( toLookup(null, 'foo') ,  {} );
+  t.same( toLookup(undefined, 'foo') ,  {} );
+});
