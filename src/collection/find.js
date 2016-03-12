250e4d5455aa91802684bbfa10843bbceb7456f0
akileez <git@keeda.net>
2016-03-12 12:38:52 -0500

    update collection/find — use ./_make_new

--- src/collection/find.js
+++ src/collection/find.js
@@ -1,4 +1,4 @@
-var make = require('./_make')
+var make = require('./_make_new')
 var makeIterator = require('../function/makeIterator_')
 var findIndex = require('./findIndex')
 var some = require('./some')
