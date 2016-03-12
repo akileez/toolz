c9a3503b7aa084193c0d6527843386b3ddfaa65d
akileez <git@keeda.net>
2016-03-12 12:38:24 -0500

    source test array/find testing array/find and collection/find

---
+++ test/array/find.js
@@ -0,0 +1,53 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/find and collection/find')
+var t = painless.assert
+
+var find = require('../../src/array/find')
+var finder = require('../../src/collection/find')
+
+test('should return first match', function () {
+  var obj = { a: 'b' }
+  var arr = [123, 'foo', 'bar', obj]
+
+  t.same(find(arr, function (val) {
+    return val === 123; }), 123)
+  t.same(find(arr, function (val) {
+    return typeof val === 'string'; }), 'foo')
+  t.same(find(arr, function (val) {
+    return val.a === 'b'; }), obj)
+
+  t.same(finder(arr, function (val) {
+    return val === 123; }), 123)
+  t.same(finder(arr, function (val) {
+    return typeof val === 'string'; }), 'foo')
+  t.same(finder(arr, function (val) {
+    return val.a === 'b'; }), obj)
+
+})
+
+test('should return undefined if array is null/undefined', function () {
+  var testFunc = function () {
+    return true
+  }
+
+  t.is(find(null, testFunc), undefined)
+  t.is(find(undefined, testFunc), undefined)
+
+  t.is(finder(null, testFunc), undefined)
+  t.is(finder(undefined, testFunc), undefined)
+})
+
+test('should support object shortcut syntax', function () {
+  var obj = { a: 'b' }
+  var arr = [123, 'foo', 'bar', obj]
+
+  t.same(finder(arr, { a: 'b' }), obj)
+})
+
+test('should support string shortcut syntax', function () {
+  var obj = { a: 'b' }
+  var arr = [123, 'foo', 'bar', obj]
+
+  t.same(finder(arr, 'a'), obj)
+})
+
