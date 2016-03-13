e5963e4ede25395e0e72703246d2023531d1d628
akileez <git@keeda.net>
2016-03-13 03:48:53 -0400

    rename/move test task/strt — root level lint,js

---
+++ lint.js
@@ -0,0 +1,14 @@
+var runr = require('./src/task/runr')
+var strt = require('./src/task/strt')
+var pp = require('./src/task/prprtr')
+var files = require('./src/task/strt-files')
+var eslint = require('./src/task/strt-eslint')
+
+function show () {
+  return strt(pp())(
+    files('./src/array/*.js'),
+    eslint()
+  )
+}
+
+runr.task('show', show).task('default', show)
