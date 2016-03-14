11753fc8243fbb3a4a872af20a6ee74d35622f0c
akileez <git@keeda.net>
2016-03-14 13:56:01 -0400

    source test array/findSomething

---
+++ test/array/findSomething.js
@@ -0,0 +1,40 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/findSomething')
+var t = painless.assert
+
+var pick = require('../../src/array/findSomething')
+
+var f = {
+  recordset: [
+    { one: 'un', two: 'deux', three: 'trois' },
+    { two: 'two', one: 'one' },
+    { four: 'quattro' },
+    { two: 'zwei' }
+  ],
+  deep: [
+    { one: { one: 1, two: 2 }},
+    { one: { one: 1, two: 2 }}
+  ]
+}
+
+test('.pick(recordset, property)', function () {
+  t.same(pick(f.recordset, 'one'), [
+    { one: 'un' },
+    { one: 'one' }
+  ])
+})
+
+test('.pick(recordset, [ properties ])', function () {
+  t.same(pick(f.recordset, [ 'one', 'two' ]), [
+    { one: 'un', two: 'deux' },
+    { two: 'two', one: 'one' },
+    { two: 'zwei' },
+  ])
+})
+
+test('.pick(recordset, property.property)', function () {
+  t.same(pick(f.deep, 'one.two'), [
+    { two: 2 },
+    { two: 2 },
+  ])
+})
