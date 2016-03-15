9a0a0b4a1a073c8bfdeb97b50ef62b094c06874e
akileez <git@keeda.net>
2016-03-15 01:23:10 -0400

    source test array/exists

---
+++ test/array/exists.js
@@ -0,0 +1,29 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/exists')
+var t = painless.assert
+
+var exists = require('../../src/array/exists')
+
+var f = {
+  recordset: [
+    { result: false, number: 1 },
+    { result: false, number: 2 }
+  ],
+  arr: [ 1, 2, 'three' ]
+}
+
+test('.exists(recordset, query)', function () {
+  t.eq(exists(f.recordset, { result: true }), false)
+  t.eq(exists(f.recordset, { result: false }), true)
+  t.eq(exists(f.recordset, { result: false, number: 3 }), false)
+  t.eq(exists(f.recordset, { result: false, number: 2 }), true)
+
+})
+
+test('.exists(array, primitive)', function () {
+  t.eq(exists(f.arr, 0), false)
+  t.eq(exists(f.arr, 1), true)
+  t.eq(exists(f.arr, '1'), false)
+  t.eq(exists(f.arr, 'three'), true)
+
+})
