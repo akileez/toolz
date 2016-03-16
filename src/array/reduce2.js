1e715f7dbe824152001172ca14cf8ef82ae04810
akileez <git@keeda.net>
2016-03-16 00:19:20 -0400

    update array/reduce2 — add comments, accumulator optional

--- src/array/reduce2.js
+++ src/array/reduce2.js
@@ -1,11 +1,22 @@
-// Apply a function against an accumulator and each value of the array
-// (from left-to-right) as to reduce it to a single value.
-// accumulator is the second parameter.
+/*
+  Apply a function against an accumulator and each value of the array
+  (from left-to-right) as to reduce it to a single value.
+  Note: accumulator is the second parameter and is optional. The first
+  value of the array passed will be the accumulator in that instance
+*/
 
 function reduce (arr, acc, fn) {
-  var result = acc
+  var i
 
-  var i = -1
+  if (arguments.length < 3) {
+    fn = acc
+    acc = arr[0]
+    i = 0
+  } else {
+    i = -1
+  }
+
+  var result = acc
   var len = arr.length
 
   while (++i < len) {
