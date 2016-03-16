721632bc694fb980024c63c02036335dbe329f4d
akileez <git@keeda.net>
2016-03-16 00:18:16 -0400

    update array/reduce — change variable names

--- src/array/reduce.js
+++ src/array/reduce.js
@@ -1,22 +1,23 @@
 // Apply a function against an accumulator and each value of the array
 // (from left-to-right) as to reduce it to a single value.
 
-function reduce (arr, fn, initVal) {
-  var hasInit = arguments.length > 2
-  var result = initVal
+function reduce (arr, fn, acc) {
+  var init = arguments.length > 2
+  var result = acc
 
   if (arr == null || !arr.length) {
-    if (!hasInit) throw new Error('reduce of empty array with no initial value')
-    else return initVal
+    if (!init)
+      throw new Error('reduce of empty array with no initial value')
+    else return acc
   }
 
   var i = -1
   var len = arr.length
 
   while (++i < len) {
-    if (!hasInit) {
+    if (!init) {
       result = arr[i]
-      hasInit = true
+      init = true
     } else {
       result = fn(result, arr[i], i, arr)
     }
