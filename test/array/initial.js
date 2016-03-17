6d7371fafc5d73b22b6f3557c41edbd6db05b8fc
akileez <git@keeda.net>
2016-03-17 04:38:48 -0400

    source test array/initial

---
+++ test/array/initial.js
@@ -0,0 +1,12 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/initial')
+var t = painless.assert
+var initial = require('../../src/array/initial')
+
+test('should return the initial element of an array', function () {
+  t.eq(initial(['one', 'two']), 'one');
+  t.same(initial([1, 2, 3, 4, 5]), [1,2,3,4]);
+  t.same(initial([1]), []);
+  t.same(initial([]), undefined);
+  t.same(initial(), undefined);
+});
