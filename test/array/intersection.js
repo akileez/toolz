9f5468d6ac5e7b5b2f1605a3eaa117fd34a9847a
akileez <git@keeda.net>
2016-03-17 04:39:18 -0400

    source test array/intersection

---
+++ test/array/intersection.js
@@ -0,0 +1,30 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/intersection')
+var t = painless.assert
+var intersection = require('../../src/array/intersection')
+
+test('should keep only items that are present on all arrays', function () {
+  var a = ['a', 'b', 1],
+    b = ['c', 1],
+    c = [1, 2, 3];
+  t.same(intersection(a, b, c), [1]);
+});
+
+test('should remove duplicates', function () {
+  var a = ['a', 'b', 1],
+    b = ['c', 'a', 1],
+    c = [1, 'b', 2, 3, 'a'];
+  t.same(intersection(a, b, c), ['a', 1]);
+});
+
+test('should return an empty array if no intersection', function () {
+  var a = ['b'],
+    b = ['c', 'a'],
+    c = [1, 'b', 2, 3, 'a'];
+  t.same(intersection(a, b, c), []);
+});
+
+test('should use empty array when null/undefined', function () {
+  t.same(intersection([1, 2], null, [1]), []);
+  t.same(intersection([1, 2], undefined, [1]), []);
+});
