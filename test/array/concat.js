1f5eb580941d062853ab7fe47a7d9c4f6da6ca5a
akileez <git@keeda.net>
2016-03-17 04:27:48 -0400

    update test array/concat — add test for array/copy

--- test/array/concat.js
+++ test/array/concat.js
@@ -1,8 +1,9 @@
 var painless = require('../../src/assertion/painless')
-var test = painless.createGroup('Test array/concat')
+var test = painless.createGroup('Test array/concat and array/copy')
 var t = painless.assert
 
 var concat = require('../../src/array/concat')
+var dupe = require('../../src/array/copy')
 
 test('should concat arrays', function () {
   var arr1 = [1,2,3,4]
@@ -22,4 +23,15 @@ test('should concat arrays', function () {
   var expected = [1,2,3,4]
 
   t.same(res, expected)
+})
+
+test('should copy arrays', function () {
+  var arr1 = [1,2,3,4]
+  var arr2 = [5,6,7,8]
+
+  var res = dupe(arr1)
+  var expected = [1,2,3,4]
+
+  t.same(res, expected)
+  t.not(res, expected)
 })
