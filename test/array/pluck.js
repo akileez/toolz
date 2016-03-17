dc94a137db9b763b6e7b3afd8c6aea24a723f7a3
akileez <git@keeda.net>
2016-03-17 04:51:34 -0400

    source test array/pluck

---
+++ test/array/pluck.js
@@ -0,0 +1,24 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test array/pluck')
+var t = painless.assert
+var pluck = require('../../src/array/pluck')
+
+test('should extract properties from items', function () {
+  var users = [{
+    name: 'John',
+    age: 21
+  }, {
+    name: 'Mary',
+    age: 25
+  }, {
+    name: 'Jane',
+    age: 27
+  }];
+  t.same(pluck(users, 'name'), ['John', 'Mary', 'Jane']);
+  t.same(pluck(users, 'age'), [21, 25, 27]);
+});
+
+test('should allow source array to be null/undefined', function () {
+  t.same(pluck(null, 'foo'), []);
+  t.same(pluck(undefined, 'foo'), []);
+});
