71d2cf34a550ca131e457d76fbb8ba98fb92648b
akileez <git@keeda.net>
2016-03-18 11:40:44 -0400

    update object/hasOwn — use Object.prototype.hasOwnProp safest way to proceed

--- src/object/hasOwn.js
+++ src/object/hasOwn.js
@@ -1,7 +1,5 @@
 function hasOwn (obj, prop) {
-  // trying to eliminate call where I can:
   return Object.prototype.hasOwnProperty.call(obj, prop)
-  // return obj.hasOwnProperty(prop)
 }
 
 module.exports = hasOwn
