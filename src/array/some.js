b87cdc647143c213f9ffee88300eb98524c2d8f8
akileez <git@keeda.net>
2016-03-18 11:37:55 -0400

    update array/some — handle null/undefined

--- src/array/some.js
+++ src/array/some.js
@@ -3,14 +3,20 @@
 // without support for callback shorthands and `this` binding.
 
 function some (arr, fn) {
+  var res = false
+  if (arr == null) return res
+
   var i = -1
   var len = arr.length
 
   while (++i < len) {
-    if (fn(arr[i], i, arr)) return true
+    if (fn(arr[i], i, arr)) {
+      res = true
+      break
+    }
   }
 
-  return false
+  return res
 }
 
 module.exports = some
