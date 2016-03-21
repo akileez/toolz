e98a82be27822769d1febf1dccc9b5d174d54141
akileez <git@keeda.net>
2016-03-21 01:51:35 -0400

    source test lang/isObject

---
+++ test/lang/isObject.js
@@ -0,0 +1,15 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test lang/isObject')
+var t = painless.assert
+
+var isObject = require('../../src/lang/isObject')
+
+test('should detect if value is an object', function () {
+  t.is(isObject({}), true)
+  t.is(isObject(new function(){}), true)
+
+  t.is(isObject(''), false)
+  t.is(isObject(123), false)
+  t.is(isObject(null), false)
+  t.is(isObject([]), false)
+})
