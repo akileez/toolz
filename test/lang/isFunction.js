c2f661655a74853e2deebd8c89b97e212a552565
akileez <git@keeda.net>
2016-03-22 04:30:17 -0400

    source test lang/isFunction

---
+++ test/lang/isFunction.js
@@ -0,0 +1,16 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test lang/isFunction')
+var t = painless.assert
+
+var isFunction = require('../../src/lang/isFunction')
+
+test('should detect if value is a function', function () {
+    t.is(isFunction(function () {}), true)
+    t.is(isFunction(new Function('return 1;')), true)
+
+    t.is(isFunction(''), false)
+    t.is(isFunction(123), false)
+    t.is(isFunction(null), false)
+    t.is(isFunction({}), false)
+    t.is(isFunction([]), false)
+})
