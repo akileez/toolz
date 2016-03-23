d8009eeab907d2a79db429f00e8061c2fc5274b4
akileez <git@keeda.net>
2016-03-23 11:30:36 -0400

    source test lang/toString

---
+++ test/lang/toString.js
@@ -0,0 +1,28 @@
+var painless = require('../../src/assertion/painless')
+var test = painless.createGroup('Test lang/toString')
+var t = painless.assert
+
+var toString = require('../../src/lang/toString')
+
+test('should convert null to empty string', function(){
+  t.is(toString(null), '')
+});
+
+test('should convert undefined to empty string', function(){
+  t.is(toString(void 0), '')
+});
+
+test('should return string unchanged', function(){
+  t.is(toString(''), '')
+  t.is(toString('test'), 'test')
+});
+
+test('should return number as string', function(){
+  t.is(toString(0), '0')
+  t.is(toString(10), '10')
+});
+
+test('should return boolean as string', function(){
+  t.is(toString(false), 'false')
+  t.is(toString(true), 'true')
+});
