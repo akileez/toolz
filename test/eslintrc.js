77d1925a433f94ba563711576e6fc42d852c1bbe
akileez <git@keeda.net>
2016-03-25 14:08:58 -0400

    update dependencies — eslintrc.js

--- test/eslintrc.js
+++ test/eslintrc.js
@@ -1,18 +1,18 @@
-var jlog = require('./src/util/jcolorz')
+var jlog = require('../src/util/jcolorz')
 
 jlog(Object.assign(
   Object.create(null),
-  {parserOptions: require('./src/lint/env/parserOptions')},
-  {env: require('./src/lint/env')},
-  {globals: require('./src/lint/env/globals')},
+  {parserOptions: require('./lint/env/parserOptions')},
+  {env: require('./lint/env')},
+  {globals: require('./lint/env/globals')},
   {rules: Object.assign(
     Object.create(null),
-    require('./src/lint/rules/possible-errors'),
-    require('./src/lint/rules/best-practices'),
-    require('./src/lint/rules/strict-mode'),
-    require('./src/lint/rules/variables'),
-    require('./src/lint/rules/node'),
-    require('./src/lint/rules/stylistic-issues'),
-    require('./src/lint/rules/ecmascript6')
+    require('./lint/rules/possible-errors'),
+    require('./lint/rules/best-practices'),
+    require('./lint/rules/strict-mode'),
+    require('./lint/rules/variables'),
+    require('./lint/rules/node'),
+    require('./lint/rules/stylistic-issues'),
+    require('./lint/rules/ecmascript6')
   )}
 ))
