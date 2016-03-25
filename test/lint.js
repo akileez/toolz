fc8dd6a8df31c1053b1b40d683ddc91bdc3184cb
akileez <git@keeda.net>
2016-03-25 14:09:10 -0400

    update dependencies — lint.js

--- test/lint.js
+++ test/lint.js
@@ -1,12 +1,12 @@
-var runr = require('./src/task/runr')
-var strt = require('./src/task/strt')
-var pp = require('./src/task/prprtr')
-var files = require('./src/task/strt-files')
-var eslint = require('./src/task/strt-eslint')
+var runr = require('../src/task/runr')
+var strt = require('../src/task/strt')
+var pp = require('../src/task/prprtr')
+var files = require('../src/task/strt-files')
+var eslint = require('../src/task/strt-eslint')
 
 function show () {
   return strt(pp())(
-    files('./src/array/*.js'),
+    files('../src/array/*.js'),
     eslint()
   )
 }
