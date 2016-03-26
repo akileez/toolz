05a15b8722b05fd55f6758e3bb7c4f3f7be0c264
akileez <git@keeda.net>
2016-03-26 11:36:59 -0400

    format test lang/isUndefined

--- test/spec/lang/isUndefined.js
+++ test/spec/lang/isUndefined.js
@@ -5,12 +5,12 @@ var t = painless.assert
 var isUndefined = require('../../../src/lang/isUndefined')
 
 test('should detect if value is undefined', function () {
-    t.is(isUndefined(undefined), true)
-    t.is(isUndefined(), true)
+  t.is(isUndefined(undefined), true)
+  t.is(isUndefined(), true)
 
-    t.is(isUndefined(''), false)
-    t.is(isUndefined(123), false)
-    t.is(isUndefined(null), false)
-    t.is(isUndefined({}), false)
-    t.is(isUndefined([]), false)
+  t.is(isUndefined(''), false)
+  t.is(isUndefined(123), false)
+  t.is(isUndefined(null), false)
+  t.is(isUndefined({}), false)
+  t.is(isUndefined([]), false)
 })
