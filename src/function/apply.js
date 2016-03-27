e8c7b8954d4342c41803fc8689196c0d3022c6c8
akileez <git@keeda.net>
2016-03-27 04:25:06 -0400

    update function/apply — remove noThrow function

--- src/function/apply.js
+++ src/function/apply.js
@@ -1,8 +1,6 @@
 // adopted from: fast-apply <"https://github.com/JayceTDE/fast-apply>
 // copyright: Jayce Pulsipher <jaycemp@gmail.com> (MIT)
 
-// add type check for `function` to prevent errors by using identity
-
 function fastApply (fn, context, args) {
   switch (args ? args.length : 0) {
     case 0:
@@ -37,19 +35,4 @@ function fastApply (fn, context, args) {
   }
 }
 
-// concept adopted from: https://github.com/wilmoore/apply-or.js
-// reasoning: Function.prototype.apply is normally sufficient; however, there are situations where it is useful to treat a value as a Function (invoke it) only if it is indeed a Function; otherwise, return it as-is. Calling .apply on a value that is not a function would cause an error.
-function fastApplyNoThrow (fn, context, args) {
-  fn = (typeof fn !== 'function')
-    ? identity
-    : fn
-
-  return fastApply(fn, context, args)
-}
-
-function identity (value) {
-  return value
-}
-
 module.exports = fastApply
-module.exports.Or = fastApplyNoThrow
