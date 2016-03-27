6f200bf976cb803992a0bcef2cee1047bc48ac9c
akileez <git@keeda.net>
2016-03-27 04:02:46 -0400

    update test stamp/deep-props — use new version 3 stampit

--- test/spec/stamp/deep-props-safety.js
+++ test/spec/stamp/deep-props-safety.js
@@ -1,7 +1,7 @@
 var painless = require('../../assertion/painless')
 var test = painless.createGroup('Test object/stampit::deep-props')
 var t = painless.assert
-var stampit = require('../../../src/object/stampit')
+var stampit = require('../../../src/object/stamp')
 
 // Props safety
 
@@ -15,7 +15,6 @@ test('Stamp deepProps deep cloned for object created', function () {
   var o2 = stamp1();
 
   o1.foo = 'another value';
-  console.log(o1)
   t.ne(o1.foo, o2.foo);
   o1.deep.foo = 'another value';
   t.ne(o1.deep.foo, o2.deep.foo);
@@ -28,25 +27,7 @@ test('Stamp deepProps deep cloned for object created', function () {
   t.ne(o1.deep.foo, o2.deep.foo);
 });
 
-test('stamp(refs) does not deep merges deepProps into refs', () => {
-  const deepInProps = { deepProp1: 'should not be merged', deepProp2: 'do not merge me!' };
-  const stamp1 = stampit().deepProps({ deep: deepInProps, shallow1: 'should not be merged', shallow2: 'merge me!' });
-  const stamp2 = stampit({ deepProps: { deep: deepInProps, shallow1: 'should not be merged', shallow2: 'merge me!' } });
-
-  const o1 = stamp1({ deep: { deepProp1: 'leave me as is' }, shallow1: 'leave me as is' });
-  const o2 = stamp2({ deep: { deepProp1: 'leave me as is' }, shallow1: 'leave me as is' });
-
-  t.is(o1.shallow1, 'leave me as is', 'A conflicting shallow reference must not be touched by deepProps');
-  t.is(o1.shallow2, 'merge me!', 'A non conflicting shallow reference must be merged from deepProps');
-  t.is(o2.shallow1, 'leave me as is', 'A conflicting shallow reference must not be touched by deepProps');
-  t.is(o2.shallow2, 'merge me!', 'A non conflicting shallow reference must be merged from deepProps');
-  t.is(o1.deep.deepProp1, 'leave me as is', 'A conflicting deep property in refs must not be touched by deepProps');
-  t.is(o1.deep.deepProp2, undefined, 'A non conflicting deep property must be merged from deepProps');
-  t.is(o2.deep.deepProp1, 'leave me as is', 'A conflicting deep property in refs must not be touched by deepProps');
-  t.is(o2.deep.deepProp2, undefined, 'A non conflicting deep property must be merged from deepProps');
-});
-
-test('stampit.deepProps(deepProps) deep merge into stamp', () => {
+test('stamp.deepProps(deepProps) deep merge into stamp', () => {
   const stamp = stampit()
     .deepProps({ deep: { foo: 'foo', bar: 'bar' }, foo: 'foo', bar: 'bar' })
     .deepProps({ deep: { foo: 'override', baz: 'baz' }, foo: 'override', baz: 'baz' });
