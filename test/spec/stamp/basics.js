1ab964e2f7d8020be158361fd461ebd0c6152597
akileez <git@keeda.net>
2016-03-27 04:02:26 -0400

    update test stamp/basics — use new version 3 stampit

--- test/spec/stamp/basics.js
+++ test/spec/stamp/basics.js
@@ -2,7 +2,7 @@ var painless = require('../../assertion/painless')
 var t        = painless.assert
 var test     = painless.createGroup('Test object/stampit (basic)')
 
-var stampit = require('../../../src/object/stampit')
+var stampit = require('../../../src/object/stamp')
 
 // Basics
 //
@@ -16,11 +16,3 @@ test('.create()', () => {
     'Should produce an object from specified prototypes.');
 });
 
-test('.create(properties)', () => {
-  'use strict'
-  let stamp = stampit({ refs: { foo: 'bar' } });
-  const obj = stamp.create({ foo: 'foo' });
-
-  t.is(obj.foo, 'foo',
-    'should override defaults.');
-});
