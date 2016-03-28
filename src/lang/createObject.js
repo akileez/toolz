570e315aadff154f08abed9ff876dc484eb029ae
akileez <git@keeda.net>
2016-03-28 03:52:17 -0400

    update lang/createObject — add parent to mixin this is probably not correct but keeping for now. this is a function I do not use ever in production. still testing/playing with

--- src/lang/createObject.js
+++ src/lang/createObject.js
@@ -3,7 +3,7 @@ var extend = require('../object/extend')
 function createObject (parent, props) {
   function F () {}
   F.prototype = parent
-  return extend(new F(), props)
+  return extend(new F(), parent, props)
 }
 
 module.exports = createObject
