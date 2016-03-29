692d0225998a48d8ca920c9b9b7567f0dc2e0a36
akileez <git@keeda.net>
2016-03-29 15:57:07 -0400

    update object/reject — use object/forOwn

--- src/object/reject.js
+++ src/object/reject.js
@@ -1,12 +1,13 @@
+var forOwn = require('./forOwn')
+
 function reject (obj, fn) {
   var result = {}
   var key
 
-  for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      if (!fn(obj[key], key, obj)) result[key] = obj[key]
-    }
-  }
+  forOwn(obj, function (val, key) {
+    if (!fn(obj[key], key, obj)) result[key] = obj[key]
+  })
+
   return result
 }
 
