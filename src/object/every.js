49fe98d3654997960e3df66362a92d5f585dc8d0
akileez <git@keeda.net>
2016-03-29 09:52:15 -0400

    update object/every use Object.prototype.hasOwnProperty. this is more safe despite the minor speed hit with fn.call

--- src/object/every.js
+++ src/object/every.js
@@ -1,10 +1,11 @@
 function every (obj, fn) {
+  var hasOwn = Object.prototype.hasOwnProperty
   var result = true
   var key
 
   for (key in obj) {
     // istanbul ignore else
-    if (obj.hasOwnProperty(key)) {
+    if (hasOwn.call(obj, key)) {
       if (!fn(obj[key], key, obj)) {
         result = false
         break
