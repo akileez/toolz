e015f14728c5a05b5e6eb49d3fd46b6b21fc1378
akileez <git@keeda.net>
2016-03-29 09:52:38 -0400

    update object/keys use Object.prototype.hasOwnProperty. this is more safe despite the minor speed hit with fn.call

--- src/object/keys.js
+++ src/object/keys.js
@@ -1,9 +1,10 @@
 function keys (obj) {
+  var hasOwn = Object.prototype.hasOwnProperty
   var result = []
   var key
 
   for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
+    if (hasOwn.call(obj, key)) {
       result.push(key)
     }
   }
