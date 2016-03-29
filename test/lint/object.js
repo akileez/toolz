89087203e421e52793c2ba19a8caee642048f070
akileez <git@keeda.net>
2016-03-29 10:23:19 -0400

    source test lint/object REM: make this process one file or just a function

---
+++ test/lint/object.js
@@ -0,0 +1,38 @@
+// test suite
+var painless = require('../assertion/painless')
+var test     = painless.createGroup('Lint object collection')
+var t        = painless.assert
+
+// helpers
+var execfile = require('child_process').execFile
+var concur   = require('../../src/async/concurrent').each
+var glob     = require('../../src/glob/globby')
+var map      = require('../../src/array/map')
+var segments = require('../../src/path/segments')
+
+// parameters
+var opts = {cwd: '../src/object'}
+var files = map(glob.sync('../src/object/*.js'), function (v, k) {
+  return segments.last(v)
+})
+
+// label each test
+function title (file) {
+  return 'lint ' + file
+}
+
+// console.log(files)
+
+concur(files, (file, idx, next) => {
+  test(title(file), function (cb) {
+    execfile('eslint', [file], opts, function (err, stdout, stderr) {
+      if (err) {
+        t.fail(stdout)
+      }
+      else t.pass('yes')
+      cb()
+    })
+  })
+}, () => {
+  // nothing here
+})
