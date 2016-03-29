59e80f1763542bba4e2786129deb315098fdc167
akileez <git@keeda.net>
2016-03-29 15:58:18 -0400

    update object/values — use object/hasOwn

--- src/object/values.js
+++ src/object/values.js
@@ -1,9 +1,11 @@
+var hasOwn = require('./hasOwn')
+
 function values (obj) {
   var result = []
   var key
 
   for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
+    if (hasOwn(obj, key)) {
       result.push(obj[key])
     }
   }
