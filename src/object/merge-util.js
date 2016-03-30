93117065a8ca40dacbe09675d0bffd0c6da6d23f
akileez <git@keeda.net>
2016-03-30 09:31:42 -0400

    lint object/merge-utils

--- src/object/merge-util.js
+++ src/object/merge-util.js
@@ -15,10 +15,10 @@ var has = Object.prototype.hasOwnProperty
  * @api public
  */
 
-function merge (a, b, opts){
+function merge (a, b, opts) {
   opts = (typeof opts === 'boolean')
-    ? { inheritance: opts }
-    : opts || { inheritance: false , shallow: false, discardEmpty: true }
+    ? {inheritance: opts}
+    : opts || {inheritance: false, shallow: false, discardEmpty: true}
 
   var key
   for (key in b) {
@@ -27,21 +27,21 @@ function merge (a, b, opts){
      : (has.call(b, key)) && b[key] != null
 
     if (copy) {
-      if (!a) a = {};
+      if (!a) a = {}
       if (!opts.shallow && type(b[key]) === 'object') {
         if (!opts.discardEmpty && isEmpty(a[key]) && isEmpty(b[key])) {
           // Preserve { }, null, undefined, 0 as they were
-          a[key] = b[key];
+          a[key] = b[key]
         } else {
           // merge recursively
-          a[key] = merge(a[key], b[key], opts);
+          a[key] = merge(a[key], b[key], opts)
         }
       } else {
-        a[key] = b[key];
+        a[key] = b[key]
       }
     }
   }
-  return a;
+  return a
 };
 
-module.exports = merge;
+module.exports = merge
