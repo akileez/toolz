7818fb0f05a5ad1ee0c5f56fc21cea903af399c0
akileez <git@keeda.net>
2016-03-30 09:29:33 -0400

    lint object/deep-assign

--- src/object/deep-assign.js
+++ src/object/deep-assign.js
@@ -1,67 +1,67 @@
-var isObj = require('./is-object');
-var hasOwnProperty = Object.prototype.hasOwnProperty;
-var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+var isObj = require('./is-object')
+var hasOwnProperty = Object.prototype.hasOwnProperty
+var propIsEnumerable = Object.prototype.propertyIsEnumerable
 
-function toObject(val) {
+function toObject (val) {
   if (val === null || val === undefined) {
-    throw new TypeError('Sources cannot be null or undefined');
+    throw new TypeError('Sources cannot be null or undefined')
   }
 
-  return Object(val);
+  return Object(val)
 }
 
-function assignKey(to, from, key) {
-  var val = from[key];
+function assignKey (to, from, key) {
+  var val = from[key]
 
   if (val === undefined || val === null) {
-    return;
+    return
   }
 
   if (hasOwnProperty.call(to, key)) {
     if (to[key] === undefined || to[key] === null) {
-      throw new TypeError('Cannot convert undefined or null to object (' + key + ')');
+      throw new TypeError('Cannot convert undefined or null to object (' + key + ')')
     }
   }
 
   if (!hasOwnProperty.call(to, key) || !isObj(val)) {
-    to[key] = val;
+    to[key] = val
   } else {
-    to[key] = assign(Object(to[key]), from[key]);
+    to[key] = assign(Object(to[key]), from[key])
   }
 }
 
-function assign(to, from) {
+function assign (to, from) {
   if (to === from) {
-    return to;
+    return to
   }
 
-  from = Object(from);
+  from = Object(from)
 
   for (var key in from) {
     if (hasOwnProperty.call(from, key)) {
-      assignKey(to, from, key);
+      assignKey(to, from, key)
     }
   }
 
   if (Object.getOwnPropertySymbols) {
-    var symbols = Object.getOwnPropertySymbols(from);
+    var symbols = Object.getOwnPropertySymbols(from)
 
     for (var i = 0; i < symbols.length; i++) {
       if (propIsEnumerable.call(from, symbols[i])) {
-        assignKey(to, from, symbols[i]);
+        assignKey(to, from, symbols[i])
       }
     }
   }
 
-  return to;
+  return to
 }
 
-module.exports = function deepAssign(target) {
-  target = toObject(target);
+module.exports = function deepAssign (target) {
+  target = toObject(target)
 
   for (var s = 1; s < arguments.length; s++) {
-    assign(target, arguments[s]);
+    assign(target, arguments[s])
   }
 
-  return target;
-};
+  return target
+}
