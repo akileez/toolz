53d18638a56121378682230ca84285a775744af7
akileez <git@keeda.net>
2016-03-30 09:35:49 -0400

    update object/update remove un-used variable, use forOwn, rid of hasOwn

--- src/object/update.js
+++ src/object/update.js
@@ -1,22 +1,20 @@
-var hasOwn = require('./hasOwn')
+var forOwn = require('./forOwn')
 
 // updates own properties of a target object from multiple sources
 
 function defaults (target) {
   var arg
-  var key
   var len = arguments.length
   var i = 0
 
   while (++i < len) {
     arg = arguments[i]
+    // istanbul ignore if
     if (!arg) continue
 
-    for (key in target) {
-      if (hasOwn(target, key)) {
-        if (arg[key]) target[key] = arg[key]
-      }
-    }
+    forOwn(target, function (val, key) {
+      if (arg[key]) target[key] = arg[key]
+    })
   }
   return target
 }
