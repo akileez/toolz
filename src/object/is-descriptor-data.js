617d7eff1a8f457035ecd56bf98c7acb220da171
akileez <git@keeda.net>
2016-03-31 04:19:42 -0400

    update object/is-descriptor-data remove unnecessary code

--- src/object/is-descriptor-data.js
+++ src/object/is-descriptor-data.js
@@ -9,14 +9,9 @@ function isDataDescriptor (obj, prop) {
 
   if (typeof prop === 'string') {
     var val = Object.getOwnPropertyDescriptor(obj, prop)
-    if (!('value' in val) && !('writable' in val)) return false
-
     return typeof val !== 'undefined'
   }
 
-  // I am being anal here. I require all properties to be present
-  // if only an object is being passed. this check is primarily for
-  // lang/defineProp -- a wrapper for Object.defineProperty
   if (isDescriptorProp('configurable')
     && isDescriptorProp('enumerable')
     && (isDescriptorProp('writable') || hasOwn(obj, 'value'))
