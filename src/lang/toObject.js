6569435d6b4161f104bbb91398e12c5dc94ce189
akileez <git@keeda.net>
2016-03-31 04:18:14 -0400

    update lang/toObject — throw if null or undefined this is for object/assign

--- src/lang/toObject.js
+++ src/lang/toObject.js
@@ -1,6 +1,10 @@
 // lodash.internal function
 
 function toObject (value) {
+  if (value === null || value === undefined) {
+    throw new TypeError('Cannot be called with null or undefined')
+  }
+
   return typeof value === 'object' ? value : Object(value)
 }
 
