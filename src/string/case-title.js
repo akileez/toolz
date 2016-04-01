5302b947acb3aebaf9a0f2bb5671b71c7614d82f
akileez <git@keeda.net>
2016-04-01 11:12:57 -0400

    source string/case-title

---
+++ src/string/case-title.js
@@ -0,0 +1,22 @@
+var sentence = require('case-sentence')
+var escrex = require('esc-regex')
+var minors = require('case-minors')
+
+var escaped = minors.map(escrex)
+var minorMatcher = new RegExp('[^^]\\b(' + escaped.join('|') + ')\\b', 'ig')
+var punctuationMatcher = /:\s*(\w)/g
+
+function toTitleCase(string) {
+  return sentence(string)
+    .replace(/(^|\s)(\w)/g, function (matches, previous, letter) {
+      return previous + letter.toUpperCase()
+    })
+    .replace(minorMatcher, function (minor) {
+      return minor.toLowerCase()
+    })
+    .replace(punctuationMatcher, function (letter) {
+      return letter.toUpperCase()
+    })
+}
+
+module.exports = toTitleCase
