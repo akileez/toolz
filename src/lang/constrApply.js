ac52a84041b684322177fe6607c010fe183f9b2f
akileez <git@keeda.net>
2016-04-01 04:19:18 -0400

    update object/constrApply — use array/slice and function/apply

--- src/lang/constrApply.js
+++ src/lang/constrApply.js
@@ -1,9 +1,13 @@
+var apply = require('../function/apply')
+var slice = require('../array/slice')
+
 function F () {}
 
 function constrApply (constr, args) {
+  args = Array.isArray(args) ? args : slice(arguments, 1)
   F.prototype = constr.prototype
   var instance = new F()
-  constr.apply(instance, args)
+  apply(constr, instance, args)
   return instance
 }
 
