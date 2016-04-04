7e21723e63356879edbe62bd701d94876e6ee8a2
akileez <git@keeda.net>
2016-04-04 03:34:47 -0400

    update string/case-pascalize adjust dependencies

--- src/string/case-pascalize.js
+++ src/string/case-pascalize.js
@@ -1,10 +1,9 @@
-var toString = require('../lang/toString')
+var stringify = require('./stringify')
 var camelCase = require('./case-camelize')
 var upperCase = require('./case-upper')
 
 function pascalCase (str) {
-  str = toString(str)
-  return camelCase(str).replace(/^[a-z]/, upperCase)
+  return camelCase(stringify(str)).replace(/^[a-z]/, upperCase)
 }
 
 module.exports = pascalCase
