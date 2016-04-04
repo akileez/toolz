71bd77fdf64a352437a891d973d14ba4f1f270ff
akileez <git@keeda.net>
2016-04-04 03:33:28 -0400

    update string/case-classify adjust dependencies

--- src/string/case-classify.js
+++ src/string/case-classify.js
@@ -1,10 +1,9 @@
 var capitalize = require('./case-capitalize')
-var camelize = require('./case-camelize')
-var stringify = require('./stringify')
+var camelize   = require('./case-camelize')
+var stringify  = require('./stringify')
 
 function classify (str) {
-  str = stringify(str)
-  return capitalize(camelize(str.replace(/[\W_]/g, ' ')).replace(/\s/g, ''))
+  return capitalize(camelize(stringify(str).replace(/[\W_]/g, ' ')).replace(/\s/g, ''))
 }
 
 module.exports = classify
