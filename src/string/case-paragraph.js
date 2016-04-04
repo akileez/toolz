aa0ce7097bccee3a0a8e2baf5359ddebceb8b269
akileez <git@keeda.net>
2016-04-04 03:34:32 -0400

    update string/case-paragraph adjust dependencies

--- src/string/case-paragraph.js
+++ src/string/case-paragraph.js
@@ -1,15 +1,12 @@
-var toString = require('../lang/toString')
+var stringify = require('./stringify')
 var lowerCase = require('./case-lower')
 var upperCase = require('./case-upper')
 
-// UPPERCASE first cha of each sentence and lowercase other chars
+// replace first char of each sentence (new line or after '.\s+') to
+// UPPERCASE and lowercase other chars
 
 function sentenceCase (str) {
-  str = toString(str)
-
-  // replace first char of each sentence (new line or after '.\s+') to
-  // UPPERCASE
-  return lowerCase(str).replace(/(^\w)|\.\s+(\w)/gm, upperCase)
+  return lowerCase(stringify(str)).replace(/(^\w)|\.\s+(\w)/gm, upperCase)
 }
 
 module.exports = sentenceCase
