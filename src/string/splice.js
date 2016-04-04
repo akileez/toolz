8ec3afb2908ef8c4846a846648a84ab49e58dc65
akileez <git@keeda.net>
2016-04-04 16:58:30 -0400

    bug fix string/slice — separate statements

--- src/string/splice.js
+++ src/string/splice.js
@@ -1,7 +1,9 @@
 var chars = require('./chars')
 
 function splice (str, i, num, substr) {
-  return chars(str).splice(~~i, ~~num, substr).join('')
+  var arr = chars(str)
+  arr.splice(~~i, ~~num, substr)
+  return arr.join('')
 }
 
 module.exports = splice
