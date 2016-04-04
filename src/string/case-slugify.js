96f7f595a4c9c488bfab8ccf39e25bd33375ede0
akileez <git@keeda.net>
2016-04-04 16:56:52 -0400

    refactor string/case-slugify

--- src/string/case-slugify.js
+++ src/string/case-slugify.js
@@ -3,22 +3,34 @@ var stringify      = require('./stringify')
 var replaceAccents = require('./replace-accents')
 var removeNonWord  = require('./remove-non-word')
 
-// Convert to lower case, remove accents, remove non-word chars and
-// replace spaces with the specified delimeter.
-// Does not split camelCase text.
+/*
+  Convert to lower case, remove accents, remove non-word chars and
+  replace spaces with the specified delimeter. Does not split camelCase text.
+  Original code:
+
+    str = stringify(str)
+
+    if (delimeter == null) delimeter = '-'
+
+    str = replaceAccents(str)
+    str = removeNonWord(str)
+
+    // trim should process after removeNonWord
+    // then replace spaces with delimeter
+
+    str = trim(str)
+      .replace(/ +/g, delimeter)
+      .toLowerCase()
+
+    return str
+*/
 
 function slugify (str, delimeter) {
-  str = stringify(str)
-
   if (delimeter == null) delimeter = '-'
 
-  str = replaceAccents(str)
-  str = removeNonWord(str)
-  str = trim(str) // should come after removeNonWord
-    .replace(/ +/g, delimeter) // replace spaces with delimeter
+  return trim(removeNonWord(replaceAccents(stringify(str))))
+    .replace(/ +/g, delimeter)
     .toLowerCase()
-
-  return str
 }
 
 module.exports = slugify
