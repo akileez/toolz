410c778353d8b0d7bc703a5cfa1e9c5ed9e1588a
akileez <git@keeda.net>
2016-04-04 16:57:58 -0400

    bug fix string/replace-all — typos

--- src/string/replace-all.js
+++ src/string/replace-all.js
@@ -1,10 +1,10 @@
 var stringify = require('./stringify')
 
-function replaceAll (str, find replace, ignorecase) {
+function replaceAll (str, find, replace, ignorecase) {
   var flags = (ignorecase === true) ? 'gi' : 'g'
   var regx  = new RegExp(find, flags)
 
-  return stringify(str).replace(reg, replace)
+  return stringify(str).replace(regx, replace)
 }
 
 module.exports = replaceAll
