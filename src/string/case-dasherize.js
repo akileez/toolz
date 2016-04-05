bb68094b313171a9f5036720497d18e1fdf2edfb
akileez <git@keeda.net>
2016-04-05 09:17:54 -0400

    source string/case-dasherize a dupe with a slight change

---
+++ src/string/case-dasherize.js
@@ -0,0 +1,10 @@
+var trim = require('./trim')
+
+function dasherize (str) {
+  return trim(str)
+    .replace(/([A-Z])/g, '-$1')
+    .replace(/[-_\s]+/g, '-')
+    .toLowerCase()
+}
+
+module.exports = dasherize
