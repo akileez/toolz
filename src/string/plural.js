c7f5d23382e8e55c5390c0a175527db2ebea2594
akileez <git@keeda.net>
2016-04-05 01:04:57 -0400

    update string/plural

--- src/string/plural.js
+++ src/string/plural.js
@@ -1,21 +1,20 @@
 // adopted from: https://github.com/sindresorhus/plur
 // Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com) (MIT)
-var contains = require('../regex/contains')
+var irregularPlurals = require('./IRREGULAR-PLURALS')
 
 function plural (str, plur, count) {
-  if (typeof plur === 'number') {
-    count = plur
-    plur = str.replace(/(?:s|x|z|ch|sh)$/i, '$&e')
+  if (typeof plur === 'number') count = plur
 
-    plur = contains(/(?:[aeiou])y$/i, plur)
-      ? plur + 's'
-      : plur.replace(/y$/i, 'ie') + 's'
-
-    plur = plur.replace(/i?e?s$/i, function (m) {
+  if (str in irregularPlurals) plur = irregularPlurals[str]
+  else if (typeof plur !== 'string') {
+    plur = (str.replace(/(?:s|x|z|ch|sh)$/i, '$&e')
+      .replace(/([^aeiou])y$/i, '$1ie') + 's')
+      .replace(/i?e?s$/i, function (m) {
         var isTailLowerCase = str.slice(-1) === str.slice(-1).toLowerCase()
         return isTailLowerCase ? m.toLowerCase() : m.toUpperCase()
       })
   }
+
   return count === 1 ? str : plur
 }
 
