2197fc8ac45678d0de05c04e987972dd2a069cb4
akileez <git@keeda.net>
2016-04-05 09:16:43 -0400

    update test string/case-sentence — to new version

--- test/spec/string/case-sentence.js
+++ test/spec/string/case-sentence.js
@@ -4,36 +4,16 @@ var t = painless.assert
 
 var sentence = require('../../../src/string/case-sentence')
 
-test('should do a basic test', () => {
-  t.pass()
+test('should uppercase first char of each sentence and lowercase others', function () {
+  var str = 'lorem Ipsum doLOr. sit amet dolor.'
+  t.eq(sentence(str),
+    'Lorem ipsum dolor. Sit amet dolor.')
 })
 
-var strings = {
-  camel: 'thisIsAString',
-  capital: 'This Is A String',
-  constant: 'THIS_IS_A_STRING',
-  dot: 'this.is.a.string',
-  pascal: 'ThisIsAString',
-  sentence: 'This is a string',
-  slug: 'this-is-a-string',
-  snake: 'this_is_a_string',
-  space: 'this is a string',
-  title: 'This Is a String',
-  junk: '-this__is$%a-string...'
-}
-
-for (var key in strings) testr(key)
-
-function testr(key) {
-  test('should convert ' + key + ' case', function () {
-    t.eq(sentence(strings[key]), strings.sentence)
-  })
-}
-
-test('shouldnt touch sentence case', function () {
-  t.eq(sentence('A sentence case string.'), 'A sentence case string.')
+test('should treat null as empty string', function () {
+  t.eq(sentence(null), '')
 })
 
-test('should preserve punctuation', function () {
-  t.eq(sentence('A Title: Case of Something.'), 'A title: case of something.')
+test('should treat undefined as empty string', function () {
+  t.eq(sentence(void 0), '')
 })
