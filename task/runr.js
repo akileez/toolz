5d5ee6c0427e42f67e6df72c1f068c3d5ada6ac7
akileez <git@keeda.net>
2016-04-06 16:24:28 -0400

    update task/runr — format changes not including time nor columns as with latest versions of ygor. keeping things simple. will use time within whatever build script used by runr. columns is just extra for output display. will do something different in the future. (maybe a command line help type of thing.)

--- task/runr.js
+++ task/runr.js
@@ -1,26 +1,28 @@
 // adopted from: ygor <https://github.com/shannonmoeller/ygor>
 // © 2016 Shannon Moeller me@shannonmoeller.com (MIT)
 
-var argv = require('../src/process/argh').argv
-var clr = require('../src/util/colorz')
+var argv   = require('../src/process/argh').argv
+var clr    = require('../src/util/colorz')
 var assert = require('assert')
 
-var cmd = argv.argv || 'default'
-var tasks = Object.create(null)
+var cmd     = argv.argv || 'default'
+var tasks   = Object.create(null)
 
-function logError (err) {
+function error (err) {
   console.error(err && err.stack || err)
+
+  return exports
 }
 
 function run (name) {
   if (!(name in tasks)) {
-    console.log(clr.cyan('Tasks:\n  ') + clr.magenta(Object.keys(tasks).join('\n  ')))
+    console.log(clr.cyan('\nTasks:  ') + clr.magenta(Object.keys(tasks).join('  ')))
     return
   }
 
   return Promise
     .resolve(tasks[name]())
-    .catch(logError)
+    .catch(error)
 }
 
 function task (name, callback) {
@@ -35,5 +37,6 @@ function task (name, callback) {
 exports.run = run
 exports.task = task
 exports.opts = argv
+exports.error = error
 
 process.nextTick(run.bind(null, cmd))
