c1e5237f3e0350e90a28c08aa80a043a2c63819d
akileez <git@keeda.net>
2016-04-07 04:30:45 -0400

    source number/toNum — underscore.string version

---
+++ src/number/toNum.js
@@ -0,0 +1,7 @@
+function toNum (num, precision) {
+  if (num == null) return 0
+  var factor = Math.pow(10, isFinite(precision) ? precision : 0)
+  return Math.round(num * factor) / factor
+}
+
+module.exports = toNum
