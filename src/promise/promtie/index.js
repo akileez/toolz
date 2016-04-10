7a5f2e863bcab8248a3bfb012cb71f47d3505b18
akileez <git@keeda.net>
2016-04-10 18:41:11 -0400

    update promise/index — promtie  lint, es6

--- src/promise/promtie/index.js
+++ src/promise/promtie/index.js
@@ -1,26 +1,27 @@
 /* eslint global-require: 0 */
 
-'use strict';
+'use strict'
 
 // Collections
-exports.each = require('./each');
-exports.map = require('./map');
-exports.filter = require('./filter');
-exports.reduce = require('./reduce');
-exports.values = require('./values');
+exports.each         = require('./each')
+exports.map          = require('./map')
+exports.filter       = require('./filter')
+exports.reduce       = require('./reduce')
+exports.values       = require('./values')
+exports.settle       = require('./settle')
 
 // Others
-exports.attempt = require('./attempt');
-exports.spread = require('./spread');
-exports.retry = require('./retry');
-exports.delay = require('./delay');
-exports.timeout = require('./timeout');
-exports.catchIf = require('./catchIf');
-exports.whilst = require('./whilst');
-exports.times = require('./times');
-exports.through = require('./through');
-exports.nodeify = require('./nodeify');
+exports.attempt      = require('./attempt')
+exports.spread       = require('./spread')
+exports.retry        = require('./retry')
+exports.delay        = require('./delay')
+exports.timeout      = require('./timeout')
+exports.catchIf      = require('./catchIf')
+exports.whilst       = require('./whilst')
+exports.times        = require('./times')
+exports.through      = require('./through')
+exports.nodeify      = require('./nodeify')
 
 // Promisification
-exports.promisify = require('./promisify');
-exports.promisifyAll = require('./promisifyAll');
+exports.promisify    = require('./promisify')
+exports.promisifyAll = require('./promisifyAll')
