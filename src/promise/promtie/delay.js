35656b7975af74f4b2a518a8386643c2ee442a3d
akileez <git@keeda.net>
2016-04-10 18:39:59 -0400

    update promise/delay — lint, es6

--- src/promise/promtie/delay.js
+++ src/promise/promtie/delay.js
@@ -1,27 +1,19 @@
-'use strict';
+'use strict'
 
-function delay(n, fn) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, n);
-    })
-    .then(fn);
-}
+const delay = (n, fn) => new Promise((resolve) => setTimeout(resolve, n).then(fn))
 
-/*
- * Delays the execution of the next promise by `n` milliseconds.
- */
-module.exports = function (n, fn) {
-    if (typeof fn === 'function') {
-        return delay(n, fn);
+// Delays the execution of the next promise by `n` milliseconds.
+
+module.exports = (n, fn) => {
+  if (typeof fn === 'function') {
+    return delay(n, fn)
+  }
+
+  return (value) => delay(n, () => {
+    if (value instanceof Error) {
+      throw value
     }
 
-    return function (value) {
-        return delay(n, function () {
-            if (value instanceof Error) {
-                throw value;
-            }
-
-            return value;
-        });
-    };
-};
+    return value
+  })
+}
