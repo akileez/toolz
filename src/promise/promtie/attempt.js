fd25f9a57980343bba71d46a0b64d4d4b5a80c69
akileez <git@keeda.net>
2016-04-10 18:39:23 -0400

    update promise/attempt — lint, es6

--- src/promise/promtie/attempt.js
+++ src/promise/promtie/attempt.js
@@ -1,14 +1,15 @@
-'use strict';
+'use strict'
 
 /*
- * Start a chain of promises while wrapping synchronous exceptions to promise rejections.
- */
-module.exports = function (fn) {
-    return new Promise(function (resolve, reject) {
-        try {
-            resolve(fn());
-        } catch (err) {
-            reject(err);
-        }
-    });
-};
+  Start a chain of promises while wrapping synchronous exceptions
+  to promise rejections.
+
+*/
+
+module.exports = (fn) => new Promise((resolve, reject) => {
+  try {
+    resolve(fn())
+  } catch (err) {
+    reject(err)
+  }
+})
