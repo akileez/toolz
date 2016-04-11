2a8904e8d0999c00b1aeacc8d17ec1b6372f5cc1
akileez <git@keeda.net>
2016-04-11 12:11:38 -0400

    update promise/nodeify — lint, es6

--- src/promise/promtie/nodeify.js
+++ src/promise/promtie/nodeify.js
@@ -1,26 +1,26 @@
-'use strict';
+'use strict'
 
 /*
- * Returns a function that calls the callback function with the resulting value.
- * If the callback throws an error, it will be globally thrown.
- * If no callback is provided, the returned function simply returns the value.
- */
-module.exports = function (callback, ctx) {
-    return function (value) {
-        if (callback) {
-            if (value instanceof Error) {
-                callback = callback.bind(ctx, value);
-            } else {
-                callback = callback.bind(ctx, null, value);
-            }
+  Returns a function that calls the callback function with the resulting value.
+  If the callback throws an error, it will be globally thrown.
+  If no callback is provided, the returned function simply returns the value.
 
-            return setImmediate(callback);
-        }
+*/
 
-        if (value instanceof Error) {
-            throw value;
-        }
+module.exports = (callback, ctx) => value => {
+  if (callback) {
+    if (value instanceof Error) {
+      callback = callback.bind(ctx, value)
+    } else {
+      callback = callback.bind(ctx, null, value)
+    }
 
-        return value;
-    };
-};
+    return setImmediate(callback)
+  }
+
+  if (value instanceof Error) {
+    throw value
+  }
+
+  return value
+}
