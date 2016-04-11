64d178110afdd9c9bf11f2d699a72f032aaa1c3d
akileez <git@keeda.net>
2016-04-11 12:12:34 -0400

    update promise/spread — lint, es6

--- src/promise/promtie/spread.js
+++ src/promise/promtie/spread.js
@@ -1,24 +1,22 @@
-'use strict';
+'use strict'
 
-function spread(array, fn, ctx) {
-    return Promise.all(array)
-    .then(function (args) {
-        return fn.apply(ctx || fn, args);
-    });
+function spread (array, fn, ctx) {
+  return Promise.all(array)
+    .then((args) => fn.apply(ctx || fn, args))
 }
 
 /*
- * Spreads array values to the arguments of `fn`.
- * You can pass a context object to bind to the function call.
- */
-module.exports = function (array, fn, ctx) {
-    if (typeof array === 'function') {
-        fn = array;
+  Spreads array values to the arguments of `fn`.
+  You can pass a context object to bind to the function call.
 
-        return function (array) {
-            return spread(array, fn, ctx);
-        };
-    }
+*/
 
-    return spread(array, fn, ctx);
-};
+module.exports = (array, fn, ctx) => {
+  if (typeof array === 'function') {
+    fn = array
+
+    return (array) => spread(array, fn, ctx)
+  }
+
+  return spread(array, fn, ctx)
+}
