0e02a940549c413c7787ec63370d43d47907d4b0
akileez <git@keeda.net>
2016-04-11 12:12:15 -0400

    update promise/reduce — lint, es6

--- src/promise/promtie/reduce.js
+++ src/promise/promtie/reduce.js
@@ -1,44 +1,43 @@
-'use strict';
+'use strict'
 
-var isPromise = require('./util/isPromise');
+var isPromise = require('./util/isPromise')
 
-function reduce(array, fn, initialValue) {
-    var promiseReduce = isPromise(initialValue) ? initialValue : Promise.resolve(initialValue);
+function reduce (array, fn, initialValue) {
+  var promiseReduce = isPromise(initialValue) ? initialValue : Promise.resolve(initialValue)
 
-    array.forEach(function (value, i) {
-        promiseReduce = promiseReduce.then(function (acc) {
-            if (acc == null) {
-                return value;
-            }
+  array.forEach((value, i) => {
+    promiseReduce = promiseReduce.then((acc) => {
+      if (acc == null) {
+        return value
+      }
 
-            if (isPromise(value)) {
-                return value.then(function (value) {
-                    return fn(acc, value, i, array.length);
-                });
-            }
+      if (isPromise(value)) {
+        return value.then((value) => fn(acc, value, i, array.length))
+      }
 
-            return fn(acc, value, i, array.length);
-        });
-    });
+      return fn(acc, value, i, array.length)
+    })
+  })
 
-    return promiseReduce;
+  return promiseReduce
 }
 
 /*
- * Iterates over the array and calls fn on each value and accumulates the result to reduce it to a single value.
- *
- * If called as `reduce(fn, [initialValue])` it returns a function that takes the array
- * and returns the desired Promise.
- */
-module.exports = function (array, fn, initialValue) {
-    if (typeof array === 'function') {
-        initialValue = fn;
-        fn = array;
+  Iterates over the array and calls fn on each value and accumulates
+  the result to reduce it to a single value.
 
-        return function (array) {
-            return reduce(array, fn, initialValue);
-        };
-    }
+  If called as `reduce(fn, [initialValue])` it returns a function that takes the array
+  and returns the desired Promise.
 
-    return reduce(array, fn, initialValue);
-};
+*/
+
+module.exports = (array, fn, initialValue) => {
+  if (typeof array === 'function') {
+    initialValue = fn
+    fn = array
+
+    return (array) => reduce(array, fn, initialValue)
+  }
+
+  return reduce(array, fn, initialValue)
+}
