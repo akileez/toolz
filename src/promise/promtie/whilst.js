e8ec1e6f63b120efb612cbbe3cd4da1d50b34f10
akileez <git@keeda.net>
2016-04-11 12:13:27 -0400

    update promise/whilist — lint, es6

--- src/promise/promtie/whilst.js
+++ src/promise/promtie/whilst.js
@@ -1,26 +1,22 @@
-'use strict';
+'use strict'
 
-var delay = require('./delay');
-var attempt = require('./attempt');
-
-function whilst(conditionFn, fn, options) {
-    return attempt(conditionFn).then(function (condition) {
-        return condition && Promise.resolve(fn())
-        // Even if delay is 0, we always do a setTimeout in order to let the event loop breathe.
-        .then(delay(options.delay))
-        .then(function () {
-            return whilst(conditionFn, fn, options);
-        });
-    });
-}
+var delay = require('./delay')
+var attempt = require('./attempt')
 
 /*
- * Whilst the `conditionFn` returns true, execute `fn`.
- * `fn` and `conditionFn` can return a value or a promise.
- * Delay in between iterations can be configured with `options.delay` (default is 0).
- */
-module.exports = function (conditionFn, fn, options) {
-    options = options || { delay: 0 };
+  Whilst the `conditionFn` returns true, execute `fn`.
+  `fn` and `conditionFn` can return a value or a promise.
+  Delay in between iterations can be configured with `options.delay` (default is 0).
 
-    return whilst(conditionFn, fn, options);
-};
+*/
+
+module.exports = function whilst (conditionFn, fn, options) {
+  options = options || {delay: 0}
+
+  return attempt(conditionFn).then((condition) => {
+    return condition && Promise.resolve(fn())
+      // Even if delay is 0, we always do a setTimeout in order to let the event loop breathe.
+      .then(delay(options.delay))
+      .then(() => whilst(conditionFn, fn, options))
+  })
+}
