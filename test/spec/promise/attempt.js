e21265de2e6c7caa89071004cf923632e8a3a697
akileez <git@keeda.net>
2016-04-11 12:17:30 -0400

    source test promise/attempt

---
+++ test/spec/promise/attempt.js
@@ -0,0 +1,21 @@
+var painless = require('../../assertion/painless')
+var test = painless.createGroup('Test promise/promtie::attempt')
+var t = painless.assert
+
+var attempt = require('../../../src/promise/promtie/attempt')
+
+test('attempt(fn)', () => {
+    return attempt(() => {
+        return 'unicorn'
+    })
+    .then((unicorn) => t.is(unicorn, 'unicorn'))
+})
+
+test('attempt(fn): deal with promise failure', () => {
+    return attempt(() => {
+        throw new Error('Bad unicorn')
+    })
+    .then(() => t.fail('Promise expected to reject'), err => {
+        t.truthy(err instanceof Error)
+    })
+})
