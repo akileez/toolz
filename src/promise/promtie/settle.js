373f2d19b94735b2049e6d65177fe5b688390eb2
akileez <git@keeda.net>
2016-04-11 12:15:31 -0400

    source promise/settle — lint, es6

---
+++ src/promise/promtie/settle.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// Wait until all the promises in the array settle
+// and then resolve a promise with all the fulfilment values and/or rejections.
+
+module.exports = (array) => Promise.all(array.map((promise) => {
+  return Promise.resolve(promise) // Make sure it is a promise
+  .then((value) => {
+    return {
+      fulfilled: true,
+      rejected: false,
+      value: value,
+      reason: null
+    }
+  }, (err) => {
+    return {
+      fulfilled: false,
+      rejected: true,
+      value: null,
+      reason: err
+    }
+  })
+}))
