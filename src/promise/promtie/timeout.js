1b9b99e12743a9d7564fb3f6ba3b07a718f800c9
akileez <git@keeda.net>
2016-04-11 12:12:55 -0400

    update promise/timeout — lint, es6

--- src/promise/promtie/timeout.js
+++ src/promise/promtie/timeout.js
@@ -1,35 +1,35 @@
-'use strict';
+'use strict'
 
-var attempt = require('./attempt');
+var attempt = require('./attempt')
 
 /*
- * Do not wait more than `n` milliseconds for the operation to finish.
- * If Timeout is reached, the promise is rejected with `timeout.TimeoutError`.
- */
-function timeout(timeout, promise) {
-    if (typeof promise === 'function') {
-        promise = attempt(promise);
-    }
+  Do not wait more than `n` milliseconds for the operation to finish.
+  If Timeout is reached, the promise is rejected with `timeout.TimeoutError`.
 
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            reject(new TimeoutError());
-        }, timeout);
+*/
 
-        return promise.then(resolve, reject);
-    });
+function timeout (timeout, promise) {
+  if (typeof promise === 'function') {
+    promise = attempt(promise)
+  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(new TimeoutError()), timeout)
+
+    return promise.then(resolve, reject)
+  })
 }
 
 /*
  * TimeoutError error object.
  */
-function TimeoutError() {
-    this.message = 'TimeoutError: Operation timed out';
-    this.code = 'ETIMEOUT';
+function TimeoutError () {
+  this.message = 'TimeoutError: Operation timed out'
+  this.code = 'ETIMEOUT'
 }
 
-TimeoutError.prototype = Object.create(Error.prototype);
-TimeoutError.prototype.constructor = TimeoutError;
+TimeoutError.prototype = Object.create(Error.prototype)
+TimeoutError.prototype.constructor = TimeoutError
 
-module.exports = timeout;
-module.exports.TimeoutError = TimeoutError;
+module.exports = timeout
+module.exports.TimeoutError = TimeoutError
