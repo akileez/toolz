22918180964b13405ae6439fa2975b9969fc855e
akileez <git@keeda.net>
2016-04-13 04:27:36 -0400

    update promise/settle — streamline

--- src/promise/promtie/settle.js
+++ src/promise/promtie/settle.js
@@ -3,21 +3,17 @@
 // Wait until all the promises in the array settle
 // and then resolve a promise with all the fulfilment values and/or rejections.
 
-module.exports = (array) => Promise.all(array.map((promise) => {
-  return Promise.resolve(promise) // Make sure it is a promise
-  .then((value) => {
-    return {
+module.exports = (array) => Promise.all(array.map((promise) =>
+  Promise.resolve(promise) // Make sure it is a promise
+  .then((value) => ({
       fulfilled: true,
       rejected: false,
       value: value,
       reason: null
-    }
-  }, (err) => {
-    return {
+  }), (err) => ({
       fulfilled: false,
       rejected: true,
       value: null,
       reason: err
-    }
-  })
-}))
+  }))
+))
