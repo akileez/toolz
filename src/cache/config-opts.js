c4e86fb0ed865b03e36e575bf5f78f2e669b1dde
akileez <git@keeda.net>
2016-04-14 09:00:04 -0400

    update cache/config-opts — remove methods adding to cache/config

--- src/cache/config-opts.js
+++ src/cache/config-opts.js
@@ -1,25 +1,13 @@
 var stampit  = require('../object/stamp')
-var has      = require('../object/has')
 
 module.exports = stampit()
   .methods({
-    isEnabled: enabled,
-    isDisabled: disabled,
     isTrue: isTrue,
     isFalse: isFalse,
     isBoolean: isBoolean,
-    hasOption: hasOption,
     canDisable: disableCheck,
     canEnable: enableCheck
   })
-
-function enabled (key) {
-  return Boolean(this.config[key])
-}
-
-function disabled (key) {
-  return !Boolean(this.config[key])
-}
 
 function isTrue (key) {
   return this.config[key] === true
@@ -31,11 +19,6 @@ function isFalse (key) {
 
 function isBoolean (key) {
   return typeof this.config[key] === 'boolean'
-}
-
-function hasOption (key) {
-  if (key.indexOf('.') === -1) return this.config.hasOwnProperty(key)
-  return has(this.config, key)
 }
 
 function disableCheck (key) {
