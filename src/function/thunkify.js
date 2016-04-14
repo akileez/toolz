b75938bfde866768320fe1b8d7a2d1c2b1527634
akileez <git@keeda.net>
2016-04-14 09:01:10 -0400

    update function/thunkify — reformat

--- src/function/thunkify.js
+++ src/function/thunkify.js
@@ -1,29 +1,11 @@
 // https://github.com/tj/node-thunkify
 // (MIT)
 
-/**
- * Module dependencies.
- */
-
-var assert = require('assert');
-
-/**
- * Expose `thunkify()`.
- */
-
-module.exports = thunkify;
-
-/**
- * Wrap a regular callback `fn` as a thunk.
- *
- * @param {Function} fn
- * @return {Function}
- * @api public
- */
+var assert = require('assert')
 
 /*
-
-Turn a regular node function into one which returns a thunk, useful for generator-based flow control such as co.
+  Turn a regular node function into one which returns a thunk,
+  useful for generator-based flow control such as co.
 
     var thunkify = require('thunkify');
     var fs = require('fs');
@@ -31,36 +13,43 @@ Turn a regular node function into one which returns a thunk, useful for generato
     var read = thunkify(fs.readFile);
 
     read('package.json', 'utf8')(function(err, str){
-
+      // something here
     });
 
+  Wrap a regular callback `fn` as a thunk.
+    @param {Function} fn
+    @return {Function}
+    @api public
 */
 
-function thunkify(fn){
-  assert('function' == typeof fn, 'function required');
+function thunkify (fn) {
+  assert(typeof fn === 'function', 'function required')
 
-  return function(){
-    var args = new Array(arguments.length);
-    var ctx = this;
+  return function () {
+    var args = new Array(arguments.length)
+    var ctx = this
 
     for(var i = 0; i < args.length; ++i) {
-      args[i] = arguments[i];
+      args[i] = arguments[i]
     }
 
-    return function(done){
-      var called;
+    return function (done) {
+      var called
 
-      args.push(function(){
-        if (called) return;
-        called = true;
-        done.apply(null, arguments);
-      });
+      args.push(function () {
+        if (called) return
+        called = true
+        done.apply(null, arguments)
+      })
 
       try {
-        fn.apply(ctx, args);
+        fn.apply(ctx, args)
       } catch (err) {
-        done(err);
+        done(err)
       }
     }
   }
-};
+}
+
+module.exports = thunkify
+
