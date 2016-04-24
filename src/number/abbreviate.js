acd289458a2e5974cb3e9a36864be7876703e5d9
akileez <git@keeda.net>
2016-04-24 17:11:57 -0400

    bug fix number/abbreviate should have summed, instead I made an assignment.

--- src/number/abbreviate.js
+++ src/number/abbreviate.js
@@ -19,7 +19,7 @@ function abbreviateNumber (val, nDecimals, dict) {
   if (val < 1000000) {
     mod = enforcePrecision(val / 1000, nDecimals)
     // might overflow to next scale during rounding
-    str = mod < 1000 ? mod = dict.thousand : 1 + dict.million
+    str = mod < 1000 ? mod + dict.thousand : 1 + dict.million
   } else if (val < 1000000000) {
     mod = enforcePrecision(val / 1000000, nDecimals)
     str = mod < 1000 ? mod + dict.million : 1 + dict.billion
