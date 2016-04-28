3e2ad7a9caf9192799b465327f6dc474af973057
akileez <git@keeda.net>
2016-04-28 17:33:11 -0400

    lint path/req-glob

--- src/path/req-glob.js
+++ src/path/req-glob.js
@@ -1,49 +1,49 @@
 // require-glob <https://github.com/shannonmoeller/require-glob>
 // © 2016 Shannon Moeller me@shannonmoeller.com (MIT)
 
-'use strict';
+'use strict'
 
-var globParent = require('../glob/glob-parent');
-var globby = require('../glob/globbieP');
-var path = require('path');
+var globParent = require('../glob/glob-parent')
+var globby = require('../glob/globbieP')
+var path = require('path')
 
-var CAMELIZE_PATTERN = /[\.\-]+(.)/g;
-var SEPARATOR_PATTERN = /[\\\/]/;
+var CAMELIZE_PATTERN = /[\.\-]+(.)/g
+var SEPARATOR_PATTERN = /[\\\/]/
 
 // Utilities
 
-function getParentDir() {
-  return path.dirname(module.parent.filename);
+function getParentDir () {
+  return path.dirname(module.parent.filename)
 }
 
-function toCamelCase(value) {
+function toCamelCase (value) {
   return value.replace(CAMELIZE_PATTERN, function (match, character) {
-    return character.toUpperCase();
-  });
+    return character.toUpperCase()
+  })
 }
 
-function toCombinedValues(a, b) {
-  return a.concat(b);
+function toCombinedValues (a, b) {
+  return a.concat(b)
 }
 
-function toNestedObject(obj, key) {
-  return obj[key] || (obj[key] = {});
+function toNestedObject (obj, key) {
+  return obj[key] || (obj[key] = {})
 }
 
-function toSplitPath(filePath) {
-  return filePath.split(SEPARATOR_PATTERN);
+function toSplitPath (filePath) {
+  return filePath.split(SEPARATOR_PATTERN)
 }
 
 // Map Reduce
 
-function mapper(options, filePath) {
-  var cwd = options.cwd;
-  var base = options.base;
+function mapper (options, filePath) {
+  var cwd = options.cwd
+  var base = options.base
 
-  filePath = require.resolve(path.resolve(cwd, filePath));
+  filePath = require.resolve(path.resolve(cwd, filePath))
 
   if (options.bustCache) {
-    delete require.cache[filePath];
+    delete require.cache[filePath]
   }
 
   return {
@@ -51,74 +51,74 @@ function mapper(options, filePath) {
     base: base,
     path: filePath,
     exports: require(filePath)
-  };
+  }
 }
 
-function reducer(options, tree, fileObj) {
+function reducer (options, tree, fileObj) {
   if (!fileObj || !fileObj.path || !fileObj.hasOwnProperty('exports')) {
-    return tree;
+    return tree
   }
 
-  var keys = [].concat(options.keygen(fileObj));
+  var keys = [].concat(options.keygen(fileObj))
 
   if (!keys.length) {
-    return tree;
+    return tree
   }
 
-  var lastKey = keys.pop();
-  var obj = keys.reduce(toNestedObject, tree);
+  var lastKey = keys.pop()
+  var obj = keys.reduce(toNestedObject, tree)
 
-  obj[lastKey] = fileObj.exports;
+  obj[lastKey] = fileObj.exports
 
-  return tree;
+  return tree
 }
 
-function keygen(options, fileObj) {
-  var uniquePath = fileObj.path.replace(fileObj.base, '');
-  var parsedPath = path.parse(uniquePath);
+function keygen (options, fileObj) {
+  var uniquePath = fileObj.path.replace(fileObj.base, '')
+  var parsedPath = path.parse(uniquePath)
 
   return [parsedPath.dir, parsedPath.name]
     .map(toSplitPath)
     .reduce(toCombinedValues)
     .map(toCamelCase)
-    .filter(Boolean);
+    .filter(Boolean)
 }
 
-function mapReduce(options, filePaths) {
+function mapReduce (options, filePaths) {
   return filePaths
     .map(options.mapper)
-    .reduce(options.reducer, {});
+    .reduce(options.reducer, {})
 }
 
 // API
 
-function normalizeOptions(pattern, options) {
-  pattern = [].concat(pattern || '');
-  options = options || {};
+function normalizeOptions (pattern, options) {
+  pattern = [].concat(pattern || '')
+  options = options || {}
 
-  options.cwd = options.cwd || getParentDir();
-  options.base = options.base || globParent(path.resolve(options.cwd, pattern[0]));
-  options.bustCache = options.bustCache || false;
+  options.cwd = options.cwd || getParentDir()
+  options.base = options.base || globParent(path.resolve(options.cwd, pattern[0]))
+  options.bustCache = options.bustCache || false
 
-  options.mapper = (options.mapper || mapper).bind(null, options);
-  options.reducer = (options.reducer || reducer).bind(null, options);
-  options.keygen = (options.keygen || keygen).bind(null, options);
+  options.mapper = (options.mapper || mapper).bind(null, options)
+  options.reducer = (options.reducer || reducer).bind(null, options)
+  options.keygen = (options.keygen || keygen).bind(null, options)
 
-  return options;
+  return options
 }
 
-function requireGlob(pattern, options) {
-  options = normalizeOptions(pattern, options);
+function requireGlob (pattern, options) {
+  options = normalizeOptions(pattern, options)
 
   return globby(pattern, options)
-    .then(mapReduce.bind(null, options));
+    .then(mapReduce.bind(null, options))
 }
 
-function requireGlobSync(pattern, options) {
-  options = normalizeOptions(pattern, options);
+function requireGlobSync (pattern, options) {
+  options = normalizeOptions(pattern, options)
 
-  return mapReduce(options, globby.sync(pattern, options));
+  return mapReduce(options, globby.sync(pattern, options))
 }
 
-module.exports = requireGlob;
-module.exports.sync = requireGlobSync;
+module.exports = requireGlob
+module.exports.sync = requireGlobSync
