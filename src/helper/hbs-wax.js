6742c8bbaf889d2f5f7c47f691c70f9c82bb2246
akileez <git@keeda.net>
2016-04-28 17:19:53 -0400

    lint helper/hbs-wax — and format for my readability

--- src/helper/hbs-wax.js
+++ src/helper/hbs-wax.js
@@ -1,219 +1,216 @@
 // handlebars-wax <https://github.com/shannonmoeller/handlebars-wax>
 // © Shannon Moeller me@shannonmoeller.com (shannonmoeller.com) (MIT)
 
-'use strict';
+'use strict'
 
-var assign = require('../object/asign');
-var fs = require('fs');
-var path = require('path');
-var requireGlob = require('../path/req-glob');
-var toString = Object.prototype.toString;
+var fs = require('fs')
+var path = require('path')
+var assign = require('../object/assign')
+var requireGlob = require('../path/req-glob')
+var toString = Object.prototype.toString
 
-var NON_WORD_CHARACTERS = /\W+/g;
-var PATH_SEPARATOR = '/';
-var PATH_SEPARATORS = /[\\\/]/g;
-var WHITESPACE_CHARACTERS = /\s+/g;
-var WORD_SEPARATOR = '-';
-var TYPE_FUNCTION = 'fun';
-var TYPE_OBJECT = 'obj';
+var NON_WORD_CHARACTERS = /\W+/g
+var PATH_SEPARATOR = '/'
+var PATH_SEPARATORS = /[\\\/]/g
+var WHITESPACE_CHARACTERS = /\s+/g
+var WORD_SEPARATOR = '-'
+var TYPE_FUNCTION = 'fun'
+var TYPE_OBJECT = 'obj'
 
 // Utilities
 
-function getParentDir() {
-  return path.dirname(module.parent.filename);
-}
-
-function getTypeOf(value) {
+function getTypeOf (value) {
   return toString
     .call(value)
     .substr(8, 3)
-    .toLowerCase();
+    .toLowerCase()
 }
 
-function hookRequire(handlebars) {
-  var extLong = require.extensions['.handlebars'];
-  var extShort = require.extensions['.hbs'];
+function hookRequire (handlebars) {
+  var extLong = require.extensions['.handlebars']
+  var extShort = require.extensions['.hbs']
 
-  function extensions(module, filename) {
-    var templateString = fs.readFileSync(filename, 'utf8');
+  function extensions (module, filename) {
+    var templateString = fs.readFileSync(filename, 'utf8')
 
-    module.exports = handlebars.compile(templateString);
+    module.exports = handlebars.compile(templateString)
   }
 
-  require.extensions['.handlebars'] = extensions;
-  require.extensions['.hbs'] = extensions;
+  require.extensions['.handlebars'] = extensions
+  require.extensions['.hbs'] = extensions
 
-  return function unhookRequire() {
-    require.extensions['.handlebars'] = extLong;
-    require.extensions['.hbs'] = extShort;
-  };
+  return function unhookRequire () {
+    require.extensions['.handlebars'] = extLong
+    require.extensions['.hbs'] = extShort
+  }
 }
 
 // Map Reduce
 
-function keygenPartial(options, file) {
-  var fullPath = file.path.replace(PATH_SEPARATORS, PATH_SEPARATOR);
-  var basePath = file.base.replace(PATH_SEPARATORS, PATH_SEPARATOR) + PATH_SEPARATOR;
-  var shortPath = fullPath.replace(basePath, '');
-  var extension = path.extname(shortPath);
+function keygenPartial (options, file) {
+  var fullPath = file.path.replace(PATH_SEPARATORS, PATH_SEPARATOR)
+  var basePath = file.base.replace(PATH_SEPARATORS, PATH_SEPARATOR) + PATH_SEPARATOR
+  var shortPath = fullPath.replace(basePath, '')
+  var extension = path.extname(shortPath)
 
   return shortPath
     .substr(0, shortPath.length - extension.length)
-    .replace(WHITESPACE_CHARACTERS, WORD_SEPARATOR);
+    .replace(WHITESPACE_CHARACTERS, WORD_SEPARATOR)
 }
 
-function keygenHelper(options, file) {
+function keygenHelper (options, file) {
   return keygenPartial(options, file)
-    .replace(NON_WORD_CHARACTERS, WORD_SEPARATOR);
+    .replace(NON_WORD_CHARACTERS, WORD_SEPARATOR)
 }
 
-function keygenDecorator(options, file) {
-  return keygenHelper(options, file);
+function keygenDecorator (options, file) {
+  return keygenHelper(options, file)
 }
 
-function reducer(options, obj, fileObj) {
-  var value = fileObj.exports;
+function reducer (options, obj, fileObj) {
+  var value = fileObj.exports
 
   if (!value) {
-    return obj;
+    return obj
   }
 
   if (getTypeOf(value.register) === TYPE_FUNCTION) {
-    value = value.register(options.handlebars, options);
+    value = value.register(options.handlebars, options)
 
     if (getTypeOf(value) === TYPE_OBJECT) {
-      return assign(obj, value);
+      return assign(obj, value)
     }
 
-    return obj;
+    return obj
   }
 
   if (getTypeOf(value) === TYPE_OBJECT) {
-    return assign(obj, value);
+    return assign(obj, value)
   }
 
-  obj[options.keygen(fileObj)] = value;
+  obj[options.keygen(fileObj)] = value
 
-  return obj;
+  return obj
 }
 
-function resolveValue(options, value) {
+function resolveValue (options, value) {
   if (!value) {
-    return {};
+    return {}
   }
 
   if (getTypeOf(value) === TYPE_FUNCTION) {
-    value = value(options.handlebars, options);
+    value = value(options.handlebars, options)
 
     if (getTypeOf(value) === TYPE_OBJECT) {
-      return value;
+      return value
     }
 
-    return {};
+    return {}
   }
 
   if (getTypeOf(value) === TYPE_OBJECT) {
-    return reducer(options, {}, {exports: value});
+    return reducer(options, {}, {exports: value})
   }
 
-  return requireGlob.sync(value, options);
+  return requireGlob.sync(value, options)
 }
 
 // Wax
 
-function HandlebarsWax(handlebars, options) {
+function HandlebarsWax (handlebars, options) {
   var defaults = {
     handlebars: handlebars,
-    cwd: getParentDir(),
+    bustCache: true,
+    cwd: process.cwd(),
     compileOptions: null,
     templateOptions: null,
     parsePartialName: keygenPartial,
     parseHelperName: keygenHelper,
     parseDecoratorName: keygenDecorator,
     parseDataName: null
-  };
+  }
 
-  this.handlebars = handlebars;
-  this.config = assign(defaults, options);
-  this.context = Object.create(null);
+  this.handlebars = handlebars
+  this.config = assign(defaults, options)
+  this.context = Object.create(null)
 }
 
 HandlebarsWax.prototype.partials = function (partials, options) {
-  options = assign({}, this.config, options);
-  options.keygen = options.parsePartialName;
-  options.reducer = options.reducer || reducer;
+  options = assign({}, this.config, options)
+  options.keygen = options.parsePartialName
+  options.reducer = options.reducer || reducer
 
-  var unhookRequire = hookRequire(options.handlebars);
+  var unhookRequire = hookRequire(options.handlebars)
 
   options.handlebars.registerPartial(
     resolveValue(options, partials)
-  );
+  )
 
-  unhookRequire();
+  unhookRequire()
 
-  return this;
-};
+  return this
+}
 
 HandlebarsWax.prototype.helpers = function (helpers, options) {
-  options = assign({}, this.config, options);
-  options.keygen = options.parseHelperName;
-  options.reducer = options.reducer || reducer;
+  options = assign({}, this.config, options)
+  options.keygen = options.parseHelperName
+  options.reducer = options.reducer || reducer
 
   options.handlebars.registerHelper(
     resolveValue(options, helpers)
-  );
+  )
 
-  return this;
-};
+  return this
+}
 
 HandlebarsWax.prototype.decorators = function (decorators, options) {
-  options = assign({}, this.config, options);
-  options.keygen = options.parseDecoratorName;
-  options.reducer = options.reducer || reducer;
+  options = assign({}, this.config, options)
+  options.keygen = options.parseDecoratorName
+  options.reducer = options.reducer || reducer
 
   options.handlebars.registerDecorator(
     resolveValue(options, decorators)
-  );
+  )
 
-  return this;
-};
+  return this
+}
 
 HandlebarsWax.prototype.data = function (data, options) {
-  options = assign({}, this.config, options);
-  options.keygen = options.parseDataName;
+  options = assign({}, this.config, options)
+  options.keygen = options.parseDataName
 
-  assign(this.context, resolveValue(options, data));
+  assign(this.context, resolveValue(options, data))
 
-  return this;
-};
+  return this
+}
 
 HandlebarsWax.prototype.compile = function (template, compileOptions) {
-  var config = this.config;
-  var context = this.context;
+  var config = this.config
+  var context = this.context
 
-  compileOptions = assign({}, config.compileOptions, compileOptions);
+  compileOptions = assign({}, config.compileOptions, compileOptions)
 
   if (getTypeOf(template) !== TYPE_FUNCTION) {
-    template = this.handlebars.compile(template, compileOptions);
+    template = this.handlebars.compile(template, compileOptions)
   }
 
   return function (data, templateOptions) {
-    templateOptions = assign({}, config.templateOptions, templateOptions);
-    templateOptions.data = assign({}, templateOptions.data);
+    templateOptions = assign({}, config.templateOptions, templateOptions)
+    templateOptions.data = assign({}, templateOptions.data)
 
     // {{@root.foo}} and {{@root._parent.foo}}
-    templateOptions.data.root = assign({_parent: context}, templateOptions.data.root || context);
+    templateOptions.data.root = assign({_parent: context}, templateOptions.data.root || context)
 
     // {{foo}} and {{_parent.foo}}
-    return template(assign({_parent: context}, context, data), templateOptions);
-  };
-};
+    return template(assign({_parent: context}, context, data), templateOptions)
+  }
+}
 
 // API
 
-function handlebarsWax(handlebars, config) {
-  return new HandlebarsWax(handlebars, config);
+function handlebarsWax (handlebars, config) {
+  return new HandlebarsWax(handlebars, config)
 }
 
-module.exports = handlebarsWax;
-module.exports.HandlebarsWax = HandlebarsWax;
+module.exports = handlebarsWax
+module.exports.HandlebarsWax = HandlebarsWax
