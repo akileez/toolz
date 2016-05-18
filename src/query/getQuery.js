f8440bebc5b0f79718b84ab0bbf855e4b928e68a
akileez <git@keeda.net>
2016-05-18 14:25:51 -0400

    format query/getQuery — minor

--- src/query/getQuery.js
+++ src/query/getQuery.js
@@ -2,9 +2,13 @@
 
 function getQuery (url) {
   url = url.replace(/#.*/, '')
+
    //valid chars according to: http://www.ietf.org/rfc/rfc1738.txt
   var queryString = /\?[a-zA-Z0-9\=\&\%\$\-\_\.\+\!\*\'\(\)\,]+/.exec(url)
-  return (queryString) ? decodeURIComponent(queryString[0]) : ''
+
+  return (queryString)
+    ? decodeURIComponent(queryString[0])
+    : ''
 }
 
 module.exports = getQuery
