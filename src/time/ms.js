87ee333adabb1499637538e3aabcc6b0558b08bb
akileez <git@keeda.net>
2016-05-18 04:59:33 -0400

    lint time/ms small refactor removing switch statement

--- src/time/ms.js
+++ src/time/ms.js
@@ -1,125 +1,90 @@
-/**
- * Helpers.
- */
+// adopted from: ms.js <https://github.com/rauchg/ms.js>
+// Copyright (c) 2014 Guillermo Rauch <rauchg@gmail.com>
 
-var s = 1000;
-var m = s * 60;
-var h = m * 60;
-var d = h * 24;
-var y = d * 365.25;
+// Constants
+var s = 1000
+var m = 60000 // s * 60
+var h = 3600000 // m * 60
+var d = 86400000 // h * 24
+var y = 31557600000 // d * 365.25
+var types = {
+  'years': y,
+  'year': y,
+  'yrs': y,
+  'yr': y,
+  'y': y,
+  'days': d,
+  'day': d,
+  'd': d,
+  'hours': h,
+  'hour': h,
+  'hrs': h,
+  'hr': h,
+  'h': h,
+  'minutes': m,
+  'minute': m,
+  'mins': m,
+  'min': m,
+  'm': m,
+  'seconds': s,
+  'second': s,
+  'secs': s,
+  'sec': s,
+  's': s,
+  'milliseconds': 1,
+  'millisecond': 1,
+  'msecs': 1,
+  'msec': 1,
+  'ms': 1
+}
 
-/**
- * Parse or format the given `val`.
- *
- * Options:
- *
- *  - `long` verbose formatting [false]
- *
- * @param {String|Number} val
- * @param {Object} options
- * @return {String|Number}
- * @api public
- */
+// Parse or format the given `val` [String|Number]
+// Options: `long` verbose formatting [false]
+module.exports = function (val, options) {
+  options = options || {}
+  if (typeof val === 'string') return parse(val)
 
-module.exports = function(val, options){
-  options = options || {};
-  if ('string' == typeof val) return parse(val);
   return options.long
     ? long(val)
-    : short(val);
-};
-
-/**
- * Parse the given `str` and return milliseconds.
- *
- * @param {String} str
- * @return {Number}
- * @api private
- */
-
-function parse(str) {
-  str = '' + str;
-  if (str.length > 10000) return;
-  var match = /^((?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|years?|yrs?|y)?$/i.exec(str);
-  if (!match) return;
-  var n = parseFloat(match[1]);
-  var type = (match[2] || 'ms').toLowerCase();
-  switch (type) {
-    case 'years':
-    case 'year':
-    case 'yrs':
-    case 'yr':
-    case 'y':
-      return n * y;
-    case 'days':
-    case 'day':
-    case 'd':
-      return n * d;
-    case 'hours':
-    case 'hour':
-    case 'hrs':
-    case 'hr':
-    case 'h':
-      return n * h;
-    case 'minutes':
-    case 'minute':
-    case 'mins':
-    case 'min':
-    case 'm':
-      return n * m;
-    case 'seconds':
-    case 'second':
-    case 'secs':
-    case 'sec':
-    case 's':
-      return n * s;
-    case 'milliseconds':
-    case 'millisecond':
-    case 'msecs':
-    case 'msec':
-    case 'ms':
-      return n;
-  }
+    : short(val)
 }
 
-/**
- * Short format for `ms`.
- *
- * @param {Number} ms
- * @return {String}
- * @api private
- */
+// Parse the given `str` and return milliseconds
+function parse (str) {
+  str = '' + str
+  if (str.length > 10000) return
 
-function short(ms) {
-  if (ms >= d) return Math.round(ms / d) + 'd';
-  if (ms >= h) return Math.round(ms / h) + 'h';
-  if (ms >= m) return Math.round(ms / m) + 'm';
-  if (ms >= s) return Math.round(ms / s) + 's';
-  return ms + 'ms';
+  var match = /^((?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|years?|yrs?|y)?$/i.exec(str)
+
+  if (!match) return
+
+  var n = parseFloat(match[1])
+  var type = (match[2] || 'ms').toLowerCase()
+
+  return n * types[type]
 }
 
-/**
- * Long format for `ms`.
- *
- * @param {Number} ms
- * @return {String}
- * @api private
- */
+// Short format for `ms`
+function short (ms) {
+  if (ms >= d) return Math.round(ms / d) + 'd'
+  if (ms >= h) return Math.round(ms / h) + 'h'
+  if (ms >= m) return Math.round(ms / m) + 'm'
+  if (ms >= s) return Math.round(ms / s) + 's'
+  return ms + 'ms'
+}
 
-function long(ms) {
+// Long format for `ms`
+function long (ms) {
   return plural(ms, d, 'day')
     || plural(ms, h, 'hour')
     || plural(ms, m, 'minute')
     || plural(ms, s, 'second')
-    || ms + ' ms';
+    || ms + ' ms'
 }
 
-/**
- * Pluralization helper.
- */
-
-function plural(ms, n, name) {
-  if (ms < n) return;
-  if (ms < n * 1.5) return Math.floor(ms / n) + ' ' + name;
-  return Math.ceil(ms / n) + ' ' + name + 's';
+// Pluralization helper.
+function plural (ms, n, name) {
+  if (ms < n) return
+  if (ms < n * 1.5) return Math.floor(ms / n) + ' ' + name
+  return Math.ceil(ms / n) + ' ' + name + 's'
 }
