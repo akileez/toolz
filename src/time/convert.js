cc1930ad35eabca56c0e5a4fd9f76093f16145e4
akileez <git@keeda.net>
2016-05-19 05:50:56 -0400

    refactor time/convert — convert switch statement to object — use object/has for checks

--- src/time/convert.js
+++ src/time/convert.js
@@ -1,26 +1,43 @@
-// convert time into another unit
+var has = require('../object/has')
+
+/*
+  convert time between units
+  Available units: millisecond, second, minute, hour, day, week, year
+  Abbreviations: ms, s, m, h, d, w, y
+
+  months are not supported as a time unit since their values are not fixed
+  default destinationUnitName is `ms`
+*/
+
+var getUnit = {
+  'ms'          : 1,
+  'millisecond' : 1,
+  's'           : 1000,
+  'second'      : 1000,
+  'm'           : 60000,
+  'minute'      : 60000,
+  'h'           : 3600000,
+  'hour'        : 3600000,
+  'd'           : 86400000,
+  'day'         : 86400000,
+  'w'           : 604800000,
+  'week'        : 604800000,
+  'y'           : 31557600000,
+  'year'        : 31557600000
+}
 
 function convert (val, sourceUnitName, destinationUnitName) {
   destinationUnitName = destinationUnitName || 'ms'
-  return (val * getUnit(sourceUnitName)) / getUnit(destinationUnitName)
-}
 
-function getUnit (unitName) {
-  switch (unitName) {
-    case 'ms'          :
-    case 'millisecond' : return 1
-    case 's'           :
-    case 'second'      : return 1000
-    case 'm'           :
-    case 'minute'      : return 60000
-    case 'h'           :
-    case 'hour'        : return 3600000
-    case 'd'           :
-    case 'day'         : return 86400000
-    case 'w'           :
-    case 'week'        : return 604800000
-    default            : throw new Error('"' + unitName + '" is not a valid unit')
+  if (!has(getUnit, destinationUnitName)) {
+    throw new Error('"' + destinationUnitName + '" is not a valid unit')
   }
+
+  if (!has(getUnit, sourceUnitName)) {
+    throw new Error('"' + sourceUnitName + '" is not a valid unit')
+  }
+
+  return (val * getUnit[sourceUnitName]) / getUnit[destinationUnitName]
 }
 
 module.exports = convert
