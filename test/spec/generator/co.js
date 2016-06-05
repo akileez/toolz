6cb9181b1807a6002505eac76e1a278b984d20d9
akileez <git@keeda.net>
2016-06-05 12:24:51 -0400

    update test generator/co — lint and add tests

--- test/spec/generator/co.js
+++ test/spec/generator/co.js
@@ -3,6 +3,9 @@ var t = painless.assert
 var testArgs = painless.createGroup('Test generator/co: Arguments')
 var testProm = painless.createGroup('Test generator/co: Promises')
 var testArr  = painless.createGroup('Test generator/co: Arrays')
+var testCtx  = painless.createGroup('Test generator/co: Context')
+var testGFx  = painless.createGroup('Test generator/co: Generator Functions')
+var testInv  = painless.createGroup('Test generator/co: Invalid')
 
 var co = require('../../../src/generator/co')
 var pify = require('../../../src/promise/pify')
@@ -10,7 +13,7 @@ var read = require('fs').readFile
 
 // pify(read)
 
-function getPromise(val, err) {
+function getPromise (val, err) {
   return new Promise(function (resolve, reject) {
     if (err) reject(err)
     else resolve(val)
@@ -18,24 +21,24 @@ function getPromise(val, err) {
 }
 
 testArgs('co(gen, args) should pass the rest of the arguments', () => {
-  return co(function* (num, str, arr, obj, fn) {
-    t.assert(num === 42);
-    t.assert(str === 'forty-two');
-    t.assert(arr[0] === 42);
-    t.assert(obj.value === 42);
+  return co(function * (num, str, arr, obj, fn) {
+    t.assert(num === 42)
+    t.assert(str === 'forty-two')
+    t.assert(arr[0] === 42)
+    t.assert(obj.value === 42)
     t.assert(fn instanceof Function)
-  }, 42, 'forty-two', [42], { value: 42 }, function () {})
+  }, 42, 'forty-two', [42], {value: 42}, function () {})
 })
 
 testProm('co(* -> yield <promise>) with one promise yield', () => {
-  return co(function* () {
+  return co(function * () {
     var a = yield getPromise(1)
     t.is(1, a)
   })
 })
 
 testProm('co(* -> yield <promise>) with several promise yields', () => {
-  return co(function* () {
+  return co(function * () {
     var a = yield getPromise(1)
     var b = yield getPromise(2)
     var c = yield getPromise(3)
@@ -44,31 +47,90 @@ testProm('co(* -> yield <promise>) with several promise yields', () => {
   })
 })
 
-testArr('co(* -> yield []) should aggregate several promises', function(){
-  return co(function* (){
+testArr('co(* -> yield []) should aggregate several promises', function () {
+  return co(function * () {
     var a = pify(read)('../package.json', 'utf8').then(data => data)
     var b = pify(read)('../LICENSE', 'utf8').then(data => data)
     var c = pify(read)('../package.json', 'utf8').then(data => data)
 
-    var res = yield [a, b, c];
-    t.is(3, res.length);
+    var res = yield [a, b, c]
+    t.is(3, res.length)
     // console.log(res)
-    t.assert(~res[0].indexOf('description'));
-    t.assert(~res[1].indexOf('ISC'));
-    t.assert(~res[2].indexOf('devDependencies'));
-  });
+    t.assert(~res[0].indexOf('description'))
+    t.assert(~res[1].indexOf('ISC'))
+    t.assert(~res[2].indexOf('devDependencies'))
+  })
 })
 
-testArr('co(* -> yield []) should noop with no args', function(){
-  return co(function* (){
-    var res = yield [];
-    t.equal(0, res.length);
-  });
+testArr('co(* -> yield []) should noop with no args', function () {
+  return co(function * () {
+    var res = yield []
+    t.equal(0, res.length)
+  })
 })
 
-testArr('co(* -> yield []) should support an array of generators', function(){
-  return co(function*(){
-    var val = yield [function*(){ return 1 }()]
+testArr('co(* -> yield []) should support an array of generators', function () {
+  return co(function * () {
+    var val = yield [function * () { return 1 }()]
     t.same(val, [1])
+  })
+})
+
+var ctx = {some: 'thing'}
+
+testCtx('co.call(this) should pass the context', function () {
+  return co.call(ctx, function * () {
+    t.assert(ctx === this)
+  })
+})
+
+function sleep (ms) {
+  return function (done) {
+    setTimeout(done, ms)
+  }
+}
+
+function * work () {
+  yield sleep(50)
+  return 'yay'
+}
+
+testGFx('co(fn*) should wrap with co', function () {
+  return co(function * () {
+    var a = yield work
+    var b = yield work
+    var c = yield work
+
+    t.assert(a === 'yay')
+    t.assert(b === 'yay')
+    t.assert(c === 'yay')
+
+    var res = yield [work, work, work]
+    t.same(res, ['yay', 'yay', 'yay'])
+  })
+})
+
+testGFx('co(fn*) should catch errors', function () {
+  return co(function * () {
+    yield function * () {
+      throw new Error('boom')
+    }
+  }).then(function () {
+    throw new Error('wtf')
+  }, function (err) {
+    t.assert(err)
+    t.assert(err.message === 'boom')
+  })
+})
+
+testInv('yield <invalid> should throw an error', function () {
+  return co(function * () {
+    try {
+      yield null
+      throw new Error('lol')
+    } catch (err) {
+      t.assert(err instanceof TypeError)
+      t.assert(~err.message.indexOf('You may only yield'))
+    }
   })
 })
