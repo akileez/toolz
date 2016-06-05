ad949d38c6da90c56ab89ed9cbbed3ffe15ebf73
akileez <git@keeda.net>
2016-06-05 14:09:52 -0400

    update test generator/co — add more tests Promises, Objects

--- test/spec/generator/co.js
+++ test/spec/generator/co.js
@@ -6,6 +6,7 @@ var testArr  = painless.createGroup('Test generator/co: Arrays')
 var testCtx  = painless.createGroup('Test generator/co: Context')
 var testGFx  = painless.createGroup('Test generator/co: Generator Functions')
 var testInv  = painless.createGroup('Test generator/co: Invalid')
+var testObj  = painless.createGroup('Test generator/co: Objects')
 
 var co = require('../../../src/generator/co')
 var pify = require('../../../src/promise/pify')
@@ -44,6 +45,63 @@ testProm('co(* -> yield <promise>) with several promise yields', () => {
     var c = yield getPromise(3)
 
     t.same([1, 2, 3], [a, b, c])
+  })
+})
+
+testProm('co(* -> yield <promise>) should throw and resume when a promise is rejected', () => {
+  var error
+
+  return co(function* () {
+    try {
+      yield getPromise(1, new Error('boom'))
+    } catch (err) {
+      error = err
+    }
+
+    t.assert(error.message === 'boom')
+    var ret = yield getPromise(1)
+    t.assert(ret === 1)
+  })
+})
+
+testProm('co(* -> yield <promise>) should return a real Promise when yielding a non-standard promise-like', function () {
+  t.assert(co(function* () {
+    yield {then: function () {}}
+  }) instanceof Promise)
+})
+
+testProm('co(function) -> promise should return a value', function (done) {
+  co(function () {
+    return 1
+  }).then(function (data) {
+    t.is(data, 1)
+    done()
+  })
+})
+
+testProm('co(function) -> promise should return a resolve promise', function () {
+  return co(function () {
+    return Promise.resolve(1)
+  }).then(function (data) {
+    t.is(data, 1)
+  })
+})
+
+testProm('co(function) -> promise should return a reject promise', function () {
+  return co(function () {
+    return Promise.reject(1)
+  }).catch(function (data) {
+    t.is(data, 1)
+  })
+})
+
+testProm('co(function) -> promise should catch errors', function () {
+  return co(function () {
+    throw new Error('boom')
+  }).then(function () {
+    throw new Error('nope')
+  }).catch(function (err) {
+    t.is(err.message, 'boom')
   })
 })
 
@@ -132,5 +190,78 @@ testInv('yield <invalid> should throw an error', function () {
       t.assert(err instanceof TypeError)
       t.assert(~err.message.indexOf('You may only yield'))
     }
+  })
+})
+
+testObj('co(* -> yield {}) should aggregate several promises', function () {
+  return co(function * () {
+    var a = pify(read)('../package.json', 'utf8').then(data => data)
+    var b = pify(read)('../LICENSE', 'utf8').then(data => data)
+    var c = pify(read)('../package.json', 'utf8').then(data => data)
+
+    var res = yield {
+      a: a,
+      b: b,
+      c: c
+    }
+    t.is(Object.keys(res).length, 3)
+    // console.log(res)
+    t.assert(~res.a.indexOf('description'))
+    t.assert(~res.b.indexOf('ISC'))
+    t.assert(~res.c.indexOf('devDependencies'))
+  })
+})
+
+function Pet (name) {
+  this.name = name
+  this.something = function () {}
+}
+
+testObj('co(* -> yield {}) should ignore non-thunkable properties', function () {
+  return co(function* () {
+    var foo = {
+      name: { first: 'tobi' },
+      age: 2,
+      // should read the lint.js file from the test directory
+      address: pify(read)('./lint.js', 'utf8').then(data => data),
+      tobi: new Pet('tobi'),
+      now: new Date(),
+      falsey: false,
+      nully: null,
+      undefiney: undefined
+    }
+
+    var res = yield foo
+
+    t.eq('tobi', res.name.first)
+    t.eq(2, res.age)
+    t.eq('tobi', res.tobi.name)
+    t.eq(foo.now, res.now)
+    t.eq(false, foo.falsey)
+    t.eq(null, foo.nully)
+    t.eq(undefined, foo.undefiney)
+    t.assert(~res.address.indexOf('exports'))
+  })
+})
+
+testObj('co(* -> yield {}) should preserve key order', function () {
+  function timedThunk (time) {
+    return function (cb) {
+      setTimeout(cb, time)
+    }
+  }
+
+  return co(function* () {
+    var before = {
+      sun: timedThunk(30),
+      rain: timedThunk(20),
+      moon: timedThunk(10)
+    }
+
+    var after = yield before
+
+    var orderBefore = Object.keys(before).join(',')
+    var orderAfter = Object.keys(after).join(',')
+    t.is(orderBefore, orderAfter)
   })
 })
