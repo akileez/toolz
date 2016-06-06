1fed1bfdb54ffaf89702af171e293b9d7d3568d2
akileez <git@keeda.net>
2016-06-06 15:05:56 -0400

    update test promise/pify — complete tests still wip. need to resolve issues running promise/generator with a sync function namely JSON.parse

--- test/spec/promise/pify.js
+++ test/spec/promise/pify.js
@@ -55,6 +55,7 @@ test('multiArgs options', function* () {
 
 test('wrap core method', function* () {
   // must be run from the test directory
+  // i.e. node test prom --file pify
   // REMINDER: resolve file module to mitigate this recurring issue
   t.is(JSON.parse(yield pify(fs.readFile)('../package.json')).name, 'toolz')
 })
@@ -67,4 +68,58 @@ test('module support - doesn\'t trasform *Sync methods by default', function () 
   t.is(JSON.parse(pify(fs).readFileSync('../package.json')).name, 'toolz')
 })
 
+test('module support - preserves non-function members', function () {
+  var module = {
+    method: function () {},
+    nonMethod: 3
+  }
 
+  t.same(Object.keys(module), Object.keys(pify(module)))
+})
+
+test('module support - transforms only members in options.include', function () {
+  var pModule = pify(fixtureModule, {
+    include: ['method1', 'method2']
+  })
+
+  t.is(typeof pModule.method1().then, 'function')
+  t.is(typeof pModule.method2().then, 'function')
+  t.not(typeof pModule.method3().then, 'function')
+})
+
+test('module support - doesn\'t transforms members in options.exclude', function () {
+  var pModule = pify(fixtureModule, {
+    exclude: ['method3']
+  })
+
+  t.is(typeof pModule.method1().then, 'function')
+  t.is(typeof pModule.method2().then, 'function')
+  t.not(typeof pModule.method3().then, 'function')
+})
+
+test('module support - options.include over options.exclude', function () {
+  var pModule = pify(fixtureModule, {
+    include: ['method1', 'method2'],
+    exclude: ['method2', 'method3']
+  })
+
+  t.is(typeof pModule.method1().then, 'function')
+  t.is(typeof pModule.method2().then, 'function')
+  t.not(typeof pModule.method3().then, 'function')
+})
+
+test('module support - function modules', function () {
+  var pModule = pify(fixture4)
+
+  t.is(typeof pModule().then, 'function')
+  t.is(typeof pModule.meow().then, 'function')
+})
+
+test('module support - function modules exclusion', function () {
+  var pModule = pify(fixture4, {
+    excludeMain: true
+  })
+
+  t.is(typeof pModule.meow().then, 'function')
+  t.not(typeof pModule(function () {}).then, 'function')
+})
