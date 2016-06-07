70677acffc3eeef858ee859f6dd6a0077df95468
akileez <git@keeda.net>
2016-06-07 08:40:13 -0400

    update test object/merge — add tests

--- test/spec/object/merge.js
+++ test/spec/object/merge.js
@@ -65,3 +65,143 @@ test('should pass lodash example', function () {
 
   t.same(result, expected)
 })
+
+test('add keys in target that do not exist at the root', function () {
+  var src = { key1: 'value1', key2: 'value2' }
+  target = {}
+
+  var res = merge(target, src)
+
+  t.deepEqual(target, {}, 'merge should be immutable')
+  t.deepEqual(res, src)
+})
+
+test('merge existing simple keys in target at the roots', function () {
+  var src = { key1: 'changed', key2: 'value2' }
+  var target = { key1: 'value1', key3: 'value3' }
+
+  var expected = {
+    key1: 'changed',
+    key2: 'value2',
+    key3: 'value3'
+  }
+
+  t.deepEqual(target, { key1: 'value1', key3: 'value3' })
+  t.deepEqual(merge(target, src), expected)
+})
+
+test('merge nested objects into target', function () {
+  var src = {
+    key1: {
+      subkey1: 'changed',
+      subkey3: 'added'
+    }
+  }
+  var target = {
+    key1: {
+      subkey1: 'value1',
+      subkey2: 'value2'
+    }
+  }
+
+  var expected = {
+    key1: {
+      subkey1: 'changed',
+      subkey2: 'value2',
+      subkey3: 'added'
+    }
+  }
+
+  t.deepEqual(target, {
+    key1: {
+      subkey1: 'value1',
+      subkey2: 'value2'
+    }
+  })
+  t.deepEqual(merge(target, src), expected)
+})
+
+test('replace simple key with nested object in target', function () {
+  var src = {
+    key1: {
+      subkey1: 'subvalue1',
+      subkey2: 'subvalue2'
+    }
+  }
+  var target = {
+    key1: 'value1',
+    key2: 'value2'
+  }
+
+  var expected = {
+    key1: {
+      subkey1: 'subvalue1',
+      subkey2: 'subvalue2'
+    },
+    key2: 'value2'
+  }
+
+  t.deepEqual(target, { key1: 'value1', key2: 'value2' })
+  t.deepEqual(merge(target, src), expected)
+})
+
+test('should add nested object in target', function() {
+  var src = {
+    "b": {
+      "c": {}
+    }
+  }
+
+  var target = {
+    "a": {}
+  }
+
+  var expected = {
+    "a": {},
+    "b": {
+      "c": {}
+    }
+  }
+
+  t.deepEqual(merge(target, src), expected)
+})
+
+test('should replace object with simple key in target', function () {
+  var src = { key1: 'value1' }
+  var target = {
+    key1: {
+      subkey1: 'subvalue1',
+      subkey2: 'subvalue2'
+    },
+    key2: 'value2'
+  }
+
+  var expected = { key1: 'value1', key2: 'value2' }
+
+  t.deepEqual(target, {
+    key1: {
+      subkey1: 'subvalue1',
+      subkey2: 'subvalue2'
+    },
+    key2: 'value2'
+  })
+  t.deepEqual(merge(target, src), expected)
+})
+
+test('should work on arrays of nested objects', function() {
+  var target = [
+  { key1: { subkey: 'one' }}
+  ]
+
+  var src = [
+  { key1: { subkey: 'two' }},
+  { key2: { subkey: 'three' }}
+  ]
+
+  var expected = [
+  { key1: { subkey: 'two' }},
+  { key2: { subkey: 'three' }}
+  ]
+
+  t.deepEqual(merge(target, src), expected)
+})
