c2f36078be87ccfaff49b5c8c7da4ec85d76ac49
akileez <git@keeda.net>
2016-06-08 04:06:49 -0400

    update collection/forEach — coverage

--- src/collection/forEach.js
+++ src/collection/forEach.js
@@ -1,14 +1,19 @@
 var make = require('./_make')
 var makeIterator = require('../function/makeIterator_')
+var hasOwn = Object.prototype.hasOwnProperty
 
 function arrForEach (arr, fn, thisObj) {
   fn = makeIterator(fn, thisObj)
+  // istanbul ignore if
+  // due to the way `_make` works at present, a null or undefined list
+  // will return undefined. may change in future
   if (arr == null) return
 
   var i = -1
   var len = arr.length
 
   while (++i < len) {
+    // istanbul ignore if
     if (fn(arr[i], i, arr) === false) break
   }
 }
@@ -18,7 +23,8 @@ function objForEach (obj, fn, thisObj) {
   var key
 
   for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
+    // istanbul ignore else
+    if (hasOwn.call(obj, key)) {
       fn(obj[key], key, obj)
     }
   }
