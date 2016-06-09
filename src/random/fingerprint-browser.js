78275a646745a5f0b2450c162883f174c052d961
akileez <git@keeda.net>
2016-06-09 02:29:51 -0400

    update random/fingerprint-browser add comments. prep for deprecation

--- src/random/fingerprint-browser.js
+++ src/random/fingerprint-browser.js
@@ -1,8 +1,12 @@
+// https://github.com/therealklanni/browser-fingerprint (MIT)
+// this module will be deprecated in this library. use actual module
+// for use in the browser
+
 'use strict'
 
 const pad = (str, size) => (new Array(size + 1).join('0') + str).slice(-size)
 
-const globalCount = (() => {
+const globalCount = () => {
   let count = 0
   const window = window || global
 
@@ -11,10 +15,20 @@ const globalCount = (() => {
   }
 
   return count
-}())
+}
+
+// for testing purposes only!!!
+let ver = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3`
+let platform = `AppleWebKit/537.36 (KHTML, like Gecko)`
+let ext = `Chrome/43.0.2357.130 Safari/537.36`
+
+global.navigator = {
+  mimeTypes: new Array(8),
+  userAgent: `${ver} ${platform} ${ext}`
+}
 
 const navi = navigator.mimeTypes.length + navigator.userAgent.length
 
 const fingerprint = pad(navi.toString(36) + globalCount.toString(36), 4)
 
-export default () => fingerprint
+module.exports = () => fingerprint
