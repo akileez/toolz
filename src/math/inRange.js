ade1ec8ebf8ae62964adc74c579ff61a5933490a
akileez <git@keeda.net>
2016-06-10 01:27:00 -0400

    update math/inRange — add lang/yoda.nor

--- src/math/inRange.js
+++ src/math/inRange.js
@@ -1,6 +1,17 @@
+var yoda = require('../lang/yoda').nor
+
 // checks if value is inside the range
 
 function inRange (val, min, max, theshold) {
+  if (max === undefined) {
+    max = min
+    min = 0
+  }
+
+  if (yoda('number', typeof val, typeof min, typeof max)) {
+    throw new TypeError('Expected all arguments to be numbers')
+  }
+
   theshold = theshold || 0
   return (val + theshold >= min && val - theshold <= max)
 }
