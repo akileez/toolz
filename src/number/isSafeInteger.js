b97e6358628adcd263dd824bf2db8c6c8cdd6457
akileez <git@keeda.net>
2016-06-10 01:29:19 -0400

    update number/isSafeInteger — change variable name

--- src/number/isSafeInteger.js
+++ src/number/isSafeInteger.js
@@ -1,8 +1,8 @@
 var MAX = require('./MAX_SAFE_INT')
-var isNumber = require('./isFinite')
+var isFinite = require('./isFinite')
 
 function isSafeInteger (val) {
-  return isNumber(val)
+  return isFinite(val)
     && val > -MAX
     && val < MAX
     && Math.floor(val) === val
