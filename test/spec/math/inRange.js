c47d385f1aaeb73e1ddc04c5dac419e61a0e80ee
akileez <git@keeda.net>
2016-06-10 01:27:49 -0400

    update test math/inRange — add coverage tests

--- test/spec/math/inRange.js
+++ test/spec/math/inRange.js
@@ -30,3 +30,26 @@ test('should tolerate threshold', function () {
   t.is(inRange(10.5, 1, 10, 0.5), true)
   t.is(inRange(10.5, 1, 10, 0.25), false)
 })
+
+test('should pass in-range tests', function () {
+  t.true(inRange(0, 0))
+  t.true(inRange(1, 1))
+  t.true(inRange(5, 10))
+  t.true(inRange(1, 0, 1))
+  t.true(inRange(2, 0, 2))
+  t.true(inRange(5, 0, 10))
+  t.true(inRange(10, -Infinity, Infinity))
+  t.true(inRange(-10, -Infinity, Infinity))
+  t.true(inRange(-1, -10, 10))
+  t.true(inRange(1.5, 1.1, 1.7))
+  t.true(inRange(1.5, 1.1, 1.7))
+  t.true(inRange(5, 1, 10)) // exception here --> orignal code inRange(5, 10, 1)
+  t.false(inRange(3, 1, 2))
+})
+
+test('should throw if val, min or max args are not numbers', function () {
+  t.throws(() => {inRange('foo', 'bar', 'baz')})
+  t.throws(() => {inRange(5, 'bar', 'baz')})
+  t.throws(() => {inRange(5, 1, 'baz', 2)})
+  t.throws(() => {inRange(5, 1, new Date(), 2)})
+})
