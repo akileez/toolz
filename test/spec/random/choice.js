013942526065790025195acce4c293000bf1377a
akileez <git@keeda.net>
2016-06-13 11:03:19 -0400

    update test random/choice — add tests

--- test/spec/random/choice.js
+++ test/spec/random/choice.js
@@ -1,10 +1,12 @@
 var painless = require('../../assertion/painless')
 var test = painless.createGroup('Test random/choice')
+var test2 = painless.createGroup('Test random/choice::random-item')
 var t = painless.assert
 
 var choice = require('../../../src/random/choice')
 var mockRandom = require('../../../src/random/mockRandom')
 var contains = require('../../../src/array/contains')
+var stableFn = require('../../../src/function/stable')
 
 test.beforeEach(function () {
   mockRandom.start()
@@ -33,4 +35,14 @@ test('should work with array', function () {
   t.true(contains(arr, a))
   t.true(contains(arr, b))
   t.isnt(a, b)
+})
+
+var fix = ['a', 'b', 'c', 'd', 'e']
+
+test2('should pass random-item tests', function () {
+  t.false(stableFn(() => choice(fix)))
+
+  for (var i = 0; i < 1000; i++) {
+    t.is(typeof choice(fix), 'string')
+  }
 })
