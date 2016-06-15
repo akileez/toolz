28e11627f6ece55de9883c745fde1212b3de7549
akileez <git@keeda.net>
2016-06-15 12:06:44 -0400

    update test path/memo — remove test

--- test/spec/path/memo.js
+++ test/spec/path/memo.js
@@ -33,23 +33,24 @@ test('should return a memoized filepath when called with no arguments', function
   t.eq(qux(), path.resolve('foo/bar/baz/qux'))
 })
 
-test('should expose the memoized filepath on the `.path` property', function () {
-  var foo = memo('foo')
-  var bar = foo('bar')
-  var baz = bar('baz')
-  var qux = baz('qux')
+// skip test. removed functionality
+// test('should expose the memoized filepath on the `.path` property', function () {
+//   var foo = memo('foo')
+//   var bar = foo('bar')
+//   var baz = bar('baz')
+//   var qux = baz('qux')
 
-  t.eq(foo.path, path.resolve('foo'))
-  t.eq(bar.path, path.resolve('foo/bar'))
-  t.eq(baz.path, path.resolve('foo/bar/baz'))
-  t.eq(qux.path, path.resolve('foo/bar/baz/qux'))
-})
+//   t.eq(foo.path, path.resolve('foo'))
+//   t.eq(bar.path, path.resolve('foo/bar'))
+//   t.eq(baz.path, path.resolve('foo/bar/baz'))
+//   t.eq(qux.path, path.resolve('foo/bar/baz/qux'))
+// })
 
 test('should return process.cwd', function () {
   var memoPath = memo()
 
   t.eq(memo()(), process.cwd())
-  t.eq(memoPath.path, process.cwd())
+  // t.eq(memoPath.path, process.cwd())
 })
 
 test('should return a file path when called', function () {
