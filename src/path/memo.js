f93f3535a2aeae7a60f2e99d144cdbd4d0d7d448
akileez <git@keeda.net>
2016-06-15 12:06:18 -0400

    update path/memo — remove path property

--- src/path/memo.js
+++ src/path/memo.js
@@ -1,10 +1,10 @@
-// memorize-path <https://github.com/jonschlinkert/memoize-path>
+// memoize-path <https://github.com/jonschlinkert/memoize-path>
 // Copyright (c) 2016, Jon Schlinkert (MIT)
 'use strict'
 
 // Easily create reusable, stackable file paths from memoized path segments.
 
-const path = require('path')
+var resolve = require('path').resolve
 
 function create (dir) {
   dir = dir || process.cwd()
@@ -14,26 +14,16 @@ function create (dir) {
 
     function memo (str) {
       if (typeof str === 'string') {
-        var dir = path.resolve(cwd, str)
+        var dir = resolve(cwd, str)
         var fn = create(dir)
         fn.parent = cwd
         fn.path = dir
         return fn
       }
 
-      return path.resolve(cwd)
+      return resolve(cwd)
     }
 
-    Object.defineProperty(memo, 'path', {
-      configurable: true,
-      enumerable: true,
-      set: function (val) {
-        curr = val
-      },
-      get: function () {
-        return path.resolve(curr || cwd)
-      }
-    })
     return memo
   }(dir))
 }
