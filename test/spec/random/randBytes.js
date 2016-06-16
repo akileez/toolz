b901aca484e15809741d79eb2dd83b264bfd6d9b
akileez <git@keeda.net>
2016-06-16 07:46:57 -0400

    update test random/randBytes — testing application of find-root and memo

--- test/spec/random/randBytes.js
+++ test/spec/random/randBytes.js
@@ -1,8 +1,16 @@
-var painless = require('../../assertion/painless')
+var memo = require('../../../src/path/memo')
+var root = require('../../../src/path/find-root')
+
+var cwd = memo(root())
+var src = cwd('src')
+var tsa = cwd('test/assertion')
+var tss = cwd('test/spec')
+
+var painless = require(`${tsa()}/painless`)
 var test = painless.createGroup('Test random/randBytes')
 var t = painless.assert
 
-var rng = require('../../../src/random/randBytes')
+var rng = require(`${src()}/random/randBytes`)
 
 test('random bytes', function () {
   var expected = rng()
