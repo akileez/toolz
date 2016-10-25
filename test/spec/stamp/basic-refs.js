b3f1323f5284909e89b58766b0b04ea8cf2d38fb
akileez <git@keeda.net>
2016-10-25 09:01:34 -0400

    update test stamp/basic-refs — stampit v3, lint

--- test/spec/stamp/basic-refs.js
+++ test/spec/stamp/basic-refs.js
@@ -5,67 +5,71 @@ var test     = painless.createGroup('Test object/stampit (refs)')
 var stampit = require('../../../src/object/stamp')
 
 // Basics refs
+// deprecated as of stampit v3 ... reference (refs) alias to properties
 
-test('stampit({ refs })', () => {
-  const obj = stampit({ refs: { foo: { bar: 'bar' } } }).create();
+test('stampit({ refs }) -- syntax no longer in use -- adjustments made', () => {
+  const obj = stampit().refs({foo: {bar: 'bar'}}).create()
 
   t.is(obj.foo.bar, 'bar',
-    'Should set default refs.');
-});
+    'Should set default refs.')
+})
 
 test('stampit().refs()', () => {
   const obj = stampit().refs({
-    foo: { bar: 'bar' },
+    foo: {bar: 'bar'},
     refsOverride: false,
-    func1() {}
+    func1 () {}
   }).refs({
     bar: 'bar',
     refsOverride: true,
-    func2() {}
-  }).create();
+    func2 () {}
+  }).create()
 
   t.is(obj.foo.bar, 'bar',
-    'Should set default refs.');
+    'Should set default refs.')
   t.is(obj.bar, 'bar',
-    'Should set let you add by chaining .refs().');
+    'Should set let you add by chaining .refs().')
   t.ok(obj.refsOverride,
-    'Should set let you override by chaining .refs().');
+    'Should set let you override by chaining .refs().')
   t.ok(obj.func1,
-    'Should mix functions.');
+    'Should mix functions.')
   t.ok(obj.func2,
-    'Should mix functions.');
-});
+    'Should mix functions.')
+})
 
-test('stampit({ refs }).refs()', () => {
-  const obj = stampit({ refs: {
-    foo: { bar: 'bar' },
-    refsOverride: false,
-    func1() {}
-  }}).refs({
-    bar: 'bar',
-    refsOverride: true,
-    func2() {}
-  }).create();
+test('stampit({ refs }).refs() -- syntax no longer in use -- adjustments made', () => {
+  const obj = stampit()
+    .refs({
+      foo: {bar: 'bar'},
+      refsOverride: false,
+      func1 () {}
+    })
+    .refs({
+      bar: 'bar',
+      refsOverride: true,
+      func2 () {}
+    })
+    .create()
 
   t.is(obj.foo.bar, 'bar',
-    'Should set default refs.');
+    'Should set default refs.')
   t.is(obj.bar, 'bar',
-    'Should set let you add by chaining .refs().');
+    'Should set let you add by chaining .refs().')
   t.ok(obj.refsOverride,
-    'Should set let you override by chaining .refs().');
+    'Should set let you override by chaining .refs().')
   t.ok(obj.func1,
-    'Should mix functions.');
+    'Should mix functions.')
   t.ok(obj.func2,
-    'Should mix functions.');
-});
+    'Should mix functions.')
+})
 
 test('stampit().refs(a, b)', () => {
   const obj = stampit().refs({
     a: 'a'
   }, {
     b: 'b'
-  }).create();
+  }).create()
 
   t.ok(obj.a && obj.b,
-    'Should mixIn objects when multiple properties are passed.');
-});
+    'Should mixIn objects when multiple properties are passed.')
+})
