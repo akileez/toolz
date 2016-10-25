4bfcd4ad80b41f246c59b181f192b81d52ba3b94
akileez <git@keeda.net>
2016-10-25 09:01:50 -0400

    update test stamp/basic-statics — stampit v3, lint

--- test/spec/stamp/basic-statics.js
+++ test/spec/stamp/basic-statics.js
@@ -9,83 +9,81 @@ var stampit = require('../../../src/object/stamp')
 test('stampit().statics()', () => {
   const stamp1 = stampit()
     .statics({
-      foo() {
-        return 42;
+      foo () {
+        return 42
       },
       bar: 'space'
-    });
+    })
 
-  t.ok(stamp1.foo, 'Should add statics props to factory.');
-  t.is(stamp1.foo(), 42, 'Should set proper reference.');
-  t.is(stamp1.bar, 'space', 'Should set proper reference.');
-});
+  t.ok(stamp1.foo, 'Should add statics props to factory.')
+  t.is(stamp1.foo(), 42, 'Should set proper reference.')
+  t.is(stamp1.bar, 'space', 'Should set proper reference.')
+})
 
-test('stampit({statics})', () => {
-  const stamp1 = stampit({
-    statics: {
+test('stampit({statics}) -- syntax no longer in use -- adjustments made', () => {
+  const stamp1 = stampit()
+    .statics({
       foo: 42
-    }
-  });
+    })
 
-  t.is(stamp1.foo, 42, 'Should accept statics in options.');
-});
+  t.is(stamp1.foo, 42, 'Should accept statics in options.')
+})
 
 test('stampit().statics() last override', () => {
   const stamp1 = stampit()
     .statics({
-      foo() {
-        return 'override';
+      foo () {
+        return 'override'
       }
-    });
+    })
 
   const stamp2 = stampit()
     .statics({
-      foo() {}
-    }).compose(stamp1);
+      foo () {}
+    }).compose(stamp1)
 
-  t.is(stamp2.foo(), 'override', 'Should override props during composition.');
-});
+  t.is(stamp2.foo(), 'override', 'Should override props during composition.')
+})
 
 test('stampit().statics(arg1, arg2)', () => {
   const stamp1 = stampit().statics(
     {
-      foo1() {}
+      foo1 () {}
     },
     {
-      foo2() {}
+      foo2 () {}
     }
-  );
+  )
 
-  t.ok(stamp1.foo1, 'Should accept multiple args.');
-  t.ok(stamp1.foo2, 'Should accept multiple args.');
-});
+  t.ok(stamp1.foo1, 'Should accept multiple args.')
+  t.ok(stamp1.foo2, 'Should accept multiple args.')
+})
 
 test('stampit.statics(arg1, arg2)', () => {
   const stamp1 = stampit.statics(
     {
-      foo1() {}
+      foo1 () {}
     },
     {
-      foo2() {}
+      foo2 () {}
     }
-  );
+  )
 
-  t.ok(stamp1.foo1, 'Should accept multiple args.');
-  t.ok(stamp1.foo2, 'Should accept multiple args.');
-});
+  t.ok(stamp1.foo1, 'Should accept multiple args.')
+  t.ok(stamp1.foo2, 'Should accept multiple args.')
+})
 
-test('stampit({statics}).statics()', () => {
-  const stamp1 = stampit({
-    statics: {
-      foo1: 'foo1 value'
-    }
-  })
+test('stampit({statics}).statics() -- syntax no longer in use -- adjustments made', () => {
+  const stamp1 = stampit()
     .statics({
-      foo2() {
-        return 'foo2 value';
+      foo1: 'foo1 value'
+    })
+    .statics({
+      foo2 () {
+        return 'foo2 value'
       }
-    });
+    })
 
-  t.is(stamp1.foo1, 'foo1 value', 'Should have statics from options.');
-  t.is(stamp1.foo2(), 'foo2 value', 'Should have statics form chain method.');
-});
+  t.is(stamp1.foo1, 'foo1 value', 'Should have statics from options.')
+  t.is(stamp1.foo2(), 'foo2 value', 'Should have statics form chain method.')
+})
