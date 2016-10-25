e54d7ae04461528284b6de992fd642a01c372584
akileez <git@keeda.net>
2016-10-25 18:45:27 -0400

    source test stamp/isComposable

---
+++ test/spec/stamp/isComposable.js
@@ -0,0 +1,27 @@
+var painless = require('../../assertion/painless')
+var test = painless.createGroup('Test object/stampit::isStamp')
+var t = painless.assert
+var stampit = require('../../../src/object/stamp')
+
+var isComposable = stampit.isComposable
+
+test('isComposable() with objects', () => {
+  const emptyStamp = stampit()
+
+  t.ok(isComposable(emptyStamp), 'Empty stamp should be seen as composable.')
+  t.ok(isComposable(emptyStamp.compose), 'stamp.compose should be seen as composable.')
+  t.ok(isComposable({}), 'An object should be seen as composable.')
+  t.ok(isComposable(() => {}), 'A function should be seen as composable.')
+})
+
+test('isComposable() with non objects', () => {
+  const obj1 = undefined
+  const obj2 = 4
+  const obj3 = 'a string'
+  const obj4 = null
+
+  t.notOk(isComposable(obj1), 'Should not be seen as composable.')
+  t.notOk(isComposable(obj2), 'Should not be seen as composable.')
+  t.notOk(isComposable(obj3), 'Should not be seen as composable.')
+  t.notOk(isComposable(obj4), 'Should not be seen as composable.')
+})
