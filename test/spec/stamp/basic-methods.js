13b533c8cc0f46cf323acad45de0e338ce794d05
akileez <git@keeda.net>
2016-10-25 09:00:25 -0400

    lint test stamp/basic-methods

--- test/spec/stamp/basic-methods.js
+++ test/spec/stamp/basic-methods.js
@@ -6,68 +6,68 @@ var stampit = require('../../../src/object/stamp')
 
 test('stampit({ methods })', () => {
   const obj = stampit().methods({
-    foo () {return 'foo'}
+    foo () { return 'foo' }
   }).create()
 
   t.ok(obj.foo() && !obj.hasOwnProperty('foo'),
-    'Should set the new object\'s prototype.');
-});
+    'Should set the new object\'s prototype.')
+})
 
 test('stampit().methods()', () => {
   const obj = stampit().methods({
-    foo() { return 'foo'; },
-    methodOverride() { return false; },
+    foo () { return 'foo' },
+    methodOverride () { return false },
     prop1: 1
   }).methods({
-    bar() { return 'bar'; },
-    methodOverride() { return true; },
+    bar () { return 'bar' },
+    methodOverride () { return true },
     prop2: 2
-  }).create();
+  }).create()
 
   t.ok(obj.foo() && !obj.hasOwnProperty('foo'),
-    'Should set the new object\'s prototype.');
+    'Should set the new object\'s prototype.')
   t.ok(obj.bar() && !obj.hasOwnProperty('bar'),
-    'Should let you chain .methods() to add more.');
+    'Should let you chain .methods() to add more.')
   t.ok(obj.methodOverride() && !obj.hasOwnProperty('methodOverride'),
-    'Should let you override by chaining .methods().');
+    'Should let you override by chaining .methods().')
   t.ok(obj.prop1 && !obj.hasOwnProperty('prop1'),
-    'Should mix properties.');
+    'Should mix properties.')
   t.ok(obj.prop2 && !obj.hasOwnProperty('prop1'),
-    'Should mix properties.');
-});
+    'Should mix properties.')
+})
 
 test('stampit({ methods }).methods()', () => {
   const obj = stampit()
   .methods({
-    foo() { return 'foo'; },
-    methodOverride() { return false; },
+    foo () { return 'foo' },
+    methodOverride () { return false },
     prop1: 1
   })
   .methods({
-    bar() { return 'bar'; },
-    methodOverride() { return true; },
+    bar () { return 'bar' },
+    methodOverride () { return true },
     prop2: 2
-  }).create();
+  }).create()
 
   t.ok(obj.foo() && !obj.hasOwnProperty('foo'),
-    'Should set the new object\'s prototype.');
+    'Should set the new object\'s prototype.')
   t.ok(obj.bar() && !obj.hasOwnProperty('bar'),
-    'Should let you chain .methods() to add more.');
+    'Should let you chain .methods() to add more.')
   t.ok(obj.methodOverride() && !obj.hasOwnProperty('methodOverride'),
-    'Should let you override by chaining .methods().');
+    'Should let you override by chaining .methods().')
   t.ok(obj.prop1 && !obj.hasOwnProperty('prop1'),
-    'Should mix properties.');
+    'Should mix properties.')
   t.ok(obj.prop2 && !obj.hasOwnProperty('prop2'),
-    'Should mix properties.');
-});
+    'Should mix properties.')
+})
 
 test('stampit().methods(a, b)', () => {
   const obj = stampit().methods({
-    a() { return 'a'; }
+    a () { return 'a' }
   }, {
-    b() { return 'b'; }
-  }).create();
+    b () { return 'b' }
+  }).create()
 
   t.ok(obj.a() === 'a' && obj.b() === 'b',
-    'Should mixIn objects when multiple methods are passed.');
-});
+    'Should mixIn objects when multiple methods are passed.')
+})
