628a123c452c4fa818f2e08ba96fce5bf23255c8
akileez <git@keeda.net>
2016-10-25 09:01:19 -0400

    update test stamp/basic-props — stampit v3, lint

--- test/spec/stamp/basic-props.js
+++ test/spec/stamp/basic-props.js
@@ -6,66 +6,68 @@ var stampit = require('../../../src/object/stamp')
 
 // Basics Props
 
-test('stampit({ props })', () => {
-  const obj = stampit({ props: { foo: { bar: 'bar' } } }).create();
+test('stampit({ props }) -- syntax no longer in use -- adjustments made', () => {
+  const obj = stampit().props({foo: {bar: 'bar'}}).create()
 
   t.is(obj.foo.bar, 'bar',
-    'Should set default props.');
-});
+    'Should set default props.')
+})
 
 test('stampit().props()', () => {
   const obj = stampit().props({
-    foo: { bar: 'bar' },
+    foo: {bar: 'bar'},
     propsOverride: false,
-    func1() {}
+    func1 () {}
   }).props({
     bar: 'bar',
     propsOverride: true,
-    func2() {}
-  }).create();
+    func2 () {}
+  }).create()
 
   t.is(obj.foo.bar, 'bar',
-    'Should set default props.');
+    'Should set default props.')
   t.is(obj.bar, 'bar',
-    'Should set let you add by chaining .props().');
+    'Should set let you add by chaining .props().')
   t.ok(obj.propsOverride,
-    'Should set let you override by chaining .props().');
+    'Should set let you override by chaining .props().')
   t.ok(obj.func1,
-    'Should mix functions.');
+    'Should mix functions.')
   t.ok(obj.func2,
-    'Should mix functions.');
-});
+    'Should mix functions.')
+})
 
-test('stampit({ props }).props()', () => {
-  const obj = stampit({ props: {
-    foo: { bar: 'bar' },
-    propsOverride: false,
-    func1() {}
-  }}).props({
-    bar: 'bar',
-    propsOverride: true,
-    func2() {}
-  }).create();
+test('stampit({ props }).props() -- syntax no longer in use -- adjustments made', () => {
+  const obj = stampit()
+    .props({
+      foo: {bar: 'bar'},
+      propsOverride: false,
+      func1 () {}
+    })
+    .props({
+      bar: 'bar',
+      propsOverride: true,
+      func2 () {}
+    }).create()
 
   t.is(obj.foo.bar, 'bar',
-    'Should set default props.');
+    'Should set default props.')
   t.is(obj.bar, 'bar',
-    'Should set let you add by chaining .props().');
+    'Should set let you add by chaining .props().')
   t.ok(obj.propsOverride,
-    'Should set let you override by chaining .props().');
+    'Should set let you override by chaining .props().')
   t.ok(obj.func1,
-    'Should mix functions.');
+    'Should mix functions.')
   t.ok(obj.func2,
-    'Should mix functions.');
-});
+    'Should mix functions.')
+})
 
 test('stampit().props(a, b)', () => {
   const obj = stampit().props({
     a: 'a'
   }, {
     b: 'b'
-  }).create();
+  }).create()
 
   t.ok(obj.a && obj.b,
-    'Should mixIn objects when multiple objects are passed.');
-});
+    'Should mixIn objects when multiple objects are passed.')
+})
