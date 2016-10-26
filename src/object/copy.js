b590418d32a0e19348d826862f75712338497e1d
akileez <git@keeda.net>
2016-10-26 11:29:45 -0400

    fix object/copy — typo props —> prop

--- src/object/copy.js
+++ src/object/copy.js
@@ -2,10 +2,10 @@ function objectCopy (source, prop, object) {
   object || (object = {})
 
   var idx = -1
-  var len = props.length
+  var len = prop.length
 
   while (++idx < len) {
-    var key = props[idx]
+    var key = prop[idx]
     object[key] = source(key)
   }
   return object
