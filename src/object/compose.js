eef4e63403b22241a841b77a6b38e8aceca0c9e5
akileez <git@keeda.net>
2016-10-29 16:07:41 -0400

    update object/compose — stampit v3 compatibility

--- src/object/compose.js
+++ src/object/compose.js
@@ -7,22 +7,22 @@
   The code is optimized to be as readable as possible.
 
   createFactory: Creates new factory instance.
-    @param {object} descriptor The information about the object the factory will be creating.
+    @param {Descriptor} descriptor The information about the object the factory will be creating.
     @returns {Function} The new factory function.
 
   createStamp: Returns a new stamp given a descriptor and a compose function implementation.
     @param {object} [descriptor={}] The information about the object the stamp will be creating.
-    @param {Function} composeFunction The "compose" function implementation.
-    @returns {Function}
+    @param {Compose} composeFunction The "compose" function implementation.
+    @returns {Stamp}
 
   mergeComposable: Mutates the dstDescriptor by merging the srcComposable data into it.
-    @param {object} dstDescriptor The descriptor object to merge into.
-    @param {object} [srcComposable] The composable (either descriptor or stamp) to merge data form.
-    @returns {object} Returns the dstDescriptor argument.
+    @param {Descriptor} dstDescriptor The descriptor object to merge into.
+    @param {Stamp|Descriptor} [srcComposable] The composable (either descriptor or stamp) to merge data form.
+    @returns {Descriptor} Returns the dstDescriptor argument.
 
   compose: Given the list of composables (stamp descriptors and stamps) returns a new stamp (composable factory function).
-    @param {...(object|Function)} [composables] The list of composables.
-    @returns {Function} A new stamp (aka composable factory function).
+    @param {...(Stamp|Descriptor)} [composables] The list of composables.
+    @returns {Stamp} A new stamp (aka composable factory function).
 */
 
 const isFunction   = require('../lang/isFunction')
@@ -38,7 +38,7 @@ const apply        = require('../function/apply')
 function createFactory (descriptor) {
   return function Stamp (options) {
     let args = slice(arguments, 1)
-    const obj = Object.create(descriptor.methods || {})
+    const obj = Object.create(descriptor.methods || null)
 
     merge(obj, descriptor.deepProperties)
     assign(obj, descriptor.properties)
