63c6886cbef93adea51f016faaf4656b65b62c95
akileez <git@keeda.net>
2017-01-01 03:47:59 -0500

    update file/readFile — minor streamline

--- src/file/readFile.js
+++ src/file/readFile.js
@@ -1,27 +1,28 @@
-var fs = require('fs')
+var fsreadFile = require('fs').readFile
+var fsreadFileSync = require('fs').readFileSync
 var assert = require('assert')
 
 function readFile (fp, opts, cb) {
-  if (arguments.length === 1) return fs.readFileSync(fp, 'utf8')
+  if (arguments.length === 1) return fsreadFileSync(fp, 'utf8')
 
   if (arguments.length === 2) {
     if (typeof opts === 'function') {
       cb = opts
       opts = 'utf8'
     } else {
-      return fs.readFileSync(fp, opts)
+      return fsreadFileSync(fp, opts)
     }
   }
 
-  fs.readFile(fp, opts, function (err, data) {
+  fsreadFile(fp, opts, function (err, data) {
     assert.ifError(err)
     cb(null, data)
   })
 }
 
-function readStream (fp, opts) {
-  return fs.createReadStream(fp, opts)
-}
+// function readStream (fp, opts) {
+//   return fs.createReadStream(fp, opts)
+// }
 
 module.exports = readFile
-module.exports.stream = readStream
+// module.exports.stream = readStream
