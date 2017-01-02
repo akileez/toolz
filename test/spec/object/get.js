f8a75edaedb73b58efd406191ed8bca7626df41f
akileez <git@keeda.net>
2017-01-02 10:19:53 -0500

    update test/object/get — comment out used test

--- test/spec/object/get.js
+++ test/spec/object/get.js
@@ -5,6 +5,18 @@ var t = painless.assert
 
 var get = require('../../../src/object/get')
 
+// function get (object, expression) {
+//   if (!(object && expression)) throw new Error('both object and expression args are required')
+//   return expression.trim().split('.').reduce(function (prev, curr) {
+//     var arr = curr.match(/(.*?)\[(.*?)\]/)
+//     if (arr) {
+//       return prev && prev[arr[1]][arr[2]]
+//     } else {
+//       return prev && prev[curr]
+//     }
+//   }, object)
+// }
+
 test('should get nested property', function () {
   var foo = {
     bar: {
