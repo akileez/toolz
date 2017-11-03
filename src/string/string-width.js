c13278761b6b1fac650d4bce35d2e711b78e605e
akileez <git@keeda.net>
2017-11-03 09:35:07 -0400

    update string/string-width — rewrite

--- src/string/string-width.js
+++ src/string/string-width.js
@@ -1,27 +1,25 @@
-function stripAnsi (str) {
-  return typeof str === 'string' ? str.replace(ansiRegex(), '') : str;
-};
-
-function ansiRegex () {
-  return /\u001b\[(?:[0-9]{1,3}(?:;[0-9]{1,3})*)?[m|K]/g;
-};
-
-var codePointAt = require('./code-point-at')
-var isFullWidthCodePoint = require('./is-fullwidth-code-point')
+const stripAnsi = require('../util/strip-ansi')
+const isFullWidthCodePoint = require('./is-fullwidth-code-point')
 
 function stringWidth (str) {
   if (typeof str !== 'string' || str.length === 0) return 0
 
-  var width = 0
   str = stripAnsi(str)
-  var i = -1
-  var len = str.length
+
+  let width = 0
+  let i = -1
+  let len = str.length
 
   while (++i < len) {
-    var code = codePointAt(str, i)
-    if (code >= 0x10000) ++i
-    if (isFullWidthCodePoint(code)) width += 2
-    else width++
+    const code = str.codePointAt(i)
+
+    // Ignore control characters
+    if (code <= 0x1F || (code >= 0x7F && code <= 0x9F)) continue
+    // Ignore combining characters
+    if (code >= 0x300 && code <= 0x36F) continue
+    if (code >= 0xFFFF) ++i
+    
+    width += isFullWidthCodePoint(code) ? 2 : 1
   }
 
   return width
