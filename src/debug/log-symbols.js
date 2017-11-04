e84a34958758cf89d04767683629934e40d08864
akileez <git@keeda.net>
2017-11-04 08:06:45 -0400

    source debug/log-symbols

---
+++ src/debug/log-symbols.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const clrz = require('../util/colorz')
+const main = {
+	info    : clrz.blue('ℹ'),
+	success : clrz.green('✔'),
+	warning : clrz.yellow('⚠'),
+	error   : clrz.red('✖')
+}
+
+module.exports = main
